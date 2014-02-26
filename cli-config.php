6338ad4475a5c8c6bb37cc36026f2b5d67c62aaf
Taras Machyshyn <taras@letrium.com>
2014-02-26 10:30:33 +0200

    deleting unnecessary files

--- cli-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// cli-config.php
-require_once "bootstrap.php";
-
-$helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
-    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($base->em)
-));
