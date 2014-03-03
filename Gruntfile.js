371bba1ee8379c1ee263a72c3e1c52d88902e44b
Yuri Kuznetsov <yurak@list.ru>
2014-03-03 12:11:23 +0200

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		clean: {
+			build: ['build/*'],
+		},
+		copy: {
+			build: {
+				expand: true,
+				src: [
+					'api/**',
+					'application/**',
+					'data',
+					'vendor/**',
+					'bootstrap.php',
+					'cron.php',
+					'index.php',
+					'LICENSE.txt',
+					'.htaccess',
+				],
+				dest: 'build/',
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerTask('default', [
+		'clean',
+		'copy',
+	]);
+};
