ca93f82b2307822b3c24dca3ef25f1573fb21e77
Yuri Kuznetsov <yurak@list.ru>
2014-03-25 15:19:26 +0200

    remove trailing spaces

--- application/Espo/Core/Jobs/Base.php
+++ application/Espo/Core/Jobs/Base.php
@@ -18,7 +18,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
- ************************************************************************/ 
+ ************************************************************************/
 
 namespace Espo\Core\Jobs;
 
@@ -28,43 +28,43 @@ use \Espo\Core\Container;
 abstract class Base
 {
 	private $container;
-	
+
 	protected function getContainer()
 	{
 		return $this->container;
 	}
-	
+
 	protected function getEntityManager()
 	{
 		return $this->getContainer()->get('entityManager');
 	}
-	
+
 	protected function getServiceFactory()
 	{
 		return $this->getContainer()->get('serviceFactory');
-	}	
-	
+	}
+
 	protected function getConfig()
 	{
 		return $this->getContainer()->get('config');
 	}
-	
+
 	protected function getMetadata()
 	{
 		return $this->getContainer()->get('metadata');
 	}
-	
+
 	protected function getUser()
 	{
 		return $this->getContainer()->get('user');
-	}	
-	
+	}
+
 	public function __construct(Container $container)
 	{
 		$this->container = $container;
 	}
-	
-	abstract public function run();	
+
+	abstract public function run();
 
 }
 
