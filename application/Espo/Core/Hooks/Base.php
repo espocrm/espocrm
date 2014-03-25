481ccc99dac03c75cf6bd84f186d9d5cd0ba9bf3
Yuri Kuznetsov <yurak@list.ru>
2014-03-25 15:22:52 +0200

    remove trailing spaces

--- application/Espo/Core/Hooks/Base.php
+++ application/Espo/Core/Hooks/Base.php
@@ -18,14 +18,14 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
- ************************************************************************/ 
+ ************************************************************************/
 
 namespace Espo\Core\Hooks;
 
 use \Espo\Core\Interfaces\Injectable;
 
 class Base implements Injectable
-{	
+{
 	protected $dependencies = array(
 		'entityManager',
 		'config',
@@ -33,35 +33,35 @@ class Base implements Injectable
 		'acl',
 		'user',
 	);
-	
+
 	protected $injections = array();
-	
+
 	public static $order = 9;
-	
+
 	public function __construct()
 	{
 		$this->init();
 	}
-	
+
 	protected function init()
-	{	
+	{
 	}
-	
+
 	public function getDependencyList()
 	{
 		return $this->dependencies;
 	}
-	
+
 	protected function getInjection($name)
 	{
 		return $this->injections[$name];
 	}
-	
+
 	public function inject($name, $object)
 	{
 		$this->injections[$name] = $object;
 	}
-		
+
 	protected function getEntityManager()
 	{
 		return $this->injections['entityManager'];
@@ -71,26 +71,25 @@ class Base implements Injectable
 	{
 		return $this->injections['user'];
 	}
-	
+
 	protected function getAcl()
 	{
 		return $this->injections['acl'];
 	}
-	
+
 	protected function getConfig()
 	{
 		return $this->injections['config'];
 	}
-	
+
 	protected function getMetadata()
 	{
 		return $this->injections['metadata'];
 	}
-	
-	protected function getRepository()
-	{		
-		return $this->getEntityManager()->getRepository($this->entityName);
-	}	
 
+	protected function getRepository()
+	{
+		return $this->getEntityManager()->getRepository($this->entityName);
+	}
 }
 
