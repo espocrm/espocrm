ad7748f11c1273743879da7947623c8c05cac24d
Taras Machyshyn <taras@letrium.com>
2014-03-27 10:30:56 +0200

    fix API tests

--- tests/Api/LayoutTest.php
+++ tests/Api/LayoutTest.php
@@ -8,6 +8,11 @@ class LayoutTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
+    	$config = include('tests/Api/config.php');
+    	if (!$config['apiTestsEnabled']) {
+    		$this->markTestSkipped('API tests are not enabled.');
+    	}
+
     	require_once('tests/Api/RestTesterClass.php');
         $this->fixture = new RestTesterClass();
 
@@ -32,17 +37,17 @@ class LayoutTest extends \PHPUnit_Framework_TestCase
 
 		//check if file exists
 		$file = 'application/Espo/Custom/Resources/layouts/CustomTest/testPut.json';
-		
+
 		$fileExists = file_exists($file);
-		$this->assertTrue($fileExists);   
-				
+		$this->assertTrue($fileExists);
+
 		if ($fileExists) {
-			$content = file_get_contents($file);   
-						
-			$this->assertEquals($data, $content);  				
-			
+			$content = file_get_contents($file);
+
+			$this->assertEquals($data, $content);
+
 			@unlink($file);
-		}        		
+		}
 	}
 
 	function testPatch()
@@ -51,25 +56,25 @@ class LayoutTest extends \PHPUnit_Framework_TestCase
 
 		$this->fixture->setUrl('/CustomTest/layout/testPatch');
 		$data= '[{"label":"MyLabel"}]';
-		$this->assertTrue($this->fixture->isSuccess( $data ));  		
-		
+		$this->assertTrue($this->fixture->isSuccess( $data ));
+
 		$data= '[{"isPathed":true}]';
 		$this->assertTrue($this->fixture->isSuccess( $data ));
 
 		//check if file exists
 		$file = 'application/Espo/Custom/Resources/layouts/CustomTest/testPatch.json';
-		
+
 		$fileExists = file_exists($file);
 		$this->assertTrue($fileExists);
-		
-		if ($fileExists) {    
-			
-			$content = file_get_contents($file);   
-						
+
+		if ($fileExists) {
+
+			$content = file_get_contents($file);
+
 			//$data= '[{"label":"MyLabel","isPathed":true}]';
 			$data= '[{"isPathed":true}]';  //now PATCH works like PUT
-			$this->assertEquals($data, $content);  
-			
+			$this->assertEquals($data, $content);
+
 			@unlink($file);
 		}
 	}
@@ -84,7 +89,7 @@ class LayoutTest extends \PHPUnit_Framework_TestCase
 		$this->fixture->setUrl('/needToBeNotReal/layout/notReal');
 		$response= $this->fixture->getResponse();
 		$this->assertEquals(404, $response['code']);
-	}   
+	}
 
 
 }
