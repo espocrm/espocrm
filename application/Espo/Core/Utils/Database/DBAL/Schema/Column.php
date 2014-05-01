e4a411797a3d2bd5c2a918d4c110dc5765454cda
Taras Machyshyn <taras@letrium.com>
2014-05-01 14:04:40 +0300

    fixed autoincrement field

--- application/Espo/Core/Utils/Database/DBAL/Schema/Column.php
+++ application/Espo/Core/Utils/Database/DBAL/Schema/Column.php
@@ -31,4 +31,52 @@ class Column extends \Doctrine\DBAL\Schema\Column
 	 */
 	protected $_notnull = false;
 
+	/**
+	 * @var boolean
+	 */
+	protected $_unique = false;
+
+
+	/**
+	 * @param boolean $unique
+	 *
+	 * @return \Doctrine\DBAL\Schema\Column
+	 */
+	public function setUnique($unique)
+	{
+		$this->_unique = (bool)$unique;
+
+		return $this;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getUnique()
+	{
+		return $this->_unique;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function toArray()
+	{
+		return array_merge(array(
+			'name'          => $this->_name,
+			'type'          => $this->_type,
+			'default'       => $this->_default,
+			'notnull'       => $this->_notnull,
+			'length'        => $this->_length,
+			'precision'     => $this->_precision,
+			'scale'         => $this->_scale,
+			'fixed'         => $this->_fixed,
+			'unsigned'      => $this->_unsigned,
+			'autoincrement' => $this->_autoincrement,
+			'unique' => $this->_unique,
+			'columnDefinition' => $this->_columnDefinition,
+			'comment' => $this->_comment,
+		), $this->_platformOptions, $this->_customSchemaOptions);
+	}
+
 }
