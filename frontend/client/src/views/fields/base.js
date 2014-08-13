bd70e2424c4f4494444e99dad29aea34644512ba
Yuri Kuznetsov <yurak@list.ru>
2014-08-13 14:21:03 +0300

    isRequired fix

--- frontend/client/src/views/fields/base.js
+++ frontend/client/src/views/fields/base.js
@@ -426,7 +426,7 @@ Espo.define('Views.Fields.Base', 'View', function (Dep) {
 		},
 
 		validateRequired: function () {
-			if (this.params.required || this.model.isRequired(this.name)) {
+			if (this.isRequired()) {
 				if (this.model.get(this.name) === '') {
 					var msg = this.translate('fieldIsRequired', 'messages').replace('{field}', this.translate(this.name, 'fields', this.model.name));
 					this.showValidationMessage(msg);
