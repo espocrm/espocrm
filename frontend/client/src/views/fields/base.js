80fd2a20060072547ff2b88acb6f414174687bf7
Yuri Kuznetsov <yurak@list.ru>
2014-08-14 17:13:08 +0300

    fix tooltips

--- frontend/client/src/views/fields/base.js
+++ frontend/client/src/views/fields/base.js
@@ -206,11 +206,13 @@ Espo.define('Views.Fields.Base', 'View', function (Dep) {
 			if ((this.mode == 'detail' || this.mode == 'edit') && this.model.getFieldParam(this.name, 'tooltip')) {
 				this.once('after:render', function () {
 					var $a = $('<a href="javascript:" class="text-muted"><span class="glyphicon glyphicon-info-sign"></span></a>');
+					var $label = this.getLabelElement();
+					$label.append(' ');
 					this.getLabelElement().append($a);
 					$a.popover({
 						placement: 'bottom',
 						container: 'body',
-						content: this.translate(this.name, 'tooltipls', this.model.name),
+						content: this.translate(this.name, 'tooltips', this.model.name),
 						trigger: 'click focus',
 					}).on('shown.bs.popover', function () {
 						$('body').one('click', function () {
