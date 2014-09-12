85fb55c99982e5931110cc1b5589cdb3836b14d8
Yuri Kuznetsov <yurak@list.ru>
2014-09-12 17:04:44 +0300

    load team for entity in checkTeam

--- application/Espo/Services/Note.php
+++ application/Espo/Services/Note.php
@@ -42,8 +42,7 @@ class Note extends Record
 	{
 		if (!empty($data['parentType']) && !empty($data['parentId'])) {
 			$entity = $this->getEntityManager()->getEntity($data['parentType'], $data['parentId']);
-			if ($entity) {
-				$entity->loadLinkMultipleField('teams');
+			if ($entity) {				
 				if (!$this->getAcl()->check($entity, 'read')) {
 					throw new Forbidden();
 				}
