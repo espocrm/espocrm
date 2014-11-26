cba59fe8478272c62136b0f02c2579e476a647ac
Yuri Kuznetsov <yurak@list.ru>
2014-11-26 10:17:36 +0200

    EmailReceived Note: dont copy attachments

--- application/Espo/Entities/Note.php
+++ application/Espo/Entities/Note.php
@@ -18,7 +18,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
- ************************************************************************/ 
+ ************************************************************************/
 
 namespace Espo\Entities;
 
@@ -26,16 +26,29 @@ class Note extends \Espo\Core\ORM\Entity
 {
     public function loadAttachments()
     {
-        $collection = $this->get('attachments');
+        $data = $this->get('data');
+        if (!empty($data) && !empty($data->attachmentsIds)) {
+            $attachmentsIds = $data->attachmentsIds;
+            $collection = array();
+            foreach ($attachmentsIds as $id) {
+                $attachment = $this->entityManager->getEntity('Attachment', $id);
+                if ($attachment) {
+                    $collection[] = $attachment;
+                }
+            }
+        } else {
+            $collection = $this->get('attachments');
+        }
+        
         $ids = array();
         $names = new \stdClass();
-        $types = new \stdClass();    
+        $types = new \stdClass();
         foreach ($collection as $e) {
             $id = $e->id;
             $ids[] = $id;
             $names->$id = $e->get('name');
             $types->$id = $e->get('type');
-        }            
+        }
         $this->set('attachmentsIds', $ids);
         $this->set('attachmentsNames', $names);
         $this->set('attachmentsTypes', $types);
--- application/Espo/Services/Stream.php
+++ application/Espo/Services/Stream.php
@@ -288,15 +288,11 @@ class Stream extends \Espo\Core\Services\Base
         $data['emailId'] = $email->id;
         $data['emailName'] = $email->get('name');
         $data['isInitial'] = $isInitial;
+        $data['attachmentsIds'] = $email->get('attachmentsIds');
 
         $note->set('data', $data);
 
         $this->getEntityManager()->saveEntity($note);
-
-        $attachmentsIds = $email->get('attachmentsIds');
-        if (!empty($attachmentsIds)) {
-            $attachmentsData = $this->getServiceFactory()->create('Email')->copyAttachments($email->id, 'Note', $note->id);
-        }
     }
 
     public function noteCreate(Entity $entity)
