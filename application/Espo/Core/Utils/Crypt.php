ac1748067b0368ce094f3975b8b2d961c05b0a19
Yuri Kuznetsov <yurak@list.ru>
2014-11-28 16:34:20 +0200

    fix crypt

--- application/Espo/Core/Utils/Crypt.php
+++ application/Espo/Core/Utils/Crypt.php
@@ -29,6 +29,8 @@ class Crypt
     private $key = null;
     
     private $cryptKey = null;
+
+    private $iv = null;
     
     public function __construct($config)
     {
@@ -43,15 +45,28 @@ class Crypt
         }
         return $this->key;
     }
+
+    protected function getIv()
+    {
+        if (empty($this->iv)) {
+            $this->iv = mcrypt_create_iv(16, MCRYPT_RAND);
+        }
+        return $this->iv;
+    }
     
     public function encrypt($string)
     {
-        return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->getKey(), $string, MCRYPT_MODE_CBC));
+        $iv = $this->getIv();
+        return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $this->getKey(), $string, MCRYPT_MODE_CBC, $iv) . $iv);
     }
     
     public function decrypt($encryptedString)
     {
-        return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->getKey(), base64_decode($encryptedString), MCRYPT_MODE_CBC));
+        $encryptedString = base64_decode($encryptedString);
+
+        $string = substr($encryptedString, 0, strlen($encryptedString) - 16);
+        $iv = substr($encryptedString, -16);
+        return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $this->getKey(), $string, MCRYPT_MODE_CBC, $iv));
     }
     
     public function generateKey()
