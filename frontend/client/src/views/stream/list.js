d9973bf353864e64e1f44990450a3e554414afff
Yuri Kuznetsov <yurak@list.ru>
2014-12-03 10:31:51 +0200

    cleanup

--- frontend/client/src/views/stream/list.js
+++ frontend/client/src/views/stream/list.js
@@ -31,13 +31,13 @@ Espo.define('Views.Stream.List', 'Views.Record.ListExpanded', function (Dep) {
             var key = 'row-' + model.id;
             this.rows.push(key);
             
-            var type = model.get('type');                        
+            var type = model.get('type');
             var viewName = 'Stream.Notes.' + type;
 
             this.createView(key, viewName, {
                 model: model,
                 acl: {
-                    edit: this.getAcl().checkModel(model)// this.getUser().isAdmin() || model.get('createdById') == this.getUser().id
+                    edit: this.getAcl().checkModel(model)
                 },
                 isUserStream: this.options.isUserStream,
                 optionsToPass: ['acl'],
@@ -54,12 +54,12 @@ Espo.define('Views.Stream.List', 'Views.Record.ListExpanded', function (Dep) {
             if (this.collection.length > 0) {
                 this.wait(true);
                                     
-                var count = this.collection.models.length;                
+                var count = this.collection.models.length;
                 var built = 0;
-                for (var i in this.collection.models) {                            
+                for (var i in this.collection.models) {
                     var model = this.collection.models[i];
                     this.buildRow(i, model, function () {
-                        built++;                        
+                        built++;
                         if (built == count) {
                             if (typeof callback == 'function') {
                                 callback();
@@ -76,28 +76,28 @@ Espo.define('Views.Stream.List', 'Views.Record.ListExpanded', function (Dep) {
         },
         
         showNewRecords: function () {
-            var collection = this.collection;        
-            var initialCount = collection.length;        
+            var collection = this.collection;
+            var initialCount = collection.length;
             
             var $list = this.$el.find(this.listContainerEl);
 
             var success = function () {
                 var rowCount = collection.length - initialCount;
                 var rowsReady = 0;
-                for (var i = rowCount - 1; i >= 0; i--) {                    
+                for (var i = rowCount - 1; i >= 0; i--) {
                     var model = collection.at(i);
                     
-                    this.buildRow(i, model, function (view) {            
+                    this.buildRow(i, model, function (view) {
                         view.getHtml(function (html) {
-                            $list.prepend(html);                                
+                            $list.prepend(html);
                             rowsReady++;
                             view._afterRender();
                             if (view.options.el) {
                                 view.setElement(view.options.el);
-                            }                                                    
+                            }
                         }.bind(this));
-                    });                        
-                }    
+                    });
+                }
                 this.noRebuild = true;
             }.bind(this);
             
