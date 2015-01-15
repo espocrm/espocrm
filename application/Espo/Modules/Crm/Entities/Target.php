8035bc424e72fcf045800e0333996736acc7121d
Taras Machyshyn <taras@letrium.com>
2015-01-15 15:36:20 +0200

    added 2015 to the license

--- application/Espo/Modules/Crm/Entities/Target.php
+++ application/Espo/Modules/Crm/Entities/Target.php
@@ -3,7 +3,7 @@
  * This file is part of EspoCRM.
  *
  * EspoCRM - Open Source CRM application.
- * Copyright (C) 2014  Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Copyright (C) 2014-2015 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
  * Website: http://www.espocrm.com
  *
  * EspoCRM is free software: you can redistribute it and/or modify
