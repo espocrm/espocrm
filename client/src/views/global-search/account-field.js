c5c86a7db31c371ace5481c5c6ec2c810777db7a
Alasdair Campbell <alasdair@culturerepublic.co.uk>
2015-01-16 10:36:26 +0000

    Show account name in global search results

--- client/src/views/global-search/account-field.js
+++ client/src/views/global-search/account-field.js
@@ -2,7 +2,7 @@
  * This file is part of EspoCRM.
  *
  * EspoCRM - Open Source CRM application.
- * Copyright (C) 2014-2015 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Copyright (C) 2017  Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
  * Website: http://www.espocrm.com
  *
  * EspoCRM is free software: you can redistribute it and/or modify
@@ -17,26 +17,18 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
- *
- * The interactive user interfaces in modified source and object code versions
- * of this program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU General Public License version 3.
- *
- * In accordance with Section 7(b) of the GNU General Public License version 3,
- * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-Espo.define('views/global-search/name-field', 'views/fields/base', function (Dep) {
+Espo.define('views/global-search/account-field', 'views/fields/base', function (Dep) {
 
     return Dep.extend({
 
-        listTemplate: 'global-search/name-field',
+        listTemplate: 'global-search.account-field',
 
         data: function () {
             return {
                 scope: this.model.get('_scope'),
-                name: this.model.get('name'),
-                acccount: this.model.get('accountName'),
+                name: this.model.get('accountName'),
                 id: this.model.id,
             };
         },
