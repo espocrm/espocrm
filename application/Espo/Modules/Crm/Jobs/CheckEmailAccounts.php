71f40fd440c22fcee7f0933c14c71441835852de
yuri <yurak@list.ru>
2015-01-23 17:12:12 +0200

    checlemailaccounts job

--- application/Espo/Modules/Crm/Jobs/CheckEmailAccounts.php
+++ application/Espo/Modules/Crm/Jobs/CheckEmailAccounts.php
@@ -24,18 +24,18 @@ namespace Espo\Modules\Crm\Jobs;
 
 use \Espo\Core\Exceptions;
 
-class CheckInboundEmails extends \Espo\Core\Jobs\Base
+class CheckEmailAccounts extends \Espo\Core\Jobs\Base
 {
     public function run()
     {
-        $service = $this->getServiceFactory()->create('InboundEmail');
-        $collection = $this->getEntityManager()->getRepository('InboundEmail')->where(array('status' => 'Active'))->find();
+        $service = $this->getServiceFactory()->create('EmailAccount');
+        $collection = $this->getEntityManager()->getRepository('EmailAccount')->where(array('status' => 'Active'))->find();
         foreach ($collection as $entity) {
             try {
                 $service->fetchFromMailServer($entity);
             } catch (\Exception $e) {}
         }
-        
+
         return true;
     }
 }
