f4ef5fc36f31a975b27c9081b2d89b9c877530e8
yuri <yurak@list.ru>
2015-01-28 17:04:45 +0200

    add tentative event status

--- application/Espo/Modules/Crm/EntryPoints/EventConfirmation.php
+++ application/Espo/Modules/Crm/EntryPoints/EventConfirmation.php
@@ -32,7 +32,7 @@ use \Espo\Core\Exceptions\Error;
 class EventConfirmation extends \Espo\Core\EntryPoints\Base
 {
     public static $authRequired = false;
-    
+
     public function run()
     {
         $uid = $_GET['uid'];
@@ -40,40 +40,42 @@ class EventConfirmation extends \Espo\Core\EntryPoints\Base
         if (empty($uid) || empty($action)) {
             throw new BadRequest();
         }
-        
-        if (!in_array($action, array('accept', 'decline'))) {
+
+        if (!in_array($action, array('accept', 'decline', 'tentative'))) {
             throw new BadRequest();
         }
-                
+
         $uniqueId = $this->getEntityManager()->getRepository('UniqueId')->where(array('name' => $uid))->findOne();
-        
+
         if (!$uniqueId) {
             throw new NotFound();
             return;
         }
-        
+
         $data = $uniqueId->get('data');
-        
+
         $eventType = $data->eventType;
         $eventId = $data->eventId;
         $inviteeType = $data->inviteeType;
         $inviteeId = $data->inviteeId;
         $link = $data->link;
-        
+
         if (!empty($eventType) && !empty($eventId) && !empty($inviteeType) && !empty($inviteeId) && !empty($link)) {
             $event = $this->getEntityManager()->getEntity($eventType, $eventId);
             $invitee = $this->getEntityManager()->getEntity($inviteeType, $inviteeId);
             if ($event && $invitee) {
                 $relDefs = $event->getRelations();
                 $tableName = Util::toUnderscore($relDefs[$link]['relationName']);
-                
+
                 $status = 'None';
                 if ($action == 'accept') {
                     $status = 'Accepted';
                 } else if ($action == 'decline') {
                     $status = 'Declined';
+                } else if ($action == 'tentative') {
+                    $status = 'Tentative';
                 }
-                
+
                 $pdo = $this->getEntityManager()->getPDO();
                 $sql = "
                     UPDATE `{$tableName}` SET status = '{$status}'
@@ -82,14 +84,14 @@ class EventConfirmation extends \Espo\Core\EntryPoints\Base
 
                 $sth = $pdo->prepare($sql);
                 $sth->execute();
-                
+
                 $this->getEntityManager()->getRepository('UniqueId')->remove($uniqueId);
-                
+
                 echo $status;
                 return;
             }
         }
-        
+
         throw new Error();
     }
 }
--- application/Espo/Modules/Crm/Resources/templates/InvitationBody.en_US.tpl
+++ application/Espo/Modules/Crm/Resources/templates/InvitationBody.en_US.tpl
@@ -1,7 +1,7 @@
 <p>Subject: {name}</p>
 <p>Start at: {dateStart}</p>
 <p>
-<a href="{acceptLink}">Accept</a>, <a href="{declineLink}">Decline</a>
+<a href="{acceptLink}">Accept</a>, <a href="{declineLink}">Decline</a>, <a href="{tentativeLink}">Tentative</a>
 </p>
 {#userOnly}
 <p><a href="{url}">Open entry</a></p>
