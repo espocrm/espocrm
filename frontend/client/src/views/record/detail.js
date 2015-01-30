cf3b4b284a02d2cafaceb522727caf6fb7ce94e2
yuri <yurak@list.ru>
2015-01-30 14:47:14 +0200

    record.base

--- frontend/client/src/views/record/detail.js
+++ frontend/client/src/views/record/detail.js
@@ -18,8 +18,8 @@
  * You should have received a copy of the GNU General Public License
  * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
  ************************************************************************/
-Espo.define('Views.Record.Detail', 'View', function (Dep) {
-    
+Espo.define('Views.Record.Detail', 'Views.Record.Base', function (Dep) {
+
     return Dep.extend({
 
         template: 'record.detail',
@@ -40,9 +40,9 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
         buttonsPosition: 'top',
 
         columnCount: 2,
-        
+
         scope: null,
-        
+
         isNew: false,
 
         buttons: [
@@ -57,7 +57,7 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                 style: 'danger',
             },
         ],
-        
+
         buttonsEdit: [
             {
                 name: 'save',
@@ -79,15 +79,15 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
         sideView: 'Record.DetailSide',
 
         bottomView: 'Record.DetailBottom',
-        
+
         editModeEnabled: true,
-        
+
         readOnly: false,
-        
+
         isWide: false,
-        
+
         dependencyDefs: {},
-        
+
         events: {
             'click .button-container button': function (e) {
                 var $target = $(e.currentTarget);
@@ -101,7 +101,7 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                 }
             }
         },
-        
+
         actionEdit: function () {
             if (this.editModeEnabled) {
                 this.setEditMode();
@@ -110,23 +110,23 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                 this.getRouter().navigate('#' + this.scope + '/edit/' + this.model.id, {trigger: true});
             }
         },
-        
+
         actionDelete: function () {
             this.delete();
         },
-        
+
         actionSave: function () {
             if (this.save()) {
                 this.setDetailMode();
                 $(window).scrollTop(0)
             }
         },
-        
+
         actionCancelEdit: function () {
             this.cancelEdit();
             $(window).scrollTop(0);
         },
-        
+
         afterRender: function () {
             var $container = this.$el.find('.detail-button-container');
             var stickTop = 62;
@@ -134,7 +134,7 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
             var $block = $('<div>').css('height', blockHeihgt + 'px').html('&nbsp;').hide().insertAfter($container);
             var $record = this.getView('record').$el;
             var $window = $(window);
-            
+
             $window.on('scroll.detail', function (e) {
                 if ($(window.document).width() < 758) {
                     $container.removeClass('stick-sub');
@@ -142,16 +142,16 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                     $container.show();
                     return;
                 }
-                            
+
                 var edge = $record.position().top + $record.outerHeight(true);
                 var scrollTop = $window.scrollTop();
-                
+
                 if (scrollTop < edge) {
                     if (scrollTop > stickTop) {
                         if (!$container.hasClass('stick-sub')) {
                             $container.addClass('stick-sub');
                             $block.show();
-                                
+
                             var $p = $('.popover');
                             $p.each(function (i, el) {
                                 $el = $(el);
@@ -162,7 +162,7 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                         if ($container.hasClass('stick-sub')) {
                             $container.removeClass('stick-sub');
                             $block.hide();
-                            
+
                             var $p = $('.popover');
                             $p.each(function (i, el) {
                                 $el = $(el);
@@ -176,9 +176,9 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                     $block.show();
                 }
             }.bind(this));
-            
+
             var fields = this.getFields();
-            
+
             var fieldInEditMode = null;
             for (var field in fields) {
                 var fieldView = fields[field];
@@ -190,11 +190,11 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                 }.bind(this));
             }
         },
-        
+
         setEditMode: function () {
             this.$el.find('.record-buttons').addClass('hidden');
             this.$el.find('.edit-buttons').removeClass('hidden');
-            
+
             var fields = this.getFields();
             for (var field in fields) {
                 var fieldView = fields[field];
@@ -208,11 +208,11 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                 }
             }
         },
-        
+
         setDetailMode: function () {
             this.$el.find('.edit-buttons').addClass('hidden');
             this.$el.find('.record-buttons').removeClass('hidden');
-            
+
             var fields = this.getFields();
             for (var field in fields) {
                 var fieldView = fields[field];
@@ -222,7 +222,7 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                 }
             }
         },
-        
+
         cancelEdit: function () {
             this.model.set(this.attributes);
             this.setDetailMode();
@@ -250,24 +250,6 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
             }
         },
 
-        hideField: function (name) {
-            this.$el.find('label.field-label-' + name).addClass('hide');
-            this.$el.find('div.field-' + name).addClass('hide');
-            var view = this.getFieldView(name);
-            if (view) {
-                view.enabled = false;
-            }
-        },
-
-        showField: function (name) {
-            this.$el.find('label.field-label-' + name).removeClass('hide');
-            this.$el.find('div.field-' + name).removeClass('hide');
-            var view = this.getFieldView(name);
-            if (view) {
-                view.enabled = true;
-            }
-        },
-        
         getFields: function () {
             var fields = _.clone(this.getView('record').nestedViews);
             if (this.hasView('side')) {
@@ -300,9 +282,9 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
 
         init: function () {
             this.scope = this.model.name;
-            
+
             this.layoutName = this.options.layoutName || this.layoutName;
-            
+
             this.type = this.options.type || this.type;
             this.buttons = this.options.buttons || this.buttons;
             this.buttons = _.clone(this.buttons);
@@ -327,186 +309,80 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
             if ('buttonsPosition' in this.options) {
                 this.buttonsPosition = this.options.buttonsPosition;
             }
-            
+
             if ('isWide' in this.options) {
                 this.isWide = this.options.isWide;
             }
-            
+
             if ('sideView' in this.options) {
                 this.sideView = this.options.sideView;
             }
-            
+
             if (this.model.isNew()) {
                 this.isNew = true;
                 this.removeButton('delete');
             }
-            
+
             if (!this.getAcl().checkModel(this.model, 'edit')) {
                 this.readOnly = true;
             }
 
 
             this.manageAccess();
-            
+
             this.attributes = this.model.getClonedAttributes();
 
             this.build();
             this.listenTo(this.model, 'sync', function () {
                 this.attributes = this.model.getClonedAttributes();
             }.bind(this));
-            
-            
+
+            this.dependencyDefs = _.extend(this.getMetadata().get('clientDefs.' + this.model.name + '.formDependency') || {}, this.dependencyDefs);
             this._initDependancy();
         },
-        
-        _initDependancy: function () {
-            this.dependencyDefs = _.extend(this.getMetadata().get('clientDefs.' + this.model.name + '.formDependency') || {}, this.dependencyDefs);
-                
-            Object.keys(this.dependencyDefs || {}).forEach(function (attr) {
-                this.listenTo(this.model, 'change:' + attr, function () {
-                    this._handleDependencyAttribute(attr);
-                }, this);
-            }, this);
-            
-            this.on('after:render', function () {
-                this._handleDependencyAttributes();
-            }, this);
-        },
-        
-        validate: function () {
-            var notValid = false;
-            var fields = this.getFields();
-            for (var i in fields) {
-                if (fields[i].mode == 'edit') {
-                    if (fields[i].enabled && !fields[i].readOnly) {
-                        notValid = fields[i].validate() || notValid;
-                    }
-                }
-            };
-            return notValid
-        },
 
-        save: function (callback) {
-            this.disableButtons();
-            var data = this.fetch();
 
-            var self = this;
-            var model = this.model;
-
-            var attrsInitialy = this.attributes;
-            
-            var attrsBefore = this.model.getClonedAttributes();
-            
-            data = _.extend(Espo.Utils.cloneDeep(attrsBefore), data);
-                        
-            var attrs = false;
-            if (model.isNew()) {
-                attrs = data;
+        afterSave: function () {
+            if (this.isNew) {
+                this.notify('Created', 'success');
             } else {
-                for (var attr in data) {
-                    if (_.isEqual(attrsInitialy[attr], data[attr])) {
-                        continue;
-                    }
-                    (attrs || (attrs = {}))[attr] =    data[attr];
-                }
+                this.notify('Saved', 'success');
             }
-            
-            if (!attrs) {
-                var msg = this.translate('notModified', 'messages');
-                Espo.Ui.warning(msg, 'warning');
-                this.trigger('cancel:save');
-                this.enableButtons();
-                return true;
-            }
-            
-            model.set(attrs, {silent: true});
+            this.enableButtons();
+        },
 
-            if (this.validate()) {
-                this.notify('Not valid', 'error');
-                model.attributes = attrsBefore;
-                this.trigger('cancel:save');
-                this.enableButtons();
-                return;
-            }
-
+        beforeSave: function () {
             this.notify('Saving...');
-            
-            this.trigger('before:save');
-
-            model.save(attrs, {
-                success: function () {
-                    if (self.isNew) {
-                        self.notify('Created', 'success');
-                        self.isNew = false;
-                    } else {
-                        self.notify('Saved', 'success');
-                    }
-                    self.trigger('after:save');
-                    model.trigger('after:save');
-                    if (!callback) {
-                        self.exit('save');
-                    } else {
-                        callback(self);
-                    }
-                    self.enableButtons();
-                },
-                error: function (e, xhr) {
-                
-                    var r = xhr.getAllResponseHeaders();
-                    
-                    var response = null;
-                    
-                    if (xhr.status == 409) {
-                        var header = xhr.getResponseHeader('X-Status-Reason');
-                        try {
-                            var response = JSON.parse(header);
-                        } catch (e) {
-                            console.error('Error while parsing response');
-                        }
-                    }
-                    
-                    if (response) {
-                        if (response.reason == 'Duplicate') {
-                            xhr.errorIsHandled = true;
-                            self.notify(false);
-                            self.showDuplicate(response.data);
-                        }
-                    }
-                    
-                    model.attributes = attrsBefore;
-                    self.trigger('cancel:save');
-                    self.enableButtons();
-                },
-                patch: !model.isNew()
-            });
-            return true;
         },
 
-        fetch: function () {
-            var data = {};
-            var fields = this.getFields();
-            for (var i in fields) {
-                if (fields[i].mode == 'edit') {
-                    if (fields[i].enabled && !fields[i].readOnly) {
-                        _.extend(data, fields[i].fetch());
-                    }
-                }
-            };
-            return data;
+        afterSaveError: function () {
+            this.enableButtons();
         },
-        
+
+        afterNotModified: function () {
+            var msg = this.translate('notModified', 'messages');
+            Espo.Ui.warning(msg, 'warning');
+            this.enableButtons();
+        },
+
+        afterNotValid: function () {
+            this.notify('Not valid', 'error');
+            this.enableButtons();
+        },
+
         showDuplicate: function (duplicates) {
+            this.notify(false);
             this.createView('duplicate', 'Modals.Duplicate', {
                 scope: this.scope,
                 duplicates: duplicates,
             }, function (view) {
                 view.render();
-                
+
                 this.listenToOnce(view, 'save', function () {
                     this.model.set('forceDuplicate', true);
                     this.actionSave();
                 }.bind(this));
-            
+
             }.bind(this));
         },
 
@@ -549,12 +425,12 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
 
                     for (var j in simplifiedLayout[p].rows[i]) {
                         var cellDefs = simplifiedLayout[p].rows[i][j];
-                        
+
                         if (cellDefs == false) {
                             row.push(false);
                             continue;
                         }
-                        
+
                         if (!cellDefs.name) {
                             continue;
                         }
@@ -562,12 +438,12 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                         var type = cellDefs.type || this.model.getFieldType(cellDefs.name) || 'base';
                         var viewName = cellDefs.view || this.model.getFieldParam(cellDefs.name, 'view') || this.getFieldManager().getViewName(type);
 
-                        
+
                         var readOnly = this.readOnly;
                         if (!readOnly) {
                             readOnly = cellDefs.readOnly || false;
                         }
-                        
+
                         var cell = {
                             name: cellDefs.name,
                             view: viewName,
@@ -600,7 +476,7 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
             }
             return layout
         },
-        
+
         getGridLayout: function (callback) {
             if (this.gridLayout !== null) {
                 callback(this.gridLayout);
@@ -620,7 +496,7 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
             this.waitForView('record');
 
             var self = this;
-            
+
             if (this.sideView) {
                 this.createView('side', this.sideView, {
                     model: this.model,
@@ -628,7 +504,7 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
                     readOnly: this.readOnly
                 });
             }
-            
+
             this.getGridLayout(function (layout) {
                 this.createView('record', 'Base', {
                     model: this.model,
@@ -675,70 +551,10 @@ Espo.define('Views.Record.Detail', 'View', function (Dep) {
             }
             this.getRouter().navigate(url, {trigger: true});
         },
-        
-        _handleDependencyAttributes: function () {
-            Object.keys(this.dependencyDefs || {}).forEach(function (attr) {
-                this._handleDependencyAttribute(attr);
-            }, this);
-        },
-        
-        _handleDependencyAttribute: function (attr) {
-            var data = this.dependencyDefs[attr];
-            var value = this.model.get(attr);
-            if (value in (data.map || {})) {
-                (data.map[value] || []).forEach(function (item) {
-                    this._doDependencyAction(item);
-                }, this);
-            } else {
-                if ('default' in data) {
-                    (data.default || []).forEach(function (item) {
-                        this._doDependencyAction(item);
-                    }, this);
-                }
-            }
-        },
-        
-        _doDependencyAction: function (data) {
-            var action = data.action;
-            
-            var methodName = 'dependencyAction' + Espo.Utils.upperCaseFirst(action);
-            if (methodName in this && typeof this.methodName == 'function') {
-                this.methodName(data);
-                return;
-            }
-            
-            var fields = data.fields || [];
-            
-            switch (action) {
-                case 'hide':
-                    fields.forEach(function (field) {
-                        this.hideField(field);
-                    }, this);
-                    break;
-                case 'show':
-                    fields.forEach(function (field) {
-                        this.showField(field);
-                    }, this);
-                    break;
-                case 'setRequired':
-                    fields.forEach(function (field) {
-                        var fieldView = this.getFieldView(field);
-                        if (fieldView) {
-                            fieldView.setRequired();
-                        }
-                    }, this);
-                    break;
-                case 'setNotRequired':
-                    fields.forEach(function (field) {
-                        var fieldView = this.getFieldView(field);
-                        if (fieldView) {
-                            fieldView.setRequired();
-                        }
-                    }, this);
-                    break;
-            }
-        },
-        
+
+        createField: function () {
+        }
+
     });
 
 });
