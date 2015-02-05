d49ee3c187099872939cdf71bf261aece43924c9
yuri <yurak@list.ru>
2015-02-05 16:31:39 +0200

    fix select and create

--- frontend/client/src/views/modals/select-records.js
+++ frontend/client/src/views/modals/select-records.js
@@ -17,48 +17,48 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
- ************************************************************************/ 
+ ************************************************************************/
 
 Espo.define('Views.Modals.SelectRecords', 'Views.Modal', function (Dep) {
 
     return Dep.extend({
-    
-        cssName: 'select-modal',    
-        
+
+        cssName: 'select-modal',
+
         multiple: false,
-        
+
         header: false,
-        
+
         template: 'modals.select-records',
-        
+
         createButton: true,
-        
+
         data: function () {
             return {
                 createButton: this.createButton && this.getAcl().check(this.scope, 'edit')
             };
         },
-        
+
         events: {
             'click button[data-action="create"]': function () {
-                this.create();
+        this.create();
             },
             'click .list a': function (e) {
                 e.preventDefault();
             }
         },
-        
-        setup: function () {        
+
+        setup: function () {
             this.filters = this.options.filters || {};
-        
+
             if ('multiple' in this.options) {
                 this.multiple = this.options.multiple;
             }
-            
+
             if ('createButton' in this.options) {
                 this.createButton = this.options.createButton;
             }
-            
+
             this.buttons = [
                 {
                     name: 'cancel',
@@ -68,7 +68,7 @@ Espo.define('Views.Modals.SelectRecords', 'Views.Modal', function (Dep) {
                     }
                 } 
             ];
-            
+
             if (this.multiple) {
                 this.buttons.unshift({
                     name: 'select',
@@ -83,62 +83,62 @@ Espo.define('Views.Modals.SelectRecords', 'Views.Modal', function (Dep) {
                     }.bind(this),
                 });
             }
-            
+
             this.scope = this.options.scope;
-            
+
             if (['User', 'Team', 'Acl'].indexOf(this.scope) !== -1) {
                 this.createButton = false;
             }
-                            
-            this.header = this.getLanguage().translate(this.scope, 'scopeNamesPlural');                
-            
+
+            this.header = this.getLanguage().translate(this.scope, 'scopeNamesPlural');
+
             this.waitForView('list');
-            this.waitForView('search');    
-            
-            Espo.require('SearchManager', function (SearchManager) {                                    
+            this.waitForView('search');
+
+            Espo.require('SearchManager', function (SearchManager) {
                 this.getCollectionFactory().create(this.scope, function (collection) {
-                        
+
                     collection.maxSize = this.getConfig().get('recordsPerPageSmall') || 5;
-                    
-                    var searchManager = new SearchManager(collection, 'listSelect', null, this.getDateTime());                    
+
+                    var searchManager = new SearchManager(collection, 'listSelect', null, this.getDateTime());
                     searchManager.setAdvanced(this.filters);
                     collection.where = searchManager.getWhere();
-                                                
+
                     this.createView('search', 'Record.Search', {
                         collection: collection,
                         el: this.containerSelector + ' .search-container',
                         searchManager: searchManager,
                         disableSavePreset: true,
                     });
-                    
-                    this.listenToOnce(collection, 'sync', function () {                    
+
+                    this.listenToOnce(collection, 'sync', function () {
                         this.createView('list', 'Record.List', {
                             collection: collection,
                             el: this.containerSelector + ' .list-container',
-                            selectable: true,                            
+                            selectable: true,
                             checkboxes: this.multiple,
                             actions: false,
                             rowActionsView: false,
                             type: 'listSmall',
-                            searchManager: searchManager,                
+                            searchManager: searchManager,
                         }, function (list) {
                             list.once('select', function (model) {
-                                this.trigger('select', model);                            
-                                this.close();    
+                                this.trigger('select', model);
+                                this.close();
                             }.bind(this));
                         }.bind(this));
-                    
+
                     }.bind(this));
-                    
-                    collection.fetch();                    
-                    
-                }.bind(this));                    
+
+                    collection.fetch();
+
+                }.bind(this));
             }.bind(this));
         },
-        
+
         create: function () {
             var self = this;
-            
+
             this.notify('Loading...');
             this.createView('quickCreate', 'Modals.Edit', {
                 scope: this.scope,
@@ -147,8 +147,8 @@ Espo.define('Views.Modals.SelectRecords', 'Views.Modal', function (Dep) {
                 view.once('after:render', function () {
                     self.notify(false);
                 });
-                view.render();                
-        
+                view.render();
+
                 self.listenToOnce(view, 'leave', function () {
                     view.close();
                     self.close();
@@ -157,7 +157,7 @@ Espo.define('Views.Modals.SelectRecords', 'Views.Modal', function (Dep) {
                     self.trigger('select', model);
                     view.close();
                     self.close();
-                }.bind(this));                        
+                }.bind(this));
             });
         },
     });
