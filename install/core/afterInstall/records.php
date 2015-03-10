87ce26f3cd04fa6b1ae14f8986b66ad3dad34449
yuri <yurak@list.ru>
2015-03-10 16:24:53 +0200

    fix default jobs scheduling value

--- install/core/afterInstall/records.php
+++ install/core/afterInstall/records.php
@@ -34,19 +34,19 @@ return array(
 			'name' => 'Check Inbound Emails',
 			'job' => 'CheckInboundEmails',
 			'status' => 'Active',
-			'scheduling' => '/5 * * * *',
+			'scheduling' => '*/5 * * * *',
 		),
 		1 => array(
 			'name' => 'Check Personal Email Accounts',
 			'job' => 'CheckEmailAccounts',
 			'status' => 'Active',
-			'scheduling' => '/10 * * * *',
+			'scheduling' => '*/10 * * * *',
 		),
 		2 => array(
 			'name' => 'Send Email Reminders',
 			'job' => 'SendEmailReminders',
 			'status' => 'Active',
-			'scheduling' => '/2 * * * *',
+			'scheduling' => '*/2 * * * *',
 		),
 		3 => array(
 			'name' => 'Clean-up',
