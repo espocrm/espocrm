843ca2a7c5896f77b7ab49ed8f40f2a961760202
yuri <yurak@list.ru>
2015-03-13 15:32:04 +0200

    fix email address primary pdo quote

--- application/Espo/Repositories/EmailAddress.php
+++ application/Espo/Repositories/EmailAddress.php
@@ -288,7 +288,7 @@ class EmailAddress extends \Espo\Core\ORM\Repositories\RDB
                                 ".$pdo->quote($entity->id).",
                                 ".$pdo->quote($entity->getEntityName()).",
                                 ".$pdo->quote($emailAddress->id).",
-                                ".$pdo->quote($address === $primary)."
+                                ".$pdo->quote((int)($address === $primary))."
                             )
                     ";
                     $sth = $pdo->prepare($query);
