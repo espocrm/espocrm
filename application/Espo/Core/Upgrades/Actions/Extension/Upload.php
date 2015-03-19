cf885cafb47d078f180824513adf61425cedb96e
Taras Machyshyn <taras@letrium.com>
2015-03-19 15:38:20 +0200

    Upgrade improvements

--- application/Espo/Core/Upgrades/Actions/Extension/Upload.php
+++ application/Espo/Core/Upgrades/Actions/Extension/Upload.php
@@ -24,5 +24,8 @@ namespace Espo\Core\Upgrades\Actions\Extension;
 
 class Upload extends \Espo\Core\Upgrades\Actions\Base\Upload
 {
-
+    protected function checkDependencies($dependencyList)
+    {
+        return $this->getHelper()->checkDependencies($dependencyList);
+    }
 }
---
+++ application/Espo/Core/Upgrades/Actions/Helper.php
@@ -0,0 +1,77 @@
+<?php
+/************************************************************************
+ * This file is part of EspoCRM.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2015 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ ************************************************************************/
+
+namespace Espo\Core\Upgrades\Actions;
+
+use Espo\Core\Exceptions\Error;
+
+class Helper
+{
+    private $actionObject;
+
+    public function __construct($actionObject = null)
+    {
+        if (isset($actionObject)) {
+            $this->setActionObject($actionObject);
+        }
+    }
+
+    public function setActionObject(\Espo\Core\Upgrades\Actions\Base $actionObject)
+    {
+        $this->actionObject = $actionObject;
+    }
+
+    protected function getActionObject()
+    {
+        return $this->actionObject;
+    }
+
+    /**
+     * Check dependencies
+     *
+     * @param  array | string $dependencyList
+     *
+     * @return bool
+     */
+    public function checkDependencies($dependencyList)
+    {
+        if (!is_array($dependencyList)) {
+            $dependencyList = (array) $dependencyList;
+        }
+
+        $actionObject = $this->getActionObject();
+
+        foreach ($dependencyList as $extensionName => $extensionVersion) {
+            $dependencyExtensionEntity = $actionObject->getEntityManager()->getRepository('Extension')->where(array(
+                'name' => trim($extensionName),
+                'isInstalled' => true,
+            ))->findOne();
+
+            $errorMessage = 'Dependency Error: The extension "'.$extensionName.'" with version "'.$extensionVersion.'" is missing.';
+            if (!isset($dependencyExtensionEntity) || !$actionObject->checkVersions($extensionVersion, $dependencyExtensionEntity->get('version'), $errorMessage)) {
+                throw new Error($errorMessage);
+            }
+        }
+
+        return true;
+    }
+}
