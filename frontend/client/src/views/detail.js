92e1d4b8e6e355c48335918772da05e007d3e857
yuri <yurak@list.ru>
2015-03-19 17:13:02 +0200

    clearnup

--- frontend/client/src/views/detail.js
+++ frontend/client/src/views/detail.js
@@ -171,10 +171,6 @@ Espo.define('Views.Detail', 'Views.Main', function (Dep) {
                 '<a href="#' + this.model.name + '">' + this.getLanguage().translate(this.model.name, 'scopeNamesPlural') + '</a>',
                 name
             ]);
-
-            var html = '<a href="#' + this.model.name + '">' + this.getLanguage().translate(this.model.name, 'scopeNamesPlural') + '</a>';
-            html += ' &raquo ' + name;
-            return html;
         },
 
         updatePageTitle: function () {
