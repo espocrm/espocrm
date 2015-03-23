9f8c0eb4a20fd6f8d9f1d704cfb48c7cbac86209
yuri <yurak@list.ru>
2015-03-23 16:50:32 +0200

    cleanup

--- frontend/client/modules/crm/src/views/contact/fields/accounts.js
+++ frontend/client/modules/crm/src/views/contact/fields/accounts.js
@@ -17,27 +17,27 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
- ************************************************************************/ 
+ ************************************************************************/
 
 Espo.define('Crm:Views.Contact.Fields.Accounts', 'Views.Fields.LinkMultipleWithRole', function (Dep) {
 
     return Dep.extend({
-    
+
         roleType: 'varchar',
-        
+
         events: {
-            'click [data-action="switchPrimary"]': function (e) {            
-                $target = $(e.currentTarget);                
+            'click [data-action="switchPrimary"]': function (e) {
+                $target = $(e.currentTarget);
                 var id = $target.data('id');
-                
+
                 if (!$target.hasClass('active')) {
-                    this.$el.find('button[data-action="switchPrimary"]').removeClass('active').children().addClass('text-muted');                    
+                    this.$el.find('button[data-action="switchPrimary"]').removeClass('active').children().addClass('text-muted');
                     $target.addClass('active').children().removeClass('text-muted');
                     this.setPrimaryId(id);
                 }
             }
         },
-        
+
         getAttributeList: function () {
             var list = Dep.prototype.getAttributeList.call(this);
             list.push('accountId');
@@ -45,23 +45,23 @@ Espo.define('Crm:Views.Contact.Fields.Accounts', 'Views.Fields.LinkMultipleWithR
             list.push('title');
             return list;
         },
-        
+
         setup: function () {
             Dep.prototype.setup.call(this);
-            
+
             this.primaryIdFieldName = 'accountId';
             this.primaryNameFieldName = 'accountName';
             this.primaryRoleFieldName = 'title';
-            
+
             this.primaryId = this.model.get(this.primaryIdFieldName);
             this.primaryName = this.model.get(this.primaryNameFieldName);
-            
+
             this.listenTo(this.model, 'change:' + this.primaryIdFieldName, function () {
                 this.primaryId = this.model.get(this.primaryIdFieldName);
                 this.primaryName = this.model.get(this.primaryNameFieldName);
             }.bind(this));
         },
-        
+
         setPrimaryId: function (id) {
             this.primaryId = id;
             if (id) {
@@ -69,10 +69,10 @@ Espo.define('Crm:Views.Contact.Fields.Accounts', 'Views.Fields.LinkMultipleWithR
             } else {
                 this.primaryName = null;
             }
-            
+
             this.trigger('change');
         },
-        
+
         renderLinks: function () {
             if (this.primaryId) {
                 this.addLinkHtml(this.primaryId, this.primaryName);
@@ -83,13 +83,13 @@ Espo.define('Crm:Views.Contact.Fields.Accounts', 'Views.Fields.LinkMultipleWithR
                 }
             }, this);
         },
-        
+
         getValueForDisplay: function () {
             if (this.mode == 'detail' || this.mode == 'list') {
                 var names = [];
                 if (this.primaryId) {
                     names.push(this.getDetailLinkHtml(this.primaryId, this.primaryName));
-                }        
+                }
                 this.ids.forEach(function (id) {
                     if (id != this.primaryId) {
                         names.push(this.getDetailLinkHtml(id));
@@ -98,40 +98,40 @@ Espo.define('Crm:Views.Contact.Fields.Accounts', 'Views.Fields.LinkMultipleWithR
                 return names.join('');
             }
         },
-        
+
         deleteLink: function (id) {
             if (id == this.primaryId) {
                 this.setPrimaryId(null);
             }
             Dep.prototype.deleteLink.call(this, id);
         },
-        
+
         deleteLinkHtml: function (id) {
             Dep.prototype.deleteLinkHtml.call(this, id);
             this.managePrimaryButton();
         },
-        
+
         addLinkHtml: function (id, name) {
             if (this.mode == 'search') {
                 return Dep.prototype.addLinkHtml.call(this, id, name);
             }
-                
+
             $el = Dep.prototype.addLinkHtml.call(this, id, name);
-            
-            var isPrimary = (id == this.primaryId); 
-            
-            var iconHtml = '<span class="glyphicon glyphicon-star ' + (!isPrimary ? 'text-muted' : '') + '"></span>';            
+
+            var isPrimary = (id == this.primaryId);
+
+            var iconHtml = '<span class="glyphicon glyphicon-star ' + (!isPrimary ? 'text-muted' : '') + '"></span>';
             var title = this.translate('Primary');
-            
+
             var $primary = $('<button type="button" class="btn btn-link btn-sm pull-right hidden" title="'+title+'" data-action="switchPrimary" data-id="'+id+'">'+iconHtml+'</button>');
             $primary.insertAfter($el.children().first().children().first());
-            this.managePrimaryButton();        
+            this.managePrimaryButton();
         },
-        
+
         afterRender: function () {
             Dep.prototype.afterRender.call(this);
         },
-        
+
         managePrimaryButton: function () {
             var $primary = this.$el.find('button[data-action="switchPrimary"]');
             if ($primary.size() > 1) {
@@ -139,23 +139,23 @@ Espo.define('Crm:Views.Contact.Fields.Accounts', 'Views.Fields.LinkMultipleWithR
             } else {
                 $primary.addClass('hidden');
             }
-            
+
             if ($primary.filter('.active').size() == 0) {
                 var $first = $primary.first();
                 if ($first.size()) {
                     $first.addClass('active').children().removeClass('text-muted');
                     this.setPrimaryId($first.data('id'));
-                }                
+                }
             }
         },
-        
+
         fetch: function () {
             var data = Dep.prototype.fetch.call(this);
-            
-            data[this.primaryIdFieldName] = this.primaryId;            
-            data[this.primaryNameFieldName] = this.primaryName;            
+
+            data[this.primaryIdFieldName] = this.primaryId;
+            data[this.primaryNameFieldName] = this.primaryName;
             data[this.primaryRoleFieldName] = (this.columns[this.primaryId] || {}).role || '';
-            
+
             return data;
         },
 
