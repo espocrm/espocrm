bf6778770e60adcaffddb741fc9835d7ce2513fe
yuri <yurak@list.ru>
2015-03-27 16:23:40 +0200

    fix label

--- frontend/client/modules/crm/src/views/account/fields/shipping-address.js
+++ frontend/client/modules/crm/src/views/account/fields/shipping-address.js
@@ -17,36 +17,36 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
- ************************************************************************/ 
+ ************************************************************************/
 
 Espo.define('Crm:Views.Account.Fields.ShippingAddress', 'Views.Fields.Address', function (Dep) {
 
     return Dep.extend({
-    
+
         copyFrom: 'billingAddress',
-        
+
         afterRender: function () {
             Dep.prototype.afterRender.call(this);
-            
+
             if (this.mode == 'edit') {
-                var label = this.translate('Copy Billing', 'labels', 'Accounts');
+                var label = this.translate('Copy Billing', 'labels', 'Account');
                 $btn = $('<button class="btn btn-default btn-sm">' + label + '</button>').on('click', function () {
                     this.copy(this.copyFrom);
                 }.bind(this));
                 this.$el.append($btn);
             }
         },
-        
+
         copy: function (fieldFrom) {
             var attrList = Object.keys(this.getMetadata().get('fields.address.fields')).forEach(function (attr) {
                 destField = this.name + Espo.Utils.upperCaseFirst(attr);
                 sourceField = fieldFrom + Espo.Utils.upperCaseFirst(attr);
-                
+
                 this.model.set(destField, this.model.get(sourceField));
             }, this);
-            
+
         },
-        
+
     });
 });
 
