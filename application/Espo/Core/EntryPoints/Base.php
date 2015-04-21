b42b2760548813472f0a363cabcbaeec3b31f6a4
yuri <yurak@list.ru>
2015-04-21 14:39:07 +0300

    cleanup

--- application/Espo/Core/EntryPoints/Base.php
+++ application/Espo/Core/EntryPoints/Base.php
@@ -18,7 +18,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
- ************************************************************************/ 
+ ************************************************************************/
 
 namespace Espo\Core\EntryPoints;
 
@@ -29,50 +29,50 @@ use \Espo\Core\Exceptions\Forbidden;
 abstract class Base
 {
     private $container;
-    
+
     public static $authRequired = true;
-    
+
     protected function getContainer()
     {
         return $this->container;
     }
-    
+
     protected function getUser()
     {
         return $this->getContainer()->get('user');
     }
-    
+
     protected function getAcl()
     {
         return $this->getContainer()->get('acl');
     }
-    
+
     protected function getEntityManager()
     {
         return $this->getContainer()->get('entityManager');
     }
-    
+
     protected function getServiceFactory()
     {
         return $this->getContainer()->get('serviceFactory');
-    }    
-    
+    }
+
     protected function getConfig()
     {
         return $this->getContainer()->get('config');
     }
-    
+
     protected function getMetadata()
     {
         return $this->getContainer()->get('metadata');
-    }    
-    
+    }
+
     public function __construct(Container $container)
     {
         $this->container = $container;
     }
-    
-    abstract public function run();    
+
+    abstract public function run();
 
 }
 
