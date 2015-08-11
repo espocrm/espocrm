d2bc49755d101cfd8ddb0f28292d5d007bbbf1a4
yuri <yurak@list.ru>
2015-08-11 11:52:07 +0300

    email address autocomplete 2

--- frontend/client/src/views/fields/email.js
+++ frontend/client/src/views/fields/email.js
@@ -188,6 +188,43 @@ Espo.define('Views.Fields.Email', 'Views.Fields.Base', function (Dep) {
             Dep.prototype.afterRender.call(this);
             this.manageButtonsVisibility();
             this.manageAddButton();
+
+
+            if (this.mode == 'search') {
+                this.$element.autocomplete({
+                    serviceUrl: function (q) {
+                        return 'EmailAddress/action/searchInAddressBook?limit=5';
+                    }.bind(this),
+                    paramName: 'q',
+                    minChars: 1,
+                    autoSelectFirst: true,
+                    formatResult: function (suggestion) {
+                        return suggestion.name + ' &#60;' + suggestion.id + '&#62;';
+                    },
+                    transformResult: function (response) {
+                        var response = JSON.parse(response);
+                        var list = [];
+                        response.forEach(function(item) {
+                            list.push({
+                                id: item.emailAddress,
+                                name: item.entityName,
+                                emailAddress: item.emailAddress,
+                                entityId: item.entityId,
+                                entityName: item.entityName,
+                                entityType: item.entityType,
+                                data: item.emailAddress,
+                                value: item.emailAddress
+                            });
+                        }, this);
+                        return {
+                            suggestions: list
+                        };
+                    }.bind(this),
+                    onSelect: function (s) {
+                        this.$element.val(s.emailAddress);
+                    }.bind(this)
+                });
+            }
         },
 
         manageAddButton: function () {
