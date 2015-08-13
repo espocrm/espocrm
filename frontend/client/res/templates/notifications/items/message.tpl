6fe7853eb78dace5cc379cb40d97a39fff818552
yuri <yurak@list.ru>
2015-08-13 09:44:55 +0300

    added new noification type message

---
+++ frontend/client/res/templates/notifications/items/message.tpl
@@ -0,0 +1,16 @@
+
+<div class="stream-head-container">
+    <div class="pull-left">
+        {{{avatar}}}
+    </div>
+    <div class="stream-head-text-container">
+        <span class="{{style}}">
+            {{{message}}}
+        </span>
+    </div>
+</div>
+
+<div class="stream-date-container">
+    <span class="text-muted small">{{{createdAt}}}</span>
+</div>
+
---
+++ frontend/client/src/views/notifications/items/message.js
@@ -0,0 +1,53 @@
+/************************************************************************
+ * This file is part of EspoCRM.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2015 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ ************************************************************************/
+
+Espo.define('views/notifications/items/message', 'views/notifications/notification', function (Dep) {
+
+    return Dep.extend({
+
+        template: 'notifications/items/message',
+
+        data: function () {
+            return _.extend({
+                style: this.style,
+            }, Dep.prototype.data.call(this));
+        },
+
+        setup: function () {
+            var data = this.model.get('data') || {};
+
+            this.style = data.style || 'text-muted';
+
+            this.messageTemplate = this.model.get('message') || data.message || '';
+
+            this.userId = data.userId;
+
+            this.messageData['entityType'] = Espo.Utils.upperCaseFirst((this.translate(data.entityType, 'scopeNames') || '').toLowerCase());
+
+            this.messageData['user'] = '<a href="#User/view/' + data.userId + '">' + data.userName + '</a>';
+            this.messageData['entity'] = '<a href="#'+data.entityType+'/view/' + data.entityId + '">' + data.entityName + '</a>';
+
+            this.createMessage();
+        }
+
+    });
+});
+
