0b547c26a2256a1aabc8a53f240dcbe9ea04de39
yuri <yurak@list.ru>
2015-08-20 13:17:30 +0300

    fix jobs

--- frontend/client/src/views/admin/job/record/list.js
+++ frontend/client/src/views/admin/job/record/list.js
@@ -23,7 +23,7 @@ Espo.define('views/admin/job/record/list', 'views/record/list', function (Dep) {
 
     return Dep.extend({
 
-        rowActionsView: 'record/row-actions/view-and-remove',
+        rowActionsView: 'views/record/row-actions/view-and-remove',
 
         massActionList: ['remove'],
 
