63a61349ea02991afa364048811b60860bb0082b
yuri <yurak@list.ru>
2015-08-20 13:28:37 +0300

    job name 3

--- frontend/client/src/views/admin/job/fields/name.js
+++ frontend/client/src/views/admin/job/fields/name.js
@@ -24,7 +24,7 @@ Espo.define('views/admin/job/fields/name', 'views/fields/varchar', function (Dep
     return Dep.extend({
 
         getValueForDisplay: function () {
-            if (this.mode == 'list') {
+            if (this.mode == 'list' || this.mode == 'detail') {
                 if (!this.model.get('name')) {
                     return this.model.get('serviceName') + ': ' + this.model.get('method');
                 } else {
