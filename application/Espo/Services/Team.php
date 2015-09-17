bd420ad963bde058970e1c0144cbdb233efb1735
yuri <yurak@list.ru>
2015-09-17 12:18:12 +0300

    clear roles cache on team change

--- application/Espo/Services/Team.php
+++ application/Espo/Services/Team.php
@@ -24,6 +24,11 @@ namespace Espo\Services;
 
 class Team extends Record
 {
+    protected function init()
+    {
+        $this->addDependency('fileManager');
+    }
+
     protected $linkSelectParams = array(
         'users' => array(
             'additionalColumns' => array(
@@ -31,5 +36,23 @@ class Team extends Record
             )
         )
     );
+
+    protected function getFileManager()
+    {
+        return $this->getInjection('fileManager');
+    }
+
+    public function afterUpdate(Entity $entity, array $data)
+    {
+        parent::afterUpdate($entity, $data);
+        if (array_key_exists('rolesIds', $data)) {
+            $this->clearRolesCache();
+        }
+    }
+
+    protected function clearRolesCache($id)
+    {
+        $this->getFileManager()->removeInDir('data/cache/application/acl');
+    }
 }
 
