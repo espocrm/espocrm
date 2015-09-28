10c51dc46e23bc547b199dde6b5524114d64f209
yuri <yurak@list.ru>
2015-09-28 10:55:58 +0300

    target lists panels

---
+++ frontend/client/src/views/record/row-actions/relationship-view-and-unlink.js
@@ -0,0 +1,49 @@
+/************************************************************************
+ * This file is part of EspoCRM.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2015 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ ************************************************************************/
+
+Espo.define('views/record/row-actions/relationship-view-and-unlink', 'views/record/row-actions/relationship', function (Dep) {
+
+    return Dep.extend({
+
+        getActionList: function () {
+            var list = [{
+                action: 'viewRelated',
+                label: 'View',
+                data: {
+                    id: this.model.id
+                }
+            }];
+            if (this.options.acl.edit) {
+                list.push({
+                    action: 'unlinkRelated',
+                    label: 'Unlink',
+                    data: {
+                        id: this.model.id
+                    }
+                });
+            }
+            return list;
+        },
+
+    });
+
+});
+
