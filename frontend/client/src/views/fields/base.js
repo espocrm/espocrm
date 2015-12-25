a388638a91c9da2cdf316ab0a7d10a1c285b8a37
yuri <yurak@list.ru>
2015-12-25 15:52:30 +0200

    fix field readOnly

--- frontend/client/src/views/fields/base.js
+++ frontend/client/src/views/fields/base.js
@@ -192,7 +192,7 @@ Espo.define('views/fields/base', 'view', function (Dep) {
 
             this.mode = this.options.mode || this.mode;
 
-            this.readOnly = this.readOnly || this.params.readOnly;
+            this.readOnly = this.readOnly || this.params.readOnly || this.model.getFieldParam(this.name, 'readOnly');
             this.readOnlyLocked = this.options.readOnlyLocked || this.readOnly;
             this.inlineEditDisabled = this.options.inlineEditDisabled || this.params.inlineEditDisabled || this.inlineEditDisabled;
             this.readOnly = this.readOnlyLocked || this.options.readOnly || false;
