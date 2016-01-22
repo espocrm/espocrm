bb11a26816dea11446c8d9a0e55f69695d8b752e
yuri <yurak@list.ru>
2016-01-22 16:01:17 +0200

    role: clear cache on team change

--- application/Espo/Services/Team.php
+++ application/Espo/Services/Team.php
@@ -61,5 +61,38 @@ class Team extends Record
     {
         $this->getFileManager()->removeInDir('data/cache/application/acl');
     }
+
+    public function linkEntity($id, $link, $foreignId)
+    {
+        $result = parent::linkEntity($id, $link, $foreignId);
+
+        if ($link === 'users') {
+            $this->getFileManager()->removeFile('data/cache/application/acl/' . $foreignId . '.php');
+        }
+
+        return $result;
+    }
+
+    public function unlinkEntity($id, $link, $foreignId)
+    {
+        $result = parent::unlinkEntity($id, $link, $foreignId);
+
+        if ($link === 'users') {
+            $this->getFileManager()->removeFile('data/cache/application/acl/' . $foreignId . '.php');
+        }
+
+        return $result;
+    }
+
+    public function linkEntityMass($id, $link, $where)
+    {
+        $result = parent::linkEntityMass($id, $link, $where);
+
+        if ($link === 'users') {
+            $this->getFileManager()->removeInDir('data/cache/application/acl');
+        }
+
+        return $result;
+    }
 }
 
