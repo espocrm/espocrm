e39a91bde664dc1615c56aee3d22324e74f10a2a
yuri <yurak@list.ru>
2016-02-22 12:42:09 +0200

    fix view naming

--- client/modules/crm/src/views/opportunity/fields/stage.js
+++ client/modules/crm/src/views/opportunity/fields/stage.js
@@ -24,17 +24,17 @@
  *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
- ************************************************************************/ 
+ ************************************************************************/
 
-Espo.define('Crm:Views.Opportunity.Fields.Stage', 'Views.Fields.Enum', function (Dep) {
+Espo.define('crm:views/opportunity/fields/stage', 'views/fields/enum', function (Dep) {
 
     return Dep.extend({
-    
-        listTemplate: 'fields.enum-styled.detail',
-        
-        detailTemplate: 'fields.enum-styled.detail',
-        
-        data: function () {        
+
+        listTemplate: 'fields/enum-styled/detail',
+
+        detailTemplate: 'fields/enum-styled/detail',
+
+        data: function () {
             var style = 'default';
             var stage = this.model.get('stage');
             if (stage == 'Closed Won') {
@@ -49,11 +49,11 @@ Espo.define('Crm:Views.Opportunity.Fields.Stage', 'Views.Fields.Enum', function 
 
         setup: function () {
             Dep.prototype.setup.call(this);
-            
+
             this.probabilityMap = this.getMetadata().get('entityDefs.Opportunity.probabilityMap') || {};
-            
+
             if (this.mode != 'list') {
-                if (!this.model.has('probability') && this.model.has('stage')) {                    
+                if (!this.model.has('probability') && this.model.has('stage')) {
                     this.model.set('probability', this.probabilityMap[this.model.get('stage')]);
                 }
 
