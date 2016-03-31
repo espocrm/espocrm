08b5f09c554af6633ee15b3a1a26492d266f6c75
yuri <yurak@list.ru>
2016-03-31 11:21:46 +0300

    fix list modal

--- client/src/views/record/list.js
+++ client/src/views/record/list.js
@@ -935,6 +935,10 @@ Espo.define('views/record/list', 'view', function (Dep) {
                         Espo.Ui.notify(false);
                     });
                     view.render();
+
+                    this.listenToOnce(view, 'remove', function () {
+                        this.clearView('modal');
+                    }, this);
                     this.listenToOnce(view, 'after:save', function (m) {
                         var model = this.collection.get(m.id);
                         if (model) {
