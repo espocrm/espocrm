a92c294255b54e5977f617c5b39cb0d67f6a8f8a
yuri <yurak@list.ru>
2016-10-05 12:37:38 +0300

    fix layout manager reset to default

--- client/src/views/admin/layouts/filters.js
+++ client/src/views/admin/layouts/filters.js
@@ -40,7 +40,12 @@ Espo.define('views/admin/layouts/filters', 'views/admin/layouts/rows', function 
             Dep.prototype.setup.call(this);
 
             this.wait(true);
+            this.loadLayout(function () {
+                this.wait(false);
+            }.bind(this));
+        },
 
+        loadLayout: function (callback) {
             this.getModelFactory().create(this.scope, function (model) {
                 this.getHelper().layoutManager.get(this.scope, this.type, function (layout) {
 
@@ -80,7 +85,7 @@ Espo.define('views/admin/layouts/filters', 'views/admin/layouts/rows', function 
                         this.rowLayout[i].label = this.getLanguage().translate(this.rowLayout[i].name, 'fields', this.scope);
                     }
 
-                    this.wait(false);
+                    callback();
                 }.bind(this), false);
             }.bind(this));
         },
