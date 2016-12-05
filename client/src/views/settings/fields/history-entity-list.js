2502cad0c1ab03e14bd5a462ad090b6aab61a83e
yuri <yurak@list.ru>
2016-12-05 14:20:50 +0200

    settings activityEntityList no emails

--- client/src/views/settings/fields/history-entity-list.js
+++ client/src/views/settings/fields/history-entity-list.js
@@ -25,7 +25,7 @@
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
-Espo.define('views/settings/fields/activities-entity-list', 'views/fields/entity-type-list', function (Dep) {
+Espo.define('views/settings/fields/history-entity-list', 'views/fields/entity-type-list', function (Dep) {
 
     return Dep.extend({
 
@@ -34,7 +34,6 @@ Espo.define('views/settings/fields/activities-entity-list', 'views/fields/entity
             Dep.prototype.setupOptions.call(this);
 
             this.params.options = this.params.options.filter(function (scope) {
-                if (scope === 'Email') return;
                 if (this.getMetadata().get('scopes.' + scope + '.disabled')) return;
                 if (!this.getMetadata().get('scopes.' + scope + '.object')) return;
                 if (!this.getMetadata().get('scopes.' + scope + '.activity')) return;
