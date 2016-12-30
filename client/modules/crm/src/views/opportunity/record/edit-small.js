62894366e60c133b061cf7c38220839c3cc2ceef
yuri <yurak@list.ru>
2016-12-30 15:50:52 +0200

    fix opp stage change

--- client/modules/crm/src/views/opportunity/record/edit-small.js
+++ client/modules/crm/src/views/opportunity/record/edit-small.js
@@ -26,39 +26,14 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-Espo.define('crm:views/opportunity/fields/stage', 'views/fields/enum', function (Dep) {
+Espo.define('crm:views/opportunity/record/edit-small', ['views/record/edit-small', 'crm:views/opportunity/record/edit'], function (Dep, Edit) {
 
     return Dep.extend({
 
-        listTemplate: 'fields/enum-styled/detail',
-
-        detailTemplate: 'fields/enum-styled/detail',
-
-        data: function () {
-            var style = 'default';
-            var stage = this.model.get('stage');
-            if (stage == 'Closed Won') {
-                style = 'success';
-            } else if (stage == 'Closed Lost') {
-                style = 'danger';
-            }
-            return _.extend({
-                style: style,
-            }, Dep.prototype.data.call(this));
-        },
-
-        setup: function () {
-            Dep.prototype.setup.call(this);
-
-            this.probabilityMap = this.getMetadata().get('entityDefs.Opportunity.fields.stage.probabilityMap') || {};
-
-            if (this.mode != 'list') {
-                this.on('change', function () {
-                    this.model.set('probability', this.probabilityMap[this.model.get(this.name)]);
-                }, this);
-            }
+        populateDefaults: function () {
+            Dep.prototype.populateDefaults.call(this);
+            Edit.prototype.populateDefaults.call(this);
         }
-
     });
-
 });
+
--- client/modules/crm/src/views/opportunity/record/edit.js
+++ client/modules/crm/src/views/opportunity/record/edit.js
@@ -26,39 +26,20 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-Espo.define('crm:views/opportunity/fields/stage', 'views/fields/enum', function (Dep) {
+Espo.define('crm:views/opportunity/record/edit', 'views/record/edit', function (Dep) {
 
     return Dep.extend({
 
-        listTemplate: 'fields/enum-styled/detail',
+        populateDefaults: function () {
+            Dep.prototype.populateDefaults.call(this);
 
-        detailTemplate: 'fields/enum-styled/detail',
+            var probabilityMap = this.getMetadata().get('entityDefs.Opportunity.fields.stage.probabilityMap') || {};
 
-        data: function () {
-            var style = 'default';
             var stage = this.model.get('stage');
-            if (stage == 'Closed Won') {
-                style = 'success';
-            } else if (stage == 'Closed Lost') {
-                style = 'danger';
-            }
-            return _.extend({
-                style: style,
-            }, Dep.prototype.data.call(this));
-        },
-
-        setup: function () {
-            Dep.prototype.setup.call(this);
-
-            this.probabilityMap = this.getMetadata().get('entityDefs.Opportunity.fields.stage.probabilityMap') || {};
-
-            if (this.mode != 'list') {
-                this.on('change', function () {
-                    this.model.set('probability', this.probabilityMap[this.model.get(this.name)]);
-                }, this);
+            if (stage in probabilityMap) {
+                this.model.set('probability', probabilityMap[stage], {silent: true});
             }
         }
-
     });
-
 });
+
