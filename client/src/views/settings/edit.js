6068cbf3cc691fac4f035726999b2614fa53cb4a
yuri <yurak@list.ru>
2017-01-16 15:57:10 +0200

    fix settings page title

--- client/src/views/settings/edit.js
+++ client/src/views/settings/edit.js
@@ -30,6 +30,8 @@ Espo.define('views/settings/edit', 'views/edit', function (Dep) {
 
     return Dep.extend({
 
+        scope: 'Settings',
+
         setupHeader: function () {
             this.createView('header', this.headerView, {
                 model: this.model,
