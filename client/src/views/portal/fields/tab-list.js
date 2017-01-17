01d8dddeac8a57c1d0d60308c692ac1cda0efffc
yuri <yurak@list.ru>
2017-01-17 12:27:21 +0200

    portal tab list

--- client/src/views/portal/fields/tab-list.js
+++ client/src/views/portal/fields/tab-list.js
@@ -30,15 +30,16 @@ Espo.define('views/portal/fields/tab-list', 'views/settings/fields/tab-list', fu
 
     return Dep.extend({
 
-        setup: function () {
-            Dep.prototype.setup.call(this);
+        setupOptions: function () {
+            Dep.prototype.setupOptions.call(this);
 
             this.params.options = this.params.options.filter(function (tab) {
+                if (tab === '_delimiter_') return true;
                 if (!!this.getMetadata().get('scopes.' + tab + '.aclPortal')) {
                     return true;
                 }
             }, this);
-        },
+        }
 
     });
 
