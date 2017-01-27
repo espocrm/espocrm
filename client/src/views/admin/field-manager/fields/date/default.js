2fc66f14f3c6a5f72a5d36e00eabd8027cc9d2ee
yuri <yurak@list.ru>
2017-01-27 15:10:58 +0200

    field manager: fix default date

--- client/src/views/admin/field-manager/fields/date/default.js
+++ client/src/views/admin/field-manager/fields/date/default.js
@@ -38,6 +38,15 @@ Espo.define('views/admin/field-manager/fields/date/default', 'views/fields/enum'
             }
 
             return data;
+        },
+
+        setupOptions: function () {
+            Dep.prototype.setupOptions.call(this);
+
+            var value = this.model.get(this.name);
+            if (this.params.options && value && !~(this.params.options).indexOf(value)) {
+                this.params.options.push(value);
+            }
         }
 
     });
