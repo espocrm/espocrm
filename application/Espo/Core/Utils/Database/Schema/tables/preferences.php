39e1303cbeb9a76a62ee2e0e5c33ad556eb3cd88
Taras Machyshyn <taras@espocrm.com>
2017-03-01 16:17:52 +0200

    Schema Converter improvements

--- application/Espo/Core/Utils/Database/Schema/tables/preferences.php
+++ application/Espo/Core/Utils/Database/Schema/tables/preferences.php
@@ -25,12 +25,13 @@
  *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
- ************************************************************************/ 
+ ************************************************************************/
 
-return array(    
+return array(
 
     'unset' => array(
-        'Preferences', 
+        '__APPEND__',
+        'Preferences',
     ),
 
 );
