5358bf4a43b439c6adf6fd04c7e1a104ab497a76
yuri <yurikuzn@users.noreply.github.com>
2017-04-18 15:00:19 +0300

    fix last viewerd

--- application/Espo/Services/LastViewed.php
+++ application/Espo/Services/LastViewed.php
@@ -58,7 +58,9 @@ class LastViewed extends \Espo\Core\Services\Base
             'userId' => $this->getUser()->id,
             'action' => 'read',
             'targetType' => $targetTypeList,
-        ))->order('number', true)->limit(0, $maxSize)->select(['targetId', 'targetType', 'number'])->distinct()->find();
+        ))->order('number', true)->limit(0, $maxSize)->select([
+            'targetId', 'targetType', 'number'
+        ])->groupBy(['targetId', 'targetType'])->find();
 
         foreach ($collection as $i => $entity) {
             $actionHistoryRecordService->loadParentNameFields($entity);
