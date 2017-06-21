0a8e0f00c29fd6ecd132d05bfb618870d0530cb7
yuri <yurikuzn@users.noreply.github.com>
2017-06-21 12:37:01 +0300

    defaultAttributes param

--- application/Espo/Core/Utils/Database/Orm/Fields/Link.php
+++ application/Espo/Core/Utils/Database/Orm/Fields/Link.php
@@ -40,24 +40,27 @@ class Link extends Base
                 'fields' => array(
                     $fieldName.'Id' => array(
                         'type' => 'foreignId',
-                        'index' => $fieldName,
+                        'index' => $fieldName
                     ),
                     $fieldName.'Name' => array(
                         'type' => 'varchar',
-                        'notStorable' => true,
-                    ),
-                ),
+                        'notStorable' => true
+                    )
+                )
             ),
             'unset' => array(
                 $entityName => array(
-                    'fields.'.$fieldName,
-                ),
-            ),
+                    'fields.'.$fieldName
+                )
+            )
         );
         if (!empty($fieldParams['notStorable'])) {
             $data[$entityName]['fields'][$fieldName.'Id']['notStorable'] = true;
         }
 
+        if (!empty($fieldParams['defaultAttributes']) && array_key_exists($fieldName.'Id', $fieldParams['defaultAttributes'])) {
+            $data[$entityName]['fields'][$fieldName.'Id']['default'] = $fieldParams['defaultAttributes'][$fieldName.'Id'];
+        }
 
         return $data;
     }
--- application/Espo/Core/Utils/Database/Orm/Fields/LinkParent.php
+++ application/Espo/Core/Utils/Database/Orm/Fields/LinkParent.php
@@ -33,31 +33,42 @@ class LinkParent extends Base
 {
     protected function load($fieldName, $entityName)
     {
-        return array(
+        $data = array(
             $entityName => array (
                 'fields' => array(
                     $fieldName.'Id' => array(
                         'type' => 'foreignId',
-                        'index' => $fieldName,
+                        'index' => $fieldName
                     ),
                     $fieldName.'Type' => array(
                         'type' => 'foreignType',
                         'notNull' => false,
-                        'index' => $fieldName,
+                        'index' => $fieldName
                     ),
                     $fieldName.'Name' => array(
                         'type' => 'varchar',
                         'notStorable' => true,
                         'relation' => $fieldName,
                         'isParentName' => true
-                    ),
-                ),
+                    )
+                )
             ),
             'unset' => array(
                 $entityName => array(
-                    'fields.'.$fieldName,
-                ),
-            ),
+                    'fields.'.$fieldName
+                )
+            )
         );
+
+        $fieldParams = $this->getFieldParams();
+
+        if (!empty($fieldParams['defaultAttributes']) && array_key_exists($fieldName.'Id', $fieldParams['defaultAttributes'])) {
+            $data[$entityName]['fields'][$fieldName.'Id']['default'] = $fieldParams['defaultAttributes'][$fieldName.'Id'];
+        }
+        if (!empty($fieldParams['defaultAttributes']) && array_key_exists($fieldName.'Type', $fieldParams['defaultAttributes'])) {
+            $data[$entityName]['fields'][$fieldName.'Type']['default'] = $fieldParams['defaultAttributes'][$fieldName.'Type'];
+        }
+
+        return $data;
     }
 }
