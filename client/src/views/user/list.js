047c5d5f652eed7b6d5713ec78a605a032bbbae8
yuri <yurikuzn@users.noreply.github.com>
2017-11-22 12:54:18 +0200

    fixes category and storing list view

--- client/src/views/user/list.js
+++ client/src/views/user/list.js
@@ -30,6 +30,8 @@ Espo.define('views/user/list', 'views/list', function (Dep) {
 
     return Dep.extend({
 
+        storeViewAfterUpdate: false,
+
         setup: function () {
             Dep.prototype.setup.call(this);
         }
