14db1291b615e84c88702046355fe81c130199ba
yuri <yurikuzn@users.noreply.github.com>
2017-12-14 16:34:46 +0200

    text minRows

--- client/src/views/fields/text.js
+++ client/src/views/fields/text.js
@@ -48,6 +48,8 @@ Espo.define('views/fields/text', 'views/fields/base', function (Dep) {
 
         rowsDefault: 10,
 
+        minRows: 2,
+
         searchTypeList: ['contains', 'startsWith', 'equals', 'endsWith', 'like', 'notContains', 'notLike', 'isEmpty', 'isNotEmpty'],
 
         events: {
@@ -63,6 +65,10 @@ Espo.define('views/fields/text', 'views/fields/base', function (Dep) {
             this.detailMaxLength = this.params.lengthOfCut || this.detailMaxLength;
 
             this.autoHeightDisabled = this.options.autoHeightDisabled || this.params.autoHeightDisabled || this.autoHeightDisabled;
+
+            if (this.params.rows < this.minRows) {
+                this.minRows = this.params.rows;
+            }
         },
 
         setupSearch: function () {
@@ -94,7 +100,7 @@ Espo.define('views/fields/text', 'views/fields/base', function (Dep) {
                 if (this.autoHeightDisabled) {
                     data.rows = this.params.rows;
                 } else {
-                    data.rows = 1;
+                    data.rows = this.minRows;
                 }
             }
             return data;
@@ -155,7 +161,7 @@ Espo.define('views/fields/text', 'views/fields/base', function (Dep) {
                 this.controlTextareaHeight(clientHeight);
             }
             if (this.$element.val().length === 0) {
-                this.$element.attr('rows', 1);
+                this.$element.attr('rows', this.minRows);
             }
         },
 
