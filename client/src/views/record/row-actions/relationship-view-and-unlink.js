9b19e67eacf508e3c85eed7bbd99ac5ac884cf16
Taras Machyshyn <taras@espocrm.com>
2017-12-20 15:29:17 +0200

    Added 2018 year

--- client/src/views/record/row-actions/relationship-view-and-unlink.js
+++ client/src/views/record/row-actions/relationship-view-and-unlink.js
@@ -2,7 +2,7 @@
  * This file is part of EspoCRM.
  *
  * EspoCRM - Open Source CRM application.
- * Copyright (C) 2014-2017 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Copyright (C) 2014-2018 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
  * Website: http://www.espocrm.com
  *
  * EspoCRM is free software: you can redistribute it and/or modify
