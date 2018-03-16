6122cbe6f79fd69f5f4e002e31fd4f782adea33b
Tanya Lei <tanya@letrium.com>
2018-03-16 13:30:23 +0200

    Activities Dashlet: fix Display Records option

--- client/modules/crm/src/views/dashlets/activities.js
+++ client/modules/crm/src/views/dashlets/activities.js
@@ -129,7 +129,7 @@ Espo.define('crm:views/dashlets/activities', ['views/dashlets/abstract/base', 'm
             this.collection = new MultiCollection();
             this.collection.seeds = this.seeds;
             this.collection.url = 'Activities/action/listUpcoming';
-            this.collection.maxSize = this.getConfig().get('recordsPerPageSmall') || 5;
+            this.collection.maxSize = this.getOption('displayRecords') || this.getConfig().get('recordsPerPageSmall') || 5;
             this.collection.data.entityTypeList = this.scopeList;
 
             this.listenToOnce(this.collection, 'sync', function () {
