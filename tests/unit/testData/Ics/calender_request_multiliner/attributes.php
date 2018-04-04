32a1f8b19a8bf84cf5f38a3070fd4058b3dd9dd7
Sebastian Foerster <foerster@forestsoft.de>
2018-04-05 01:52:24 +0200

    Fix Multiline descriptions for Outlook.
    See https://social.technet.microsoft.com/Forums/lync/en-US/c16cc4aa-0a07-4742-929d-1b01d698066e/ics-file-description-with-colon-and-newline-is-displayed-without-newlines?forum=outlook

---
+++ tests/unit/testData/Ics/calender_request_multiliner/attributes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'startDate' => strtotime("2018-04-05 04:00:00"),
+    'endDate' => strtotime("2018-04-05 05:00:00"),
+    'uid' => "5ac5535dee45f064e",
+    'summary' => "Meeting Formatierung",
+    'who' => "Sebastian Förster",
+    'email' => "user@example.com",
+    'description' => "Tagesordnung.\n\n1. Schauen ob das angehangen wird\n2. Die Formatierung sollte korrekt sein.\n\n",
+];
+
