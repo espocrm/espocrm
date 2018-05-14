fd4753d19000e28ac39291da765a368eb7c30019
yuri <yurikuzn@users.noreply.github.com>
2018-05-14 15:45:35 +0300

    email template info panel fix

--- client/src/views/email-template/record/panels/information.js
+++ client/src/views/email-template/record/panels/information.js
@@ -33,8 +33,28 @@ Espo.define('views/email-template/record/panels/information', 'views/record/pane
         _template: '{{{infoText}}}',
 
         data: function () {
+            var infoText = '';
+            var placeholderList = this.getMetadata().get(['clientDefs', 'EmailTemplate', 'placeholderList']);
+            if (placeholderList.length) {
+                infoText += '<h4>' + this.translate('Available placeholders') + ':</h4>';
+                infoText += '<ul>';
+                placeholderList.forEach(function (item, i) {
+                    infoText += '<li>' + '<code>{' + item + '}</code> &#8211; ' + this.translate(item, 'placeholderTexts', 'EmailTemplate');
+                    if (i === placeholderList.length - 1) {
+                        infoText += '.';
+                    } else {
+                        infoText += ';';
+                    }
+
+                    infoText += '</li>';
+                }, this);
+                infoText += '</ul>';
+
+                infoText = '<span class="complex-text">' + infoText + '</span>';
+            }
+
             return {
-                infoText: this.translate('infoText', 'messages', 'EmailTemplate').replace(/(\r\n|\n|\r)/gm, '<br>')
+                infoText: infoText
             };
         }
 
