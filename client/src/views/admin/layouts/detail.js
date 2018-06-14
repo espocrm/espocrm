89d706c94f414205a1e1985965f627c704ba0f5b
yuri <yurikuzn@users.noreply.github.com>
2018-06-14 13:00:18 +0300

    layout manager default panel

--- client/src/views/admin/layouts/detail.js
+++ client/src/views/admin/layouts/detail.js
@@ -66,7 +66,7 @@ Espo.define('views/admin/layouts/detail', 'views/admin/layouts/grid', function (
             }
         },
 
-        ignoreList: ['modifiedAt', 'createdAt', 'modifiedBy', 'createdBy', 'assignedUser', 'teams'],
+        defaultPanelFieldList: ['modifiedAt', 'createdAt', 'modifiedBy', 'createdBy', 'assignedUser', 'teams'],
 
         setup: function () {
             Dep.prototype.setup.call(this);
@@ -83,14 +83,41 @@ Espo.define('views/admin/layouts/detail', 'views/admin/layouts/grid', function (
         },
 
         loadLayout: function (callback) {
-            this.getModelFactory().create(this.scope, function (model) {
-                this.getHelper().layoutManager.get(this.scope, this.type, function (layout) {
-                    this.readDataFromLayout(model, layout);
-                    if (callback) {
-                        callback();
-                    }
-                }.bind(this), false);
+            var layout;
+            var model;
+
+            var promiseList = [];
+
+            promiseList.push(
+                new Promise(function (resolve) {
+                    this.getModelFactory().create(this.scope, function (m) {
+                        this.getHelper().layoutManager.get(this.scope, this.type, function (layoutLoaded) {
+                            layout = layoutLoaded;
+                            model = m;
+                            resolve();
+                        });
+                    }.bind(this));
+                }.bind(this))
+            );
+
+            if (~['detail', 'detailSmall'].indexOf(this.type)) {
+                promiseList.push(
+                    new Promise(function (resolve) {
+                        this.getHelper().layoutManager.get(this.scope, 'sidePanels' + Espo.Utils.upperCaseFirst(this.type), function (layoutLoaded) {
+                            this.sidePanelsLayout = layoutLoaded;
+                            resolve();
+                        }.bind(this));
+                    }.bind(this))
+                );
+            }
+
+            Promise.all(promiseList).then(function () {
+                this.readDataFromLayout(model, layout);
+                if (callback) {
+                    callback();
+                }
             }.bind(this));
+
         },
 
         readDataFromLayout: function (model, layout) {
@@ -130,11 +157,27 @@ Espo.define('views/admin/layouts/detail', 'views/admin/layouts/grid', function (
         },
 
         isFieldEnabled: function (model, name) {
-            if (this.ignoreList.indexOf(name) != -1) {
-                return false;
+            if (this.hasDefaultPanel()) {
+                if (this.defaultPanelFieldList.indexOf(name) !== -1) {
+                    return false;
+                }
             }
             return !model.getFieldParam(name, 'disabled') && !model.getFieldParam(name, 'layoutDetailDisabled');
+        },
+
+        hasDefaultPanel: function () {
+            if (this.getMetadata().get(['clientDefs', this.scope, 'defaultSidePanel', this.viewType]) === false) return false;
+            if (this.getMetadata().get(['clientDefs', this.scope, 'defaultSidePanelDisabled'])) return false;
+
+            if (this.sidePanelsLayout) {
+                for (var name in this.sidePanelsLayout) {
+                    if (name === 'default' && this.sidePanelsLayout[name].disabled) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     });
 });
-
