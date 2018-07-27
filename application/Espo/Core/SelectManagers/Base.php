bab4a3b0d6853fc259a24893c349ad5ca1b14899
yuri <yurikuzn@users.noreply.github.com>
2018-07-27 10:40:48 +0300

    fix full text

--- application/Espo/Core/SelectManagers/Base.php
+++ application/Espo/Core/SelectManagers/Base.php
@@ -1565,6 +1565,12 @@ class Base
             $useFullTextSearch = false;
         }
 
+        if ($isAuxiliaryUse) {
+            if (mb_strpos($textFilter, '@') !== false) {
+                $useFullTextSearch = false;
+            }
+        }
+
         if ($useFullTextSearch) {
             $textFilter = str_replace(['(', ')'], '', $textFilter);
 
@@ -1582,9 +1588,10 @@ class Base
                 $function = 'MATCH_NATURAL_LANGUAGE';
             } else {
                 $function = 'MATCH_BOOLEAN';
-
-                $textFilter = str_replace('@', '*', $textFilter);
             }
+
+            $textFilter = str_replace('"*', '"', $textFilter);
+            $textFilter = str_replace('*"', '"', $textFilter);
 
             while (strpos($textFilter, '**')) {
                 $textFilter = str_replace('**', '*', $textFilter);
