f8839518d5c2ed9b8234658ac337736619cc5a04
yuri <yurikuzn@users.noreply.github.com>
2018-07-30 15:48:00 +0300

    avatar check exif read data exists

--- application/Espo/EntryPoints/Image.php
+++ application/Espo/EntryPoints/Image.php
@@ -197,9 +197,10 @@ class Image extends \Espo\Core\EntryPoints\Base
                 break;
         }
 
-        $targetImage = imagerotate($targetImage, array_values([0, 0, 0, 180, 0, 0, -90, 0, 90])[@exif_read_data($filePath)['Orientation'] ?: 0], 0);
+        if (function_exists('exif_read_data')) {
+            $targetImage = imagerotate($targetImage, array_values([0, 0, 0, 180, 0, 0, -90, 0, 90])[@exif_read_data($filePath)['Orientation'] ?: 0], 0);
+        }
 
         return $targetImage;
     }
 }
-
