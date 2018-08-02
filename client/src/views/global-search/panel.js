71864056bc3f60ca34ffd030308ddeb71c624b9e
yuri <yurikuzn@users.noreply.github.com>
2018-08-02 15:05:00 +0300

    navbar panel height

--- client/src/views/global-search/panel.js
+++ client/src/views/global-search/panel.js
@@ -32,8 +32,14 @@ Espo.define('views/global-search/panel', 'view', function (Dep) {
 
         template: 'global-search/panel',
 
-        afterRender: function () {
+        setup: function () {
+            this.maxSize = this.getConfig().get('globalSearchMaxSize') || 10;
 
+            this.navbarPanelHeightSpace = this.getThemeManager().getParam('navbarPanelHeightSpace') || 100;
+            this.navbarPanelBodyMaxHeight = this.getThemeManager().getParam('navbarPanelBodyMaxHeight') || 600;
+        },
+
+        afterRender: function () {
             this.listenToOnce(this.collection, 'sync', function () {
                 this.createView('list', 'views/record/list-expanded', {
                     el: this.options.el + ' .list-container',
@@ -46,7 +52,7 @@ Espo.define('views/global-search/panel', 'view', function (Dep) {
                                     view: 'views/global-search/name-field',
                                     params: {
                                         containerEl: this.options.el
-                                    },
+                                    }
                                 }
                             ]
                         ],
@@ -59,12 +65,18 @@ Espo.define('views/global-search/panel', 'view', function (Dep) {
                 }, function (view) {
                     view.render();
                 });
-            }.bind(this));
-            this.collection.maxSize = this.getConfig().get('recordsPerPageSmall') || 10;
+            }, this);
+
+            this.collection.reset();
+            this.collection.maxSize = this.maxSize;
             this.collection.fetch();
+
+            var windowHeight = $(window).height();
+            if (windowHeight - this.navbarPanelBodyMaxHeight < this.navbarPanelHeightSpace) {
+                var maxHeight = windowHeight - this.navbarPanelHeightSpace;
+                this.$el.find('> .panel > .panel-body').css('maxHeight', maxHeight + 'px');
+            }
         }
 
     });
-
 });
-
