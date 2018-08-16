f085d2140cd2bc0b824379317119151ec3fab9cf
yuri <yurikuzn@users.noreply.github.com>
2018-08-16 16:47:16 +0300

    fix strip url

--- client/src/views/fields/url.js
+++ client/src/views/fields/url.js
@@ -47,6 +47,31 @@ Espo.define('views/fields/url', 'views/fields/varchar', function (Dep) {
             }, Dep.prototype.data.call(this));
         },
 
+        afterRender: function () {
+            Dep.prototype.afterRender.call(this);
+
+            if (this.mode === 'edit') {
+                if (this.params.strip) {
+                    this.$element.on('change', function () {
+                        var value = this.$element.val() || '';
+                        value = this.strip(value);
+                        this.$element.val(value);
+                    }.bind(this));
+                }
+            }
+        },
+
+        strip: function (value) {
+            value = value.trim();
+            if (value.indexOf('http://') === 0) {
+                value = value.substr(7);
+            } else if (value.indexOf('https://') === 0) {
+                value = value.substr(8);
+            }
+            value = value.replace(/\/+$/, '');
+            return value;
+        },
+
         getUrl: function () {
             var url = this.model.get(this.name);
             if (url && url != '') {
@@ -56,8 +81,16 @@ Espo.define('views/fields/url', 'views/fields/varchar', function (Dep) {
                 return url;
             }
             return url;
+        },
+
+        fetch: function () {
+            var data = Dep.prototype.fetch.call(this);
+
+            if (this.params.strip) {
+                data[this.name] = this.strip(data[this.name]);
+            }
+            return data;
         }
 
     });
 });
-
