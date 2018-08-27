f37a49ddaf0cc9cc6a677dcc05ca1c7f5e01e40e
yuri <yurikuzn@users.noreply.github.com>
2018-08-27 10:23:15 +0300

    fix typo

--- application/Espo/Core/Container.php
+++ application/Espo/Core/Container.php
@@ -178,7 +178,7 @@ class Container
 
     protected function loadMailSender()
     {
-        $className = $this->getServiceClassName('mailSernder', '\\Espo\\Core\\Mail\\Sender');
+        $className = $this->getServiceClassName('mailSender', '\\Espo\\Core\\Mail\\Sender');
         return new $className(
             $this->get('config'),
             $this->get('entityManager')
