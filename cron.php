739d1dd3d16385fc5f5416142fd8871949f1d91e
yuri <yurikuzn@users.noreply.github.com>
2018-11-05 16:50:12 +0200

    fix msg

--- cron.php
+++ cron.php
@@ -28,9 +28,8 @@
  ************************************************************************/
 
 $sapiName = php_sapi_name();
-
 if (substr($sapiName, 0, 3) != 'cli') {
-    die("Cron can be run only via CLI");
+    die("Daemon can be run only via CLI");
 }
 
 include "bootstrap.php";
