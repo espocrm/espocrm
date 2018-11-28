c9bb6dfddeac1f760a3d2c64eda3eff3607e448a
yuri <yurikuzn@users.noreply.github.com>
2018-11-28 17:25:49 +0200

    fix note collection

--- client/src/collections/note.js
+++ client/src/collections/note.js
@@ -29,6 +29,20 @@ Espo.define('collections/note', 'collection', function (Dep) {
 
     return Dep.extend({
 
+        parse: function (response, params) {
+            var total = this.total;
+            var list = Dep.prototype.parse.call(this, response, params);
+
+            if (params.data && params.data.after) {
+                if (total >= 0 && response.total >= 0) {
+                    this.total = total + response.total;
+                } else {
+                    this.total = total;
+                }
+            }
+            return list;
+        },
+
         fetchNew: function (options) {
             var options = options || {};
             options.data = options.data || {};
