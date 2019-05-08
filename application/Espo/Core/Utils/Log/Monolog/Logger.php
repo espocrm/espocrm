68301c52c1d1c5e9cc2daeb6a52a3e369586f7b9
yuri <yurikuzn@users.noreply.github.com>
2019-05-08 11:31:35 +0300

    logger cleanup

--- application/Espo/Core/Utils/Log/Monolog/Logger.php
+++ application/Espo/Core/Utils/Log/Monolog/Logger.php
@@ -28,27 +28,10 @@
  ************************************************************************/
 
 namespace Espo\Core\Utils\Log\Monolog;
+
 class Logger extends \Monolog\Logger
 {
     protected $defaultLevelName = 'DEBUG';
-
-    /**
-     * Get Level Code
-     * @param  string $level Ex. DEBUG, ...
-     * @return int
-     */
-    public function getLevelCode($levelName)
-    {
-        $levelName = strtoupper($levelName);
-
-        $levels = $this->getLevels();
-
-        if (isset($levels[$levelName])) {
-            return $levels[$levelName];
-        }
-
-        return $levels[$this->defaultLevelName];
-    }
 
     public function setLevel($levelName)
     {
