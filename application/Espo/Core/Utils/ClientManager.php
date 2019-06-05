5adcf013d7f3e9a3380bcde9cf6797f55b6c8ac2
yuri <yurikuzn@users.noreply.github.com>
2019-06-05 12:56:02 +0300

    fix client manager

--- application/Espo/Core/Utils/ClientManager.php
+++ application/Espo/Core/Utils/ClientManager.php
@@ -133,7 +133,7 @@ class ClientManager
             $rel = $item['rel'] ?? '';
             $type = $item['type'] ?? '';
             $additinalPlaceholder = '';
-            if (empty($item['crossorigin'])) {
+            if (!empty($item['crossorigin'])) {
                 $additinalPlaceholder .= ' crossorigin';
             }
             $linksHtml .= "\n        <link rel=\"{$rel}\" href=\"{$href}\" as=\"{$as}\" as=\"{$type}\"{$additinalPlaceholder}>";
