a6a6ce672228777ff34d522f75c63ef86b3f8c45
yuri <yurikuzn@users.noreply.github.com>
2019-08-28 10:37:23 +0300

    fix layout

--- application/Espo/Core/Portal/Utils/Layout.php
+++ application/Espo/Core/Portal/Utils/Layout.php
@@ -34,8 +34,11 @@ use \Espo\Core\Utils\Json;
 
 class Layout extends \Espo\Core\Utils\Layout
 {
-    public function get($scope, $name)
+    public function get(string $scope, string $name) : ?string
     {
+        $originalScope = $scope;
+        $originalName = $name;
+
         $scope = $this->sanitizeInput($scope);
         $name = $this->sanitizeInput($name);
 
@@ -43,87 +46,16 @@ class Layout extends \Espo\Core\Utils\Layout
             return Json::encode($this->changedData[$scope][$name]);
         }
 
-        $fileFullPath = Util::concatPath($this->getLayoutPath($scope, true), 'portal/' . $name . '.json');
+        $filePath = Util::concatPath($this->getLayoutPath($scope, true), 'portal/' . $name . '.json');
 
-        if (!file_exists($fileFullPath)) {
-            $fileFullPath = Util::concatPath($this->getLayoutPath($scope), 'portal/' . $name . '.json');
-        }
-        if (!file_exists($fileFullPath)) {
-            $fileFullPath = Util::concatPath($this->getLayoutPath($scope, true), $name . '.json');
-        }
-        if (!file_exists($fileFullPath)) {
-            $fileFullPath = Util::concatPath($this->getLayoutPath($scope), $name . '.json');
+        if (!file_exists($filePath)) {
+            $filePath = Util::concatPath($this->getLayoutPath($scope), 'portal/' . $name . '.json');
         }
 
-
-        if (!file_exists($fileFullPath)) {
-            $defaultPath = $this->params['defaultsPath'];
-            $fileFullPath = Util::concatPath(Util::concatPath($defaultPath, 'layouts'), $name . '.json' );
-
-            if (!file_exists($fileFullPath)) {
-                return false;
-            }
+        if (file_exists($filePath)) {
+            return $this->getFileManager()->getContents($filePath);
         }
 
-        return $this->getFileManager()->getContents($fileFullPath);
+        return parent::get($originalScope, $originalName);
     }
-
-
-    public function set($data, $scope, $name)
-    {
-        $scope = $this->sanitizeInput($scope);
-        $name = $this->sanitizeInput($name);
-
-        if (empty($scope) || empty($name)) {
-            return false;
-        }
-
-        $this->changedData[$scope][$name] = $data;
-    }
-
-    public function resetToDefault($scope, $name)
-    {
-        $scope = $this->sanitizeInput($scope);
-        $name = $this->sanitizeInput($name);
-
-        $filePath = 'custom/Espo/Custom/Resources/layouts/' . $scope . '/' . $name . '.json';
-        if ($this->getFileManager()->isFile($filePath)) {
-            $this->getFileManager()->removeFile($filePath);
-        }
-        if (!empty($this->changedData[$scope]) && !empty($this->changedData[$scope][$name])) {
-            unset($this->changedData[$scope][$name]);
-        }
-        return $this->get($scope, $name);
-    }
-
-    /**
-     * Save changes
-     *
-     * @return bool
-     */
-    public function save()
-    {
-        $result = true;
-
-        if (!empty($this->changedData)) {
-            foreach ($this->changedData as $scope => $rowData) {
-                foreach ($rowData as $layoutName => $layoutData) {
-                    if (empty($scope) || empty($layoutName)) {
-                        continue;
-                    }
-                    $layoutPath = $this->getLayoutPath($scope, true);
-                    $data = Json::encode($layoutData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-
-                    $result &= $this->getFileManager()->putContents(array($layoutPath, $layoutName.'.json'), $data);
-                }
-            }
-        }
-
-        if ($result == true) {
-            $this->clearChanges();
-        }
-
-        return (bool) $result;
-    }
-
 }
