604aca50c80d195d9d5c5b9a14289bb425b7b67f
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2019-12-11 15:08:12 +0200

    array valiation

--- application/Espo/Core/FieldValidators/ArrayIntType.php
+++ application/Espo/Core/FieldValidators/ArrayIntType.php
@@ -29,34 +29,7 @@
 
 namespace Espo\Core\FieldValidators;
 
-class ArrayType extends BaseType
+class ArrayIntType extends ArrayType
 {
-    public function checkRequired(\Espo\ORM\Entity $entity, string $field, $validationValue, $data) : bool
-    {
-        return $this->isNotEmpty($entity, $field);
-    }
 
-    public function checkMaxCount(\Espo\ORM\Entity $entity, string $field, $validationValue, $data) : bool
-    {
-        if (!$this->isNotEmpty($entity, $field)) return true;
-        $list = $entity->get($field);
-        if (count($list) > $validationValue) return false;
-        return true;
-    }
-
-    public function checkArray(\Espo\ORM\Entity $entity, string $field, $validationValue, $data) : bool
-    {
-        if (!$entity->has($field) || $entity->get($field) === null) return true;
-
-        return is_array($entity->get($field));
-    }
-
-    protected function isNotEmpty(\Espo\ORM\Entity $entity, $field)
-    {
-        if (!$entity->has($field) || $entity->get($field) === null) return false;
-        $list = $entity->get($field);
-        if (!is_array($list)) return false;
-        if (count($list)) return true;
-        return false;
-    }
 }
