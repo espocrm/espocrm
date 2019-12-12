97fa72042e21c9a88d71144cd48bbce61d6dcffe
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2019-12-12 12:21:29 +0200

    currency translation

--- client/src/views/settings/fields/currency-list.js
+++ client/src/views/settings/fields/currency-list.js
@@ -32,6 +32,17 @@ define('views/settings/fields/currency-list', 'views/fields/multi-enum', functio
 
         setupOptions: function () {
             this.params.options = this.getMetadata().get(['app', 'currency', 'list']) || [];
+
+            this.translatedOptions = {};
+
+            this.params.options.forEach(function (item) {
+                var value = item
+                var name = this.getLanguage().get('Currency', 'names', item);
+                if (name) {
+                    value += ' - ' + name;
+                }
+                this.translatedOptions[item] = value;
+            }, this);
         },
 
     });
