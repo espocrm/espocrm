2615cffd82bbac3f04d98f34fed583d941310fd2
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2019-12-17 15:21:08 +0200

    case contact save refactor

--- application/Espo/Modules/Crm/Repositories/CaseObj.php
+++ application/Espo/Modules/Crm/Repositories/CaseObj.php
@@ -39,60 +39,48 @@ class CaseObj extends \Espo\Core\ORM\Repositories\RDB
         $this->addDependency('serviceFactory');
     }
 
-    public function afterSave(Entity $entity, array $options = array())
+    public function afterSave(Entity $entity, array $options = [])
     {
-        $result = parent::afterSave($entity, $options);
+        parent::afterSave($entity, $options);
         $this->handleAfterSaveContacts($entity, $options);
-        return $result;
     }
 
-    protected function handleAfterSaveContacts(Entity $entity, array $options = array())
+    protected function handleAfterSaveContacts(Entity $entity, array $options = [])
     {
-        $contactIdChanged = $entity->has('contactId') && $entity->get('contactId') != $entity->getFetched('contactId');
-
-        if ($contactIdChanged) {
+        if ($entity->isAttributeChanged('contactId')) {
             $contactId = $entity->get('contactId');
+            $contactIdList = $entity->get('contactsIds') ?? [];
+            $fetchedContactId = $entity->getFetched('contactId');
 
-            if ($entity->getFetched('contactId')) {
-                $previousPortalUser = $this->getEntityManager()->getRepository('User')->where([
-                    'contactId' => $entity->getFetched('contactId'),
-                    'type' => 'portal'
+            if ($fetchedContactId) {
+                $previousPortalUser = $this->getEntityManager()->getRepository('User')->select(['id'])->where([
+                    'contactId' => $fetchedContactId,
+                    'type' => 'portal',
                 ])->findOne();
+
                 if ($previousPortalUser) {
                     $this->getInjection('serviceFactory')->create('Stream')->unfollowEntity($entity, $previousPortalUser->id);
                 }
             }
 
-            if (empty($contactId)) {
-                $this->unrelate($entity, 'contacts', $entity->getFetched('contactId'));
+            if (!$contactId) {
+                if ($fetchedContactId) {
+                    $this->unrelate($entity, 'contacts', $fetchedContactId);
+                }
                 return;
             }
 
-            $portalUser = $this->getEntityManager()->getRepository('User')->where([
+            $portalUser = $this->getEntityManager()->getRepository('User')->select(['id'])->where([
                 'contactId' => $contactId,
                 'type' => 'portal',
-                'isActive' => true
+                'isActive' => true,
             ])->findOne();
 
             if ($portalUser) {
                 $this->getInjection('serviceFactory')->create('Stream')->followEntity($entity, $portalUser->id);
             }
-        }
 
-        if ($contactIdChanged) {
-            $pdo = $this->getEntityManager()->getPDO();
-
-            $sql = "
-                SELECT id FROM case_contact
-                WHERE
-                    contact_id = ".$pdo->quote($contactId)." AND
-                    case_id = ".$pdo->quote($entity->id)." AND
-                    deleted = 0
-            ";
-            $sth = $pdo->prepare($sql);
-            $sth->execute();
-
-            if (!$sth->fetch()) {
+            if (!in_array($contactId, $contactIdList) && !$this->isRelated($entity, 'contacts', $contactId)) {
                 $this->relate($entity, 'contacts', $contactId);
             }
         }
