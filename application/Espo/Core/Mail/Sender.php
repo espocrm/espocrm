7bfd2f32deaac5048a0fff252a5d78ff5c8608b4
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2020-01-02 11:09:13 +0200

    fix mail sender params merge

--- application/Espo/Core/Mail/Sender.php
+++ application/Espo/Core/Mail/Sender.php
@@ -177,13 +177,15 @@ class Sender
         return $this;
     }
 
-    public function send(Email $email, $params = [], $message = null, $attachmentList = [])
+    public function send(Email $email, ?array $params = [], $message = null, $attachmentList = [])
     {
         if (!$message) {
             $message = new Message();
         }
+        $params = $params ?? [];
+
         $config = $this->config;
-        $params = $this->params + $params;
+        $params = $params + $this->params;
 
         if ($email->get('from')) {
             $fromName = null;
