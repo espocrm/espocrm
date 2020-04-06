8336fe3f660dbaa5a2ea4557804154a0b8518174
Taras Machyshyn <taras@espocrm.com>
2020-04-06 11:25:30 +0300

    Bug fixing in Util

--- application/Espo/Core/Utils/Util.php
+++ application/Espo/Core/Utils/Util.php
@@ -252,7 +252,8 @@ class Util
         if (substr($folderPath, -1) == static::getSeparator() || substr($folderPath, -1) == '/') {
             return static::fixPath($folderPath . $filePath);
         }
-        return $folderPath . static::getSeparator() . $filePath;
+
+        return static::fixPath($folderPath) . static::getSeparator() . $filePath;
     }
 
     /**
