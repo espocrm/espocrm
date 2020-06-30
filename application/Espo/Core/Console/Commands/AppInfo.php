b7d256687a2c82e486154e0af80d4b3ca8b5eb49
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2020-06-30 10:43:50 +0300

    app info container command

---
+++ application/Espo/Core/Console/Commands/AppInfo.php
@@ -0,0 +1,96 @@
+<?php
+/************************************************************************
+ * This file is part of EspoCRM.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2020 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: https://www.espocrm.com
+ *
+ * EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
+ ************************************************************************/
+
+namespace Espo\Core\Console\Commands;
+
+use Espo\Core\Container;
+use Espo\Core\Utils\Metadata;
+
+class AppInfo implements Command
+{
+    protected $container;
+    protected $metadata;
+
+    public function __construct(Container $container, Metadata $metadata)
+    {
+        $this->container = $container;
+        $this->metadata = $metadata;
+    }
+
+    public function run(array $options, array $flagList)
+    {
+        if (in_array('container', $flagList)) {
+            $this->printContainerInfo(in_array('nameOnly', $flagList));
+        }
+    }
+
+    protected function printContainerInfo(bool $nameOnly = false)
+    {
+        $serviceList = [
+            'injectableFactory', 'config', 'log', 'fileManager', 'dataManager', 'metadata', 'user',
+        ];
+
+        $fileList = scandir('application/Espo/Core/Loaders');
+
+        if (file_exists('custom/Espo/Custom/Core/Loaders')) {
+            $fileList = array_merge($fileList, scandir('custom/Espo/Custom/Core/Loaders'));
+        }
+
+        foreach ($fileList as $file) {
+            if (substr($file, -4) === '.php') {
+                $name = lcfirst(substr($file, 0, -4));
+
+                if (!in_array($name, $serviceList) && $this->container->has($name)) {
+                    $serviceList[] = $name;
+                }
+            }
+        }
+
+        foreach ($this->metadata->get(['app', 'containerServices']) ?? [] as $name => $data) {
+            if (!in_array($name, $serviceList)) {
+                $serviceList[] = $name;
+            }
+        }
+        if ($nameOnly) {
+            foreach ($serviceList as $name) {
+                echo $name . "\n";
+            }
+            return;
+        }
+
+        foreach ($serviceList as $name) {
+            echo $name . "\n";
+
+            $obj = $this->container->get($name);
+            echo get_class($obj) . "\n";
+
+            echo "\n";
+        }
+    }
+}
