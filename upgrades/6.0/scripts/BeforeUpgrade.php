88948c4b66fc213b91b29e559c49c3685ef934b6
Taras Machyshyn <taras@espocrm.com>
2020-09-10 12:35:42 +0300

    BeforeUpgrade script

---
+++ upgrades/6.0/scripts/BeforeUpgrade.php
@@ -0,0 +1,68 @@
+<?php
+/************************************************************************
+ * This file is part of EspoCRM.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2020 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: https://www.espocrm.com
+ *
+ * EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
+ ************************************************************************/
+
+class BeforeUpgrade
+{
+    public function run($container)
+    {
+        $this->container = $container;
+
+        $myisamTableList = $this->getMyisamTableList($container);
+        if (!empty($myisamTableList)) {
+            $isCli = (substr(php_sapi_name(), 0, 3) == 'cli') ? true : false;
+            throw new \Espo\Core\Exceptions\Error("In v6.0 we have dropped a support of MyISAM engine for DB tables. You have the following tables that use MyISAM: " . implode(", ", $myisamTableList) . ". " . ($isCli ? "\n" : "<br>") . "Please change the engine to InnoDB for these tables then run upgrade again. See ". ($isCli ? "" : "<a href=\"https://www.espocrm.com/blog/converting-myisam-engine-to-innodb\" target=\"_blank\">") . "https://www.espocrm.com/blog/converting-myisam-engine-to-innodb." . ($isCli ? "" : "</a>"));
+        }
+    }
+
+    protected function getMyisamTableList($container)
+    {
+        $pdo = $container->get('entityManager')->getPDO();
+        $databaseInfo = $container->get('config')->get('database');
+
+        try {
+            $sth = $pdo->prepare("
+                SELECT TABLE_NAME as tableName
+                FROM information_schema.TABLES
+                WHERE TABLE_SCHEMA = '". $databaseInfo['dbname'] ."'
+                AND ENGINE = 'MyISAM'
+            ");
+            $sth->execute();
+        } catch (\Exception $e) {
+            return;
+        }
+
+        $tableList = $sth->fetchAll(\PDO::FETCH_COLUMN);
+
+        if (empty($tableList)) {
+            return;
+        }
+
+        return $tableList;
+    }
+}
