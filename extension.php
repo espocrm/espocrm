e2bf27e524d40ec6be1d3061e64d2e869540455d
Taras Machyshyn <taras@espocrm.com>
2020-10-21 12:00:47 +0300

    Fix

--- extension.php
+++ extension.php
@@ -31,6 +31,11 @@ if (substr(php_sapi_name(), 0, 3) != 'cli') exit;
 
 include "bootstrap.php";
 
+use Espo\Core\{
+    Application,
+    ApplicationRunners\Rebuild,
+};
+
 $arg = isset($_SERVER['argv'][1]) ? trim($_SERVER['argv'][1]) : '';
 
 if (empty($arg)) {
@@ -46,7 +51,7 @@ if (!isset($pathInfo['extension']) || $pathInfo['extension'] !== 'zip' || !is_fi
     die("Unsupported package.\n");
 }
 
-$app = new \Espo\Core\Application();
+$app = new Application();
 $app->setupSystemUser();
 
 $config = $app->getContainer()->get('config');
@@ -67,8 +72,7 @@ try {
 }
 
 try {
-    $app = new \Espo\Core\Application();
-    $app->runRebuild();
+    (new Application())->run(Rebuild::class);
 } catch (\Exception $e) {}
 
 echo "Extension installation is complete.\n";
