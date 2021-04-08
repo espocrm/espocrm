1285955e3224d6fbd76d6351a5e24f50d171aaac
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2021-04-08 10:48:30 +0300

    cleanup

--- tests/integration/Espo/Core/Binding/BindingTest.php
+++ tests/integration/Espo/Core/Binding/BindingTest.php
@@ -34,10 +34,6 @@ use Espo\Core\{
     Binding\BindingLoader,
     Binding\BindingData,
     Binding\Binding,
-    Binding\Binder,
-    Binding\BindingContainer,
-    InjectableFactory,
-    Container,
 };
 
 use tests\integration\testClasses\Binding\{
