2e3642671f9a59c4c1390b29af29d87705b160f1
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2021-04-26 10:59:16 +0300

    fix test

--- tests/unit/Espo/Core/Utils/File/ClassMapTest.php
+++ tests/unit/Espo/Core/Utils/File/ClassMapTest.php
@@ -31,7 +31,7 @@ namespace tests\unit\Espo\Core\Utils\File;
 
 use tests\unit\ReflectionHelper;
 
-use Espo\Core\Utils\File\ClassMAp;
+use Espo\Core\Utils\File\ClassMap;
 use Espo\Core\Utils\DataCache;
 use Espo\Core\Utils\File\Manager as FileManager;
 
