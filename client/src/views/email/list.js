0f3cd9dd245b8f8c6723bd937ed456bf824ce31b
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2021-09-08 13:02:33 +0300

    cs fix

--- client/src/views/email/list.js
+++ client/src/views/email/list.js
@@ -53,7 +53,8 @@ define('views/email/list', 'views/list', function (Dep) {
                 this.addMenuItem('dropdown', {
                     name: 'reply',
                     label: 'Email Accounts',
-                    link: '#EmailAccount/list/userId=' + this.getUser().id + '&userName=' +  encodeURIComponent(this.getUser().get('name'))
+                    link: '#EmailAccount/list/userId=' + this.getUser().id + '&userName=' +
+                        encodeURIComponent(this.getUser().get('name'))
                 });
             }
 
@@ -83,57 +84,65 @@ define('views/email/list', 'views/list', function (Dep) {
         data: function () {
             var data = {};
             data.foldersDisabled = this.foldersDisabled;
+
             return data;
         },
 
         actionComposeEmail: function () {
             this.notify('Loading...');
-            var viewName = this.getMetadata().get('clientDefs.Email.modalViews.compose') || 'views/modals/compose-email';
+
+            var viewName = this.getMetadata().get('clientDefs.Email.modalViews.compose') ||
+                'views/modals/compose-email';
+
             this.createView('quickCreate', viewName, {
                 attributes: {
                     status: 'Draft'
                 }
-            }, function (view) {
+            }, (view) => {
                 view.render();
                 view.notify(false);
-                this.listenToOnce(view, 'after:save', function () {
+
+                this.listenToOnce(view, 'after:save', () => {
                     this.collection.fetch();
-                }, this);
-            }, this);
+                });
+            });
         },
 
         afterRender: function () {
             Dep.prototype.afterRender.call(this);
+
             if (!this.foldersDisabled && !this.hasView('folders')) {
                 this.loadFolders();
             }
         },
 
         getFolderCollection: function (callback) {
-            this.getCollectionFactory().create(this.folderScope, function (collection) {
+            this.getCollectionFactory().create(this.folderScope, (collection) => {
                 collection.url = 'EmailFolder/action/listAll';
                 collection.maxSize = 200;
 
-                this.listenToOnce(collection, 'sync', function () {
+                this.listenToOnce(collection, 'sync', () =>{
                     callback.call(this, collection);
-                }, this);
+                });
+
                 collection.fetch();
-            }, this);
+            });
         },
 
         loadFolders: function () {
             var xhr = null;
-            this.getFolderCollection(function (collection) {
 
+            this.getFolderCollection((collection) => {
                 this.createView('folders', 'views/email-folder/list-side', {
                     collection: collection,
                     emailCollection: this.collection,
                     el: this.options.el + ' .folders-container',
                     showEditLink: this.getAcl().check(this.folderScope, 'edit'),
-                    selectedFolderId: this.selectedFolderId
+                    selectedFolderId: this.selectedFolderId,
                 }, function (view) {
                     view.render();
-                    this.listenTo(view, 'select', function (id) {
+
+                    this.listenTo(view, 'select', (id) => {
                         this.selectedFolderId = id;
                         this.applyFolder();
 
@@ -142,22 +151,21 @@ define('views/email/list', 'views/list', function (Dep) {
                         }
 
                         this.notify(this.translate('pleaseWait', 'messages'));
-                        xhr = this.collection.fetch({
-                            success: function () {
-                                this.notify(false);
-                            }.bind(this)
-                        });
+
+                        xhr = this.collection
+                            .fetch()
+                            .then(() => this.notify(false));
 
                         if (id !== this.defaultFolderId) {
                             this.getRouter().navigate('#Email/list/folder=' + id);
                         } else {
                             this.getRouter().navigate('#Email');
                         }
-                        this.updateLastUrl();
-                    }, this);
-                }, this);
 
-            }, this);
+                        this.updateLastUrl();
+                    });
+                });
+            });
         },
 
         applyFolder: function () {
@@ -175,13 +183,14 @@ define('views/email/list', 'views/list', function (Dep) {
 
             if (!params.isReturnThroughLink && id !== this.selectedFolderId) {
                 var foldersView = this.getView('folders');
+
                 if (foldersView) {
                     foldersView.actionSelectFolder(id);
                     foldersView.reRender();
                     $(window).scrollTop(0);
                 }
             }
-        }
+        },
 
     });
 });
