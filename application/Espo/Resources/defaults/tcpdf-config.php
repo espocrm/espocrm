41b25a43baed5c947aacdd438bff27d591497a77
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2021-11-01 11:27:20 +0200

    tcpdf fix

---
+++ application/Espo/Resources/defaults/tcpdf-config.php
@@ -0,0 +1,31 @@
+<?php
+
+define('K_TCPDF_EXTERNAL_CONFIG', true);
+
+define('K_TCPDF_CALLS_IN_HTML', true);
+
+define('K_BLANK_IMAGE', '_blank.png');
+define('PDF_PAGE_FORMAT', 'A4');
+define('PDF_PAGE_ORIENTATION', 'P');
+define('PDF_CREATOR', 'TCPDF');
+define('PDF_AUTHOR', 'TCPDF');
+define('PDF_UNIT', 'mm');
+define('PDF_MARGIN_HEADER', 5);
+define('PDF_MARGIN_FOOTER', 10);
+define('PDF_MARGIN_TOP', 27);
+define('PDF_MARGIN_BOTTOM', 25);
+define('PDF_MARGIN_LEFT', 15);
+define('PDF_MARGIN_RIGHT', 15);
+define('PDF_FONT_NAME_MAIN', 'helvetica');
+define('PDF_FONT_SIZE_MAIN', 10);
+define('PDF_FONT_NAME_DATA', 'helvetica');
+define('PDF_FONT_SIZE_DATA', 8);
+define('PDF_FONT_MONOSPACED', 'courier');
+define('PDF_IMAGE_SCALE_RATIO', 1.25);
+define('HEAD_MAGNIFICATION', 1.1);
+define('K_CELL_HEIGHT_RATIO', 1.25);
+define('K_TITLE_MAGNIFICATION', 1.3);
+define('K_SMALL_RATIO', 2/3);
+define('K_THAI_TOPCHARS', true);
+define('K_TCPDF_THROW_EXCEPTION_ERROR', false);
+define('K_TIMEZONE', 'UTC');
--- application/Espo/Tools/Pdf/Tcpdf/Tcpdf.php
+++ application/Espo/Tools/Pdf/Tcpdf/Tcpdf.php
@@ -29,43 +29,13 @@
 
 namespace Espo\Tools\Pdf\Tcpdf;
 
-define('K_TCPDF_EXTERNAL_CONFIG', true);
-
-define('K_TCPDF_CALLS_IN_HTML', true);
-
-define('K_BLANK_IMAGE', '_blank.png');
-define('PDF_PAGE_FORMAT', 'A4');
-define('PDF_PAGE_ORIENTATION', 'P');
-define('PDF_CREATOR', 'TCPDF');
-define('PDF_AUTHOR', 'TCPDF');
-define('PDF_UNIT', 'mm');
-define('PDF_MARGIN_HEADER', 5);
-define('PDF_MARGIN_FOOTER', 10);
-define('PDF_MARGIN_TOP', 27);
-define('PDF_MARGIN_BOTTOM', 25);
-define('PDF_MARGIN_LEFT', 15);
-define('PDF_MARGIN_RIGHT', 15);
-define('PDF_FONT_NAME_MAIN', 'helvetica');
-define('PDF_FONT_SIZE_MAIN', 10);
-define('PDF_FONT_NAME_DATA', 'helvetica');
-define('PDF_FONT_SIZE_DATA', 8);
-define('PDF_FONT_MONOSPACED', 'courier');
-define('PDF_IMAGE_SCALE_RATIO', 1.25);
-define('HEAD_MAGNIFICATION', 1.1);
-define('K_CELL_HEIGHT_RATIO', 1.25);
-define('K_TITLE_MAGNIFICATION', 1.3);
-define('K_SMALL_RATIO', 2/3);
-define('K_THAI_TOPCHARS', true);
-define('K_TCPDF_THROW_EXCEPTION_ERROR', false);
-define('K_TIMEZONE', 'UTC');
-
-require "vendor/tecnickcom/tcpdf/tcpdf.php";
-
-use TCPDF_STATIC;
+use TCPDF as TcpdfOriginal;
 
 use Espo\Core\Utils\Util;
 
-class Tcpdf extends \TCPDF
+use TCPDF_STATIC;
+
+class Tcpdf extends TcpdfOriginal
 {
     protected $footerHtml = '';
 
@@ -132,7 +102,7 @@ class Tcpdf extends \TCPDF
             $html = str_replace('{totalPageNumber}', '{:ptp:}', $html);
         }
 
-        $this->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, '', 0, false, 'T');
+        $this->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, '', 0, false);
     }
 
     public function Footer()
@@ -160,7 +130,7 @@ class Tcpdf extends \TCPDF
             $html = str_replace('{totalPageNumber}', '{:ptp:}', $html);
         }
 
-        $this->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, '', 0, false, 'T');
+        $this->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, '', 0, false);
 
         $this->SetAutoPageBreak($autoPageBreak, $breakMargin);
     }
