3a7cccbda259ce70468cad1d092126249d750726
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2021-12-24 12:01:05 +0200

    cs fix

--- client/src/views/stream/record/list.js
+++ client/src/views/stream/record/list.js
@@ -26,7 +26,7 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-Espo.define('views/stream/record/list', 'views/record/list-expanded', function (Dep) {
+define('views/stream/record/list', 'views/record/list-expanded', function (Dep) {
 
     return Dep.extend({
 
@@ -34,6 +34,7 @@ Espo.define('views/stream/record/list', 'views/record/list-expanded', function (
 
         setup: function () {
             this.itemViews = this.getMetadata().get('clientDefs.Note.itemViews') || {};
+
             Dep.prototype.setup.call(this);
         },
 
@@ -55,7 +56,7 @@ Espo.define('views/stream/record/list', 'views/record/list-expanded', function (
                 optionsToPass: ['acl'],
                 name: this.type + '-' + model.name,
                 el: this.options.el + ' li[data-id="' + model.id + '"]',
-                setViewBeforeCallback: this.options.skipBuildRows && !this.isRendered()
+                setViewBeforeCallback: this.options.skipBuildRows && !this.isRendered(),
             }, callback);
         },
 
@@ -68,22 +69,29 @@ Espo.define('views/stream/record/list', 'views/record/list-expanded', function (
 
                 var count = this.collection.models.length;
                 var built = 0;
+
                 for (var i in this.collection.models) {
                     var model = this.collection.models[i];
-                    this.buildRow(i, model, function () {
+
+                    this.buildRow(i, model, () => {
                         built++;
-                        if (built == count) {
-                            if (typeof callback == 'function') {
+
+                        if (built === count) {
+                            if (typeof callback === 'function') {
                                 callback();
                             }
+
                             this.wait(false);
+
                             this.trigger('after:build-rows');
                         }
-                    }.bind(this));
+                    });
                 }
-            } else {
-                if (typeof callback == 'function') {
+            }
+            else {
+                if (typeof callback === 'function') {
                     callback();
+
                     this.trigger('after:build-rows');
                 }
             }
@@ -95,37 +103,44 @@ Espo.define('views/stream/record/list', 'views/record/list-expanded', function (
 
             var $list = this.$el.find(this.listContainerEl);
 
-            var success = function () {
+            var success = () => {
                 if (initialCount === 0) {
                     this.reRender();
+
                     return;
                 }
+
                 var rowCount = collection.length - initialCount;
                 var rowsReady = 0;
+
                 for (var i = rowCount - 1; i >= 0; i--) {
                     var model = collection.at(i);
 
-                    this.buildRow(i, model, function (view) {
-                        view.getHtml(function (html) {
+                    this.buildRow(i, model, (view) => {
+                        view.getHtml((html) => {
                             var $row = $(this.getRowContainerHtml(model.id));
+
                             $row.append(html);
                             $list.prepend($row);
+                            
                             rowsReady++;
+
                             view._afterRender();
+
                             if (view.options.el) {
                                 view.setElement(view.options.el);
                             }
-                        }.bind(this));
+                        });
                     });
                 }
+
                 this.noRebuild = true;
-            }.bind(this);
+            };
 
             collection.fetchNew({
                 success: success,
             });
-        }
+        },
 
     });
-
 });
