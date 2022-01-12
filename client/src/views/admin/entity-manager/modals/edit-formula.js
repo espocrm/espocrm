14c12a252808f1519b428eac57a160075ccfd318
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2022-01-12 17:16:27 +0200

    cs fix

--- client/src/views/admin/entity-manager/modals/edit-formula.js
+++ client/src/views/admin/entity-manager/modals/edit-formula.js
@@ -52,56 +52,66 @@ define('views/admin/entity-manager/modals/edit-formula', ['views/modal', 'model'
                 {
                     name: 'cancel',
                     label: 'Cancel'
-                }
+                },
             ];
 
             var scope = this.scope = this.options.scope || false;
 
-            this.header = this.translate('Formula', 'labels', 'EntityManager') + ': ' + this.translate(scope, 'scopeNames');
+            this.headerHtml = this.translate('Formula', 'labels', 'EntityManager') + ': ' +
+                this.translate(scope, 'scopeNames');
 
             var model = this.model = new Model();
+
             model.name = 'EntityManager';
 
-            this.wait(true);
-            this.ajaxGetRequest('Metadata/action/get', {
-                key: 'formula.' + scope
-            }).then(function (formulaData) {
-                formulaData = formulaData || {};
+            this.wait(
+                Espo.Ajax
+                    .getRequest('Metadata/action/get', {
+                      key: 'formula.' + scope
+                    })
+                    .then(formulaData => {
+                        formulaData = formulaData || {};
 
-                model.set('beforeSaveCustomScript', formulaData.beforeSaveCustomScript || null);
+                        model.set('beforeSaveCustomScript', formulaData.beforeSaveCustomScript || null);
 
-                this.createView('record', 'views/admin/entity-manager/record/edit-formula', {
-                    el: this.getSelector() + ' .record',
-                    model: model,
-                    targetEntityType: this.scope
-                });
-                this.wait(false);
-            }.bind(this));
+                        this.createView('record', 'views/admin/entity-manager/record/edit-formula', {
+                            el: this.getSelector() + ' .record',
+                            model: model,
+                            targetEntityType: this.scope,
+                        });
+                    })
+            );
         },
 
         actionSave: function () {
             this.disableButton('save');
 
             var data = this.getView('record').fetch();
+
             this.model.set(data);
-            if (this.getView('record').validate()) return;
+
+            if (this.getView('record').validate()) {
+                return;
+            }
 
             if (data.beforeSaveCustomScript === '') {
                 data.beforeSaveCustomScript = null;
             }
 
             Espo.Ui.notify(this.translate('pleaseWait', 'messages'));
-            this.ajaxPostRequest('EntityManager/action/formula', {
-                data: data,
-                scope: this.scope
-            }).then(function () {
-                Espo.Ui.success(this.translate('Saved'));
-                this.trigger('after:save');
-            }.bind(this)).fail(function () {
-                this.enableButton('save');
-            }.bind(this));
-        }
 
+            Espo.Ajax
+                .postRequest('EntityManager/action/formula', {
+                    data: data,
+                    scope: this.scope
+                })
+                .then(() => {
+                    Espo.Ui.success(this.translate('Saved'));
+
+                    this.trigger('after:save');
+                })
+                .catch(() => this.enableButton('save'));
+        },
 
     });
 });
