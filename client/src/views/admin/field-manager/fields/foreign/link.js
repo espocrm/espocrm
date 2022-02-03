27946bcbac0ad470e76345adb92cde698fb52708
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2022-02-03 18:18:39 +0200

    foreing field skip disabled link

--- client/src/views/admin/field-manager/fields/foreign/link.js
+++ client/src/views/admin/field-manager/fields/foreign/link.js
@@ -32,6 +32,7 @@ define('views/admin/field-manager/fields/foreign/link', 'views/fields/enum', fun
 
         setup: function () {
             Dep.prototype.setup.call(this);
+
             if (!this.model.isNew()) {
                 this.setReadOnly(true);
             }
@@ -40,26 +41,35 @@ define('views/admin/field-manager/fields/foreign/link', 'views/fields/enum', fun
         setupOptions: function () {
             var links = this.getMetadata().get(['entityDefs', this.options.scope, 'links']) || {};
 
-            this.params.options = Object.keys(Espo.Utils.clone(links)).filter(function (item) {
-                if (links[item].type !== 'belongsTo' && links[item].type !== 'hasOne') return;
-                if (links[item].noJoin) return;
+            this.params.options = Object.keys(Espo.Utils.clone(links)).filter((item) => {
+                if (links[item].type !== 'belongsTo' && links[item].type !== 'hasOne') {
+                    return;
+                }
+
+                if (links[item].noJoin) {
+                    return;
+                }
+                
+                if (links[item].disabled) {
+                    return;
+                }
 
                 return true;
-            }, this);
+            });
 
             var scope = this.options.scope;
 
             this.translatedOptions = {};
-            this.params.options.forEach(function (item) {
-                this.translatedOptions[item] = this.translate(item, 'links', scope);
-            }, this);
 
-            this.params.options = this.params.options.sort(function (v1, v2) {
+            this.params.options.forEach((item) => {
+                this.translatedOptions[item] = this.translate(item, 'links', scope);
+            });
+
+            this.params.options = this.params.options.sort((v1, v2) => {
                 return this.translate(v1, 'links', scope).localeCompare(this.translate(v2, 'links', scope));
-            }.bind(this));
+            });
 
             this.params.options.unshift('');
         },
     });
-
 });
