f563194445fb93c2cb5c075a76162933ba7ffc6d
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2022-02-09 17:21:17 +0200

    fix calendar dark theme

--- client/modules/crm/src/views/calendar/calendar.js
+++ client/modules/crm/src/views/calendar/calendar.js
@@ -390,8 +390,22 @@ define('crm:views/calendar/calendar', ['view', 'lib!full-calendar'], function (D
         },
 
         shadeColor: function (color, percent) {
-            var f = parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
-            return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+            if (this.getThemeManager().getParam('isDark')) {
+                percent *= -1;
+            }
+
+            var f = parseInt(color.slice(1), 16),
+                t = percent<0?0:255,
+                p = percent < 0 ?percent *- 1 : percent,
+                R = f >> 16,
+                G = f >> 8&0x00FF,
+                B = f&0x0000FF;
+
+            return "#" + (
+                0x1000000 + (
+                    Math.round((t-R)*p)+R)*0x10000 +
+                    (Math.round((t-G)*p)+G)*0x100 +
+                    (Math.round((t-B)*p)+B)).toString(16).slice(1);
         },
 
         handleAllDay: function (event, notInitial) {
--- client/modules/crm/src/views/calendar/timeline.js
+++ client/modules/crm/src/views/calendar/timeline.js
@@ -481,10 +481,23 @@ define('crm:views/calendar/timeline', ['view', 'lib!vis'], function (Dep, Vis) {
         },
 
         shadeColor: function (color, percent) {
-            var f = parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
-            return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
-        },
+            if (this.getThemeManager().getParam('isDark')) {
+                percent *= -1;
+            }
 
+            var f = parseInt(color.slice(1), 16),
+                t = percent<0?0:255,
+                p = percent < 0 ?percent *- 1 : percent,
+                R = f >> 16,
+                G = f >> 8&0x00FF,
+                B = f&0x0000FF;
+
+            return "#" + (
+                0x1000000 + (
+                    Math.round((t-R)*p)+R)*0x10000 +
+                    (Math.round((t-G)*p)+G)*0x100 +
+                    (Math.round((t-B)*p)+B)).toString(16).slice(1);
+        },
 
         convertEventList: function (list) {
             var resultList = [];
