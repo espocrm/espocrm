73036c73476d091ae039ea46c4b78f27a13346ee
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2022-07-15 15:52:22 +0300

    fix stream

--- client/src/views/stream/record/list.js
+++ client/src/views/stream/record/list.js
@@ -54,7 +54,7 @@ define('views/stream/record/list', 'views/record/list-expanded', function (Dep) 
                 let lengthBeforeFetch = options.lengthBeforeFetch || 0;
 
                 if (lengthBeforeFetch === 0) {
-                    this.reRender();
+                    this.buildRows(() => this.reRender());
 
                     return;
                 }
