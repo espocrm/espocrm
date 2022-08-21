a8631a6ce964ca9538126d2b7975dfc78a11a58a
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2022-08-21 19:36:36 +0300

    kanban fix

--- client/src/views/record/kanban.js
+++ client/src/views/record/kanban.js
@@ -148,7 +148,7 @@ define('views/record/kanban', ['views/record/list'], function (Dep) {
                 topBar: this.displayTotalCount || this.buttonList.length && !this.buttonsDisabled,
                 showCount: this.showCount && this.collection.total > 0,
                 buttonList: this.buttonList,
-                displayTotalCount: this.displayTotalCount && this.collection.total > 0,
+                displayTotalCount: this.displayTotalCount,
                 totalCount: this.collection.total,
                 statusList: this.statusList,
                 groupDataList: this.groupDataList,
