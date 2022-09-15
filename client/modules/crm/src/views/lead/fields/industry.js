8ee9a792fc1e0eaf5c205b00cecd942ce40ccf27
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2022-09-15 10:09:37 +0300

    lead industry acl fix

--- client/modules/crm/src/views/lead/fields/industry.js
+++ client/modules/crm/src/views/lead/fields/industry.js
@@ -26,17 +26,7 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-Espo.define('crm:views/lead/fields/industry', 'views/fields/enum', function (Dep) {
+define('crm:views/lead/fields/industry', ['views/fields/enum'], function (Dep) {
 
-    return Dep.extend({
-
-        setup: function () {
-            this.params.options = this.getMetadata().get('entityDefs.Account.fields.industry.options');
-            this.params.translation = 'Account.options.industry';
-
-            Dep.prototype.setup.call(this);
-        }
-
-    });
-
+    return Dep.extend({});
 });
