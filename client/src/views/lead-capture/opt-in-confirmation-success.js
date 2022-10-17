0cb1629f75198d4f7a4326c4418c009f8cfcddcc
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2022-10-17 13:10:35 +0300

    cs fix

--- client/src/views/lead-capture/opt-in-confirmation-success.js
+++ client/src/views/lead-capture/opt-in-confirmation-success.js
@@ -26,36 +26,35 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-Espo.define('views/lead-capture/opt-in-confirmation-success', ['view', 'model'], function (Dep, Model) {
+define('views/lead-capture/opt-in-confirmation-success', ['view', 'model'], function (Dep, Model) {
 
     return Dep.extend({
 
         template: 'lead-capture/opt-in-confirmation-success',
 
         setup: function () {
-            var model = new Model();
+            let model = new Model();
 
             this.resultData = this.options.resultData;
 
             if (this.resultData.message) {
                 model.set('message', this.resultData.message);
+
                 this.createView('messageField', 'views/fields/text', {
                     el: this.getSelector() + ' .field[data-name="message"]',
                     mode: 'detail',
                     inlineEditDisabled: true,
                     model: model,
-                    name: 'message'
+                    name: 'message',
                 });
             }
         },
 
         data: function () {
-            var data = {
+            return {
                 resultData: this.options.resultData,
-                defaultMessage: this.getLanguage().translate('optInIsConfirmed', 'messages', 'LeadCapture')
+                defaultMessage: this.getLanguage().translate('optInIsConfirmed', 'messages', 'LeadCapture'),
             };
-            return data;
-        }
-
+        },
     });
 });
