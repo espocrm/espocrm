866c336a0b7ba0dd9430c7dc1b714aa0f9533a78
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2022-11-27 13:16:48 +0200

    remove zend bridge

---
+++ application/Espo/Resources/defaults/class-aliases.php
@@ -0,0 +1,7 @@
+<?php
+
+/** @todo Remove in v8.0. */
+
+class_alias('Laminas\\Mail\\Protocol\\Smtp', 'Zend\\Mail\\Protocol\\Smtp');
+class_alias('Laminas\\Mail\\Protocol\\Imap', 'Zend\\Mail\\Protocol\\Imap');
+class_alias('Laminas\\Mail\\Message', 'Zend\\Mail\\Message');
