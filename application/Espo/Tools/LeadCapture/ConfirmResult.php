86a092ddb1ff7c6716244ef2884bf5a1e2a0376d
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2022-12-29 14:08:27 +0200

    fix

--- application/Espo/Tools/LeadCapture/ConfirmResult.php
+++ application/Espo/Tools/LeadCapture/ConfirmResult.php
@@ -34,29 +34,19 @@ class ConfirmResult
     public const STATUS_SUCCESS = 'success';
     public const STATUS_EXPIRED = 'expired';
 
-    private string $status;
-    private string $message;
-    private ?string $leadCaptureId;
-    private ?string $leadCaptureName;
-
     public function __construct(
-        string $status,
-        string $message,
-        ?string $leadCaptureId = null,
-        ?string $leadCaptureName = null
-    ) {
-        $this->status = $status;
-        $this->message = $message;
-        $this->leadCaptureId = $leadCaptureId;
-        $this->leadCaptureName = $leadCaptureName;
-    }
+        private string $status,
+        private ?string $message,
+        private ?string $leadCaptureId = null,
+        private ?string $leadCaptureName = null
+    ) {}
 
     public function getStatus(): string
     {
         return $this->status;
     }
 
-    public function getMessage(): string
+    public function getMessage(): ?string
     {
         return $this->message;
     }
