2b4f6239f785c6893fe1bea7b8b33d8e2daf71a9
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-01-02 17:31:06 +0200

    revert

--- upgrades/5.7/scripts/BeforeUpgrade.php
+++ upgrades/5.7/scripts/BeforeUpgrade.php
@@ -44,7 +44,7 @@ class BeforeUpgrade
         $databaseHelper = new \Espo\Core\Utils\Database\Helper($this->container->get('config'));
 
         $databaseType = $databaseHelper->getDatabaseType();
-        $fullVersion = $databaseHelper->getDatabaseServerVersion($this->container->get('entityManager')->getPDO());
+        $fullVersion = $databaseHelper->getPdoDatabaseVersion($this->container->get('entityManager')->getPDO());
 
         if (preg_match('/[0-9]+\.[0-9]+\.[0-9]+/', $fullVersion, $match)) {
             $version = $match[0];
