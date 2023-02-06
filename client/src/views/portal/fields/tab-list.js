5b8ccb4513a9ff3cb3fd00e4228b6eef076d51d2
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-02-06 17:10:41 +0200

    fisable group tabs for portal

--- client/src/views/portal/fields/tab-list.js
+++ client/src/views/portal/fields/tab-list.js
@@ -26,21 +26,24 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-Espo.define('views/portal/fields/tab-list', 'views/settings/fields/tab-list', function (Dep) {
+define('views/portal/fields/tab-list', ['views/settings/fields/tab-list'], function (Dep) {
 
     return Dep.extend({
+
+        noGroups: true,
 
         setupOptions: function () {
             Dep.prototype.setupOptions.call(this);
 
-            this.params.options = this.params.options.filter(function (tab) {
-                if (tab === '_delimiter_') return true;
+            this.params.options = this.params.options.filter(tab => {
+                if (tab === '_delimiter_') {
+                    return true;
+                }
+
                 if (!!this.getMetadata().get('scopes.' + tab + '.aclPortal')) {
                     return true;
                 }
-            }, this);
-        }
-
+            });
+        },
     });
-
 });
