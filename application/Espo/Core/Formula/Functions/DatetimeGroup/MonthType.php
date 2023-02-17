854f6c63906c0c9ba333c5c436819fcd8d1c5720
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-02-17 13:00:22 +0200

    fix

--- application/Espo/Core/Formula/Functions/DatetimeGroup/MonthType.php
+++ application/Espo/Core/Formula/Functions/DatetimeGroup/MonthType.php
@@ -27,7 +27,7 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-namespace Espo\Core\Formula\Functions\DateTimeGroup;
+namespace Espo\Core\Formula\Functions\DatetimeGroup;
 
 use Espo\Core\Di;
 
