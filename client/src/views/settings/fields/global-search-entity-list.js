06b31d537ffccb71955ed2666a1ec0b139a00d4e
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-03-08 13:53:47 +0200

    note no globals search

--- client/src/views/settings/fields/global-search-entity-list.js
+++ client/src/views/settings/fields/global-search-entity-list.js
@@ -33,13 +33,14 @@ define('views/settings/fields/global-search-entity-list', ['views/fields/multi-e
         setup: function () {
 
             this.params.options = Object.keys(this.getMetadata().get('scopes'))
-                .filter(scope =>{
-                    if (this.getMetadata().get('scopes.' + scope + '.disabled')) {
+                .filter(scope => {
+                    let defs = this.getMetadata().get(['scopes', scope]) || {};
+
+                    if (defs.disabled || scope === 'Note') {
                         return;
                     }
 
-                    return this.getMetadata().get('scopes.' + scope + '.customizable') &&
-                        this.getMetadata().get('scopes.' + scope + '.entity');
+                    return defs.customizable && defs.entity;
                 })
                 .sort((v1, v2) => {
                     return this.translate(v1, 'scopeNamesPlural')
