3ae84a0e5b140e0a126e35c3b5f4bfd0eb69d9b1
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-04-24 14:43:06 +0300

    consts

--- application/Espo/Core/Templates/Entities/Event.php
+++ application/Espo/Core/Templates/Entities/Event.php
@@ -34,4 +34,8 @@ use Espo\Core\ORM\Entity;
 class Event extends Entity
 {
     public const TEMPLATE_TYPE = 'Event';
+
+    public const STATUS_PLANNED = 'Planned';
+    public const STATUS_HELD = 'Held';
+    public const STATUS_NOT_HELD = 'Not Held';
 }
