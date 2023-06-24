447d949537be4695da97476ae43c0e3ac7688f08
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-06-24 19:27:40 +0300

    discard lib!espo usage

--- client/src/views/admin/entity-manager/formula.js
+++ client/src/views/admin/entity-manager/formula.js
@@ -26,7 +26,7 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-define('views/admin/entity-manager/formula', ['view', 'lib!espo', 'model'], function (Dep, Espo, Model) {
+define('views/admin/entity-manager/formula', ['view', 'model'], function (Dep, Model) {
 
     return Dep.extend({
 
