60c8502d272508a2b6ac3ac24b0306a82d48d57a
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-07-14 09:05:20 +0300

    contact role options reference

--- client/modules/crm/src/views/opportunity/fields/contact-role.js
+++ client/modules/crm/src/views/opportunity/fields/contact-role.js
@@ -31,12 +31,5 @@ define('crm:views/opportunity/fields/contact-role', ['views/fields/enum'], funct
     return Dep.extend({
 
         searchTypeList: ['anyOf', 'noneOf'],
-
-        setup: function () {
-            this.params.options = this.getMetadata().get('entityDefs.Contact.fields.opportunityRole.options');
-            this.params.translation = 'Contact.options.opportunityRole';
-
-            Dep.prototype.setup.call(this);
-        },
     });
 });
