8bce12aca3f50fef339b7d8c9ecb24f7b13d5129
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-09-28 07:54:42 +0300

    fix calendar initial date

--- client/modules/crm/src/views/calendar/calendar.js
+++ client/modules/crm/src/views/calendar/calendar.js
@@ -1020,7 +1020,7 @@ class CalendarView extends View {
         }
 
         if (this.date) {
-            options.initialDate = moment.utc(this.date).toDate();
+            options.initialDate = this.date;
         } else {
             this.$el.find('button[data-action="today"]').addClass('active');
         }
