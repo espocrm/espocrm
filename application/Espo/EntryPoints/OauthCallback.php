675a1575df20277eea29f206b0cfd1135fe1bce6
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-12-12 11:44:42 +0200

    fix msg

--- application/Espo/EntryPoints/OauthCallback.php
+++ application/Espo/EntryPoints/OauthCallback.php
@@ -34,13 +34,16 @@ use Espo\Core\Api\Response;
 use Espo\Core\EntryPoint\EntryPoint;
 use Espo\Core\EntryPoint\Traits\NoAuth;
 
+/**
+ * @noinspection PhpUnused
+ */
 class OauthCallback implements EntryPoint
 {
     use NoAuth;
 
     public function run(Request $request, Response $response): void
     {
-        echo "If this window is not closed automatically, it's probable that URL you use to access ".
-            "EspoCRM doesn't match URL specified at Administration > Settings > Site URL.";
+        echo "If this window is not closed automatically, it's probable that the URL you use to access ".
+            "EspoCRM doesn't match the URL specified at Administration > Settings > Site URL.";
     }
 }
