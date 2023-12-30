190dc8d08074f41317c41a5984c818e3be584601
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-12-30 12:39:20 +0200

    update file headers

--- install/vendor/smarty/libs/Smarty.class.php
+++ install/vendor/smarty/libs/Smarty.class.php
@@ -11,7 +11,7 @@
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
@@ -302,7 +302,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * template inheritance merge compiled includes
      * @var boolean
-     */    
+     */
     public $inheritance_merge_compiled_includes = true;
     /**
      * cache lifetime in seconds
