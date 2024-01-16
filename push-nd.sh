9df21f87e43559010a8bcf11db1287cd3db1f47a
Alexander Pylypenko <root@hw.com>
2024-01-16 19:04:40 +0200

    add scripts with no data file

---
+++ push-nd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SERVER="www-data:www-data";
+CLIENT="hw:hw";
+
+PROJECT="/var/www/html";    
+BUILD=$PROJECT"/build/dsfreedom";
+
+FRONTEND="/client";
+BACKEND="/cutom/Espo";
+
+#project custom code path
+FRONTEND_SRC=$PROJECT$FRONTEND"/custom";
+BACKEND_SRC=$PROJECT$BACKEND"/Custom";
+DATA_SRC=$PROJECT"/data";
+
+
+cp -r $BUILD"/client/custom" $PROJECT"/client/";
+cp -r $BUILD"/custom/Espo/Custom" $PROJECT"/custom/Espo/";
+
+
+echo "Done!";
