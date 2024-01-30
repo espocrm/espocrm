ef2d129699ff420533411cd9de1b983eeba29897
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2024-01-30 16:34:35 +0200

    ref

--- client/src/views/email/fields/body.js
+++ client/src/views/email/fields/body.js
@@ -26,92 +26,106 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-define('views/email/fields/body', ['views/fields/wysiwyg'], function (Dep) {
+import WysiwygFieldView from 'views/fields/wysiwyg';
 
-    return Dep.extend({
+class EmailBodyFieldView extends WysiwygFieldView {
 
-        useIframe: true,
+    useIframe = true
 
-        getAttributeList: function () {
-            return ['body', 'bodyPlain'];
-        },
+    getAttributeList() {
+        return ['body', 'bodyPlain'];
+    }
 
-        setupToolbar: function () {
-            Dep.prototype.setupToolbar.call(this);
+    setupToolbar() {
+        super.setupToolbar();
 
-            this.toolbar.unshift([
-                'insert-field',
-                ['insert-field']
-            ]);
+        this.toolbar.unshift([
+            'insert-field',
+            ['insert-field']
+        ]);
 
-            this.buttons['insert-field'] = function (context) {
-                var ui = $.summernote.ui;
-                var button = ui.button({
-                    contents: '<i class="fas fa-plus"></i>',
-                    tooltip: this.translate('Insert Field', 'labels', 'Email'),
-                    click: function () {
-                        this.showInsertFieldModal();
-                    }.bind(this)
-                });
-                return button.render();
-            }.bind(this);
+        this.buttons['insert-field'] = () => {
+            const ui = $.summernote.ui;
 
-            this.listenTo(this.model, 'change', function (m) {
-                if (!this.isRendered()) return;
-                if (m.hasChanged('parentId') || m.hasChanged('to')) {
-                    this.controInsertFieldButton();
-                }
-            }, this);
-        },
-
-        afterRender: function () {
-            Dep.prototype.afterRender.call(this);
-
-            this.controInsertFieldButton();
-        },
-
-        controInsertFieldButton: function () {
-            var $b = this.$el.find('.note-insert-field > button');
-
-            if (this.model.get('to') && this.model.get('to').length || this.model.get('parentId')) {
-                $b.removeAttr('disabled').removeClass('disabled');
-            } else {
-                $b.attr('disabled', 'disabled').addClass('disabled');
-            }
-        },
-
-        showInsertFieldModal: function () {
-            var to = this.model.get('to');
-            if (to) {
-                to = to.split(';')[0].trim();
-            }
-            var parentId = this.model.get('parentId');
-            var parentType = this.model.get('parentType');
-
-            Espo.Ui.notify(' ... ');
-
-            this.createView('insertFieldDialog', 'views/email/modals/insert-field', {
-                parentId: parentId,
-                parentType: parentType,
-                to: to,
-            }, function (view) {
-                view.render();
-                Espo.Ui.notify();
-
-                this.listenToOnce(view, 'insert', function (string) {
-                    if (this.$summernote) {
-                        if (~string.indexOf('\n')) {
-                            string = string.replace(/(?:\r\n|\r|\n)/g, '<br>');
-                            var html = '<p>' + string + '</p>';
-                            this.$summernote.summernote('editor.pasteHTML', html);
-                        } else {
-                            this.$summernote.summernote('editor.insertText', string);
-                        }
-                    }
-                    this.clearView('insertFieldDialog');
-                }, this);
+            const button = ui.button({
+                contents: '<i class="fas fa-plus"></i>',
+                tooltip: this.translate('Insert Field', 'labels', 'Email'),
+                click: () => {
+                    this.showInsertFieldModal();
+                },
             });
-        },
 
-    });
-});
+            return button.render();
+        };
+
+        this.listenTo(this.model, 'change', m => {
+            if (!this.isRendered()) {
+                return;
+            }
+
+            if (m.hasChanged('parentId') || m.hasChanged('to')) {
+                this.controlInsertFieldButton();
+            }
+        });
+    }
+
+    afterRender() {
+        super.afterRender();
+
+        this.controlInsertFieldButton();
+    }
+
+    controlInsertFieldButton() {
+        const $b = this.$el.find('.note-insert-field > button');
+
+        if (
+            this.model.get('to') &&
+            this.model.get('to').length ||
+            this.model.get('parentId')
+        ) {
+            $b.removeAttr('disabled').removeClass('disabled');
+        } else {
+            $b.attr('disabled', 'disabled').addClass('disabled');
+        }
+    }
+
+    showInsertFieldModal() {
+        let to = this.model.get('to');
+
+        if (to) {
+            to = to.split(';')[0].trim();
+        }
+
+        const parentId = this.model.get('parentId');
+        const parentType = this.model.get('parentType');
+
+        Espo.Ui.notify(' ... ');
+
+        this.createView('insertFieldDialog', 'views/email/modals/insert-field', {
+            parentId: parentId,
+            parentType: parentType,
+            to: to,
+        }, view => {
+            view.render();
+
+            Espo.Ui.notify();
+
+            this.listenToOnce(view, 'insert', /** string */string => {
+                if (this.$summernote) {
+                    if (string.includes('\n')) {
+                        string = string.replace(/\r\n|\r|\n/g, '<br>');
+                        const html = '<p>' + string + '</p>';
+
+                        this.$summernote.summernote('editor.pasteHTML', html);
+                    } else {
+                        this.$summernote.summernote('editor.insertText', string);
+                    }
+                }
+
+                this.clearView('insertFieldDialog');
+            });
+        });
+    }
+}
+
+export default EmailBodyFieldView;
