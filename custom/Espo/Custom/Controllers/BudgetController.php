e58056ee8997319170622a7bb2326138d8d6bc66
Alexander Pylypenko <root@hw.com>
2024-01-31 23:42:51 +0200

    add budget routes, controller, service

--- custom/Espo/Custom/Controllers/BudgetController.php
+++ custom/Espo/Custom/Controllers/BudgetController.php
@@ -2,115 +2,37 @@
 
 namespace Espo\Custom\Controllers;
 
-use \Datetime;
 use Espo\Core\Api\Request;
 use Espo\Core\Api\Response;
-use Espo\ORM\EntityManager;
+use Espo\Custom\Services\BudgetService;
 
 class BudgetController {
+    private $totalProfit = 0;
+    private $totalExpenses = 0;
 
-    public function __construct(private EntityManager $entityManager) {}
+    public function __construct(private BudgetService $budgetService) {}
 
     public function getActionIncome(Request $request, Response $response): string 
     {
         $dateFrom = $request->getRouteParam('dateFrom');
         $dateTo = $request->getRouteParam('dateTo');
+        $teamsString = explode('=', $request->getRouteParam('teams'))[1];
+        $teamsIds = explode('&', $teamsString);
         
-        return json_encode(
-            array(
-                'total' => array(
-                    'expenses' => 0,
-                    'profit' => 0,
-                    'income' => 0
-                ),
-                'list' => $this->getBudgetList($dateFrom, $dateTo)
-            )
+
+        return json_encode( 
+            $this->budgetService->getBudget($dateFrom, $dateTo, $teamsIds)
         );
     }
 
-    private function getBudgetList($dateFrom, $dateTo)
+    public function getActionDetail(Request $request, Response $response): string
     {
-        $date1 = new DateTime($dateFrom);
-        $date2 = new DateTime($dateTo);   
+        $date = $request->getRouteParam('dateFrom');
+        $teamsString = explode('=', $request->getRouteParam('teams'))[1];
+        $teamsIds = explode('&', $teamsString);
 
-        $interval = $date1->diff($date2);
-        $daysCount = $interval->days;
-
-        $abons = $this->getProfitByDate('Abonement', $dateFrom, $dateTo);
-        $indivs = $this->getProfitByDate('Indiv', $dateFrom, $dateTo);
-        $rents = $this->getProfitByDate('Rent', $dateFrom, $dateTo);
-        $rentplans = $this->getProfitByDate('RentPlan', $dateFrom, $dateTo);
-        $goods = $this->getProfitByDate('Goods', $dateFrom, $dateTo);
-        $expenses = $this->getExpensesByDate($dateFrom, $dateTo);
-
-        $date = $dateFrom;
-        $expensesList = array();
-        for ($i = 0;  $i <= $daysCount; $i++) {
-            $profitTotalSum = $this->getAllProfitTotalSum($date, $abons, $indivs, $rents, $rentplans, $goods);
-            $expensesTotalSum = $this->getTotalSumByDate($date, $expenses, 'cost', 'date');
-
-            array_push($expensesList,
-                array(
-                    'date' => $date,
-                    'profit' => $profitTotalSum,
-                    'expenses' => $expensesTotalSum,
-                    'income' => $profitTotalSum - $expensesTotalSum
-                )
-            );
-
-            $date = date('Y-m-d', strtotime('+1 day', strtotime($date)));
-        }
-        return $expensesList;
-    }
-
-    public function getProfitByDate($entityName, $dateFrom, $dateTo)
-    {
-        $dateTimeParams = [
-            'createdAt>=' => "${dateFrom} 00:00:00",
-            'createdAt<=' => "${dateTo} 23:59:59",
-        ];
-
-        return $this->entityManager
-            ->getRDBRepository($entityName)
-            ->where($dateTimeParams)
-            ->find();
-    }
-
-    private function getExpensesByDate($dateFrom, $dateTo)
-    {
-        $dateParams = [
-            'date>=' => $dateFrom,
-            'date<=' => $dateTo,
-        ];
-
-        return $this->entityManager
-            ->getRDBRepository('Expenses')
-            ->where($dateParams)
-            ->find();
-    }
-
-    private function getAllProfitTotalSum($date, $abons, $indivs, $rents, $rentplans, $goods)
-    {
-        $allProfitTotalSum = 0;
-        
-        $allProfitTotalSum += $this->getTotalSumByDate($date, $abons, 'price', 'createdAt');
-        $allProfitTotalSum += $this->getTotalSumByDate($date, $indivs, 'defaultPrice', 'createdAt');
-        $allProfitTotalSum += $this->getTotalSumByDate($date, $rents, 'customPrice', 'createdAt');
-        $allProfitTotalSum += $this->getTotalSumByDate($date, $rentplans, 'price', 'createdAt');
-        $allProfitTotalSum += $this->getTotalSumByDate($date, $goods, 'price', 'createdAt');
-        
-        return $allProfitTotalSum;
-    }
-
-    private function getTotalSumByDate($date, $entities, $priceFieldName, $dateFieldName): float
-    {
-        $sum = 0;
-        foreach($entities as $entity) {
-            $datePart = explode(" ", $entity->get($dateFieldName))[0];
-            if ($datePart == $date) {
-                $sum +=  $entity->get($priceFieldName);
-            }
-        }
-        return $sum;
+        return json_encode(
+            $this->budgetService->getDetails($date, $teamsIds)
+        );
     }
 }
---
+++ custom/Espo/Custom/Services/BudgetService.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Espo\Custom\Services;
+
+use \Datetime;
+use Espo\ORM\EntityManager;
+
+class BudgetService {
+    private $totalProfit = 0;
+    private $totalExpenses = 0;
+
+    public function __construct(private EntityManager $entityManager) {}
+
+    public function getBudget($dateFrom, $dateTo, $teamsIds)
+    {   
+        return array(
+            'list' => $this->getBudgetList($dateFrom, $dateTo, $teamsIds),
+            'total' => array(
+                'profit' => $this->totalProfit,
+                'expenses' => $this->totalExpenses,
+                'income' => $this->totalProfit - $this->totalExpenses
+                )
+            );
+    }
+
+    public function getDetails($date, $teamsIds)
+    {
+        $abons = $this->getProfitByDate('Abonement', $date, $date, $teamsIds);
+        $indivs = $this->getProfitByDate('Indiv', $date, $date, $teamsIds);
+        $rents = $this->getProfitByDate('Rent', $date, $date, $teamsIds);
+        $rentplans = $this->getProfitByDate('RentPlan', $date, $date, $teamsIds);
+        $goods = $this->getProfitByDate('Goods', $date, $date, $teamsIds);
+
+        $profitDetailList = [
+            [
+                'name' => 'Абонементи', 
+                'value' => $this->getTotalSumByDate($date, $abons, 'price', 'createdAt')
+            ],
+            [
+                'name' => 'Індиви', 
+                'value' => $this->getTotalSumByDate($date, $indivs, 'price', 'createdAt')
+            ],
+            [
+                'name' => 'Оренда разова', 
+                'value' => $this->getTotalSumByDate($date, $rents, 'price', 'createdAt')
+            ],
+            [
+                'name' => 'Оренда планова', 
+                'value' => $this->getTotalSumByDate($date, $rentplans, 'price', 'createdAt')
+            ],
+            [
+                'name' => 'Товари', 
+                'value' => $this->getTotalSumByDate($date, $goods, 'price', 'createdAt')
+            ],
+        ];
+
+        $expensesList = $this->getExpensesByDate($date, $date, $teamsIds);
+
+        return array(
+            'profitDetails' => $profitDetailList,
+            'expensesDetails' => $this->getExpensesDetailList($expensesList)
+        );
+    }
+
+    private function getExpensesDetailList($expensesList)
+    {
+        $expensesDetailList = [];
+        foreach($expensesList as $expense) {
+            array_push(
+                $expensesDetailList, 
+                [
+                    'name' => $expense->get('name'),
+                    'value' => $expense->get('price')
+                ]);
+        }
+        return $expensesDetailList;
+    }
+
+    private function getBudgetList($dateFrom, $dateTo, $teamsIds)
+    {
+        $date1 = new DateTime($dateFrom);
+        $date2 = new DateTime($dateTo);   
+
+        $interval = $date1->diff($date2);
+        $daysCount = $interval->days;
+
+        $abons = $this->getProfitByDate('Abonement', $dateFrom, $dateTo, $teamsIds);
+        $indivs = $this->getProfitByDate('Indiv', $dateFrom, $dateTo, $teamsIds);
+        $rents = $this->getProfitByDate('Rent', $dateFrom, $dateTo, $teamsIds);
+        $rentplans = $this->getProfitByDate('RentPlan', $dateFrom, $dateTo, $teamsIds);
+        $goods = $this->getProfitByDate('Goods', $dateFrom, $dateTo, $teamsIds);
+        $expenses = $this->getExpensesByDate($dateFrom, $dateTo, $teamsIds);
+
+        $date = $dateFrom;
+        $expensesList = array();
+        for ($i = 0;  $i <= $daysCount; $i++) {
+            $profitTotalSum = $this->getAllProfitTotalSum($date, $abons, $indivs, $rents, $rentplans, $goods);
+            $expensesTotalSum = $this->getTotalSumByDate($date, $expenses, 'price', 'date');
+
+            array_push($expensesList,
+                array(
+                    'date' => $date,
+                    'profit' => $profitTotalSum,
+                    'expenses' => $expensesTotalSum,
+                    'income' => $profitTotalSum - $expensesTotalSum
+                )
+            );
+            $this->totalProfit += $profitTotalSum;
+            $this->totalExpenses += $expensesTotalSum;
+
+            $date = date('Y-m-d', strtotime('+1 day', strtotime($date)));
+        }
+        return $expensesList;
+    }
+
+    private function getProfitByDate($entityName, $dateFrom, $dateTo, $teamsIds)
+    {
+        $selectParams = [
+            'createdAt>=' => "${dateFrom} 00:00:00",
+            'createdAt<=' => "${dateTo} 23:59:59",
+            'teams.id' => $teamsIds
+        ];
+
+        return $this->entityManager
+            ->getRepository($entityName)
+            ->distinct()
+            ->join('teams')
+            ->where($selectParams)
+            ->find();
+    }
+
+    private function getExpensesByDate($dateFrom, $dateTo, $teamsIds)
+    {
+        $selectParams = [
+            'date>=' => $dateFrom,
+            'date<=' => $dateTo,
+            'teams.id' => $teamsIds
+        ];
+
+        return $this->entityManager
+            ->getRepository('Expenses')
+            ->distinct()
+            ->join('teams')
+            ->where($selectParams)
+            ->find();
+    }
+
+    private function getAllProfitTotalSum($date, $abons, $indivs, $rents, $rentplans, $goods)
+    {
+        $allProfitTotalSum = 0;
+        
+        $allProfitTotalSum += $this->getTotalSumByDate($date, $abons, 'price', 'createdAt');
+        $allProfitTotalSum += $this->getTotalSumByDate($date, $indivs, 'price', 'createdAt');
+        $allProfitTotalSum += $this->getTotalSumByDate($date, $rents, 'price', 'createdAt');
+        $allProfitTotalSum += $this->getTotalSumByDate($date, $rentplans, 'price', 'createdAt');
+        $allProfitTotalSum += $this->getTotalSumByDate($date, $goods, 'price', 'createdAt');
+        
+        return $allProfitTotalSum;
+    }
+
+    private function getTotalSumByDate($date, $entities, $priceFieldName, $dateFieldName): float
+    {
+        $sum = 0;
+        foreach($entities as $entity) {
+            $datePart = explode(" ", $entity->get($dateFieldName))[0];
+            if ($datePart == $date) {
+                $sum +=  $entity->get($priceFieldName);
+            }
+        }
+        return $sum;
+    }
+}
