ee8963ace86fb95b808ead07233f95122324983f
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2024-03-01 11:12:41 +0200

    ref

--- application/Espo/Classes/Record/AuthToken/UpdateInputFilter.php
+++ application/Espo/Classes/Record/AuthToken/UpdateInputFilter.php
@@ -27,31 +27,26 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-namespace Espo\Services;
+namespace Espo\Classes\Record\AuthToken;
 
-use stdClass;
+use Espo\Core\Record\Input\Data;
+use Espo\Core\Record\Input\Filter;
 
 /**
- * @extends Record<\Espo\Entities\AuthToken>
+ * @noinspection PhpUnused
  */
-class AuthToken extends Record
+class UpdateInputFilter implements Filter
 {
-    public function filterUpdateInput(stdClass $data): void
+    public function filter(Data $data): void
     {
-        parent::filterUpdateInput($data);
-
-        $dataArray = get_object_vars($data);
-
-        foreach (array_keys($dataArray) as $attribute) {
+        foreach ($data->getAttributeList() as $attribute) {
             if ($attribute !== 'isActive') {
-                unset($data->$attribute);
-
-                continue;
+                $data->clear($attribute);
             }
         }
 
-        if ($data->isActive ?? false) {
-            unset($data->isActive);
+        if ($data->get('isActive')) {
+            $data->clear('isActive');
         }
     }
 }
