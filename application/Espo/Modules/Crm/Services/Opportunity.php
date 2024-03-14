e44617e27670469263731559a131a2f0d3a00a08
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2024-03-14 10:16:48 +0200

    fix extend

--- application/Espo/Modules/Crm/Services/Opportunity.php
+++ application/Espo/Modules/Crm/Services/Opportunity.php
@@ -29,12 +29,14 @@
 
 namespace Espo\Modules\Crm\Services;
 
+use Espo\ORM\Entity;
 use Espo\Services\Record;
 
 /**
  * For backward compatibility.
  * @todo Remove in v9.0.
  * @deprecated As of v8.2.
+ * @extends Record<Entity>
  */
 class Opportunity extends Record
 {}
