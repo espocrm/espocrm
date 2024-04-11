162e0ab5468c0527c3d6ae8c916b22fb49be5684
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2024-04-11 10:32:59 +0300

    ref

--- client/modules/crm/src/views/dashlets/activities.js
+++ client/modules/crm/src/views/dashlets/activities.js
@@ -26,26 +26,48 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-define('crm:views/dashlets/activities',
-['views/dashlets/abstract/base', 'multi-collection'], function (Dep, MultiCollection) {
+import BaseDashletView from 'views/dashlets/abstract/base';
+import MultiCollection from 'multi-collection';
 
-    return Dep.extend({
+class ActivitiesDashletView extends BaseDashletView {
 
-        name: 'Activities',
+    name = 'Activities'
 
-        // language=Handlebars
-        templateContent: '<div class="list-container">{{{list}}}</div>',
+    // language=Handlebars
+    templateContent = '<div class="list-container">{{{list}}}</div>'
 
-        rowActionsView: 'crm:views/record/row-actions/activities-dashlet',
+    rowActionsView = 'crm:views/record/row-actions/activities-dashlet'
 
-        defaultListLayout: {
+    defaultListLayout = {
+        rows: [
+            [
+                {
+                    name: 'ico',
+                    view: 'crm:views/fields/ico',
+                    params: {
+                        notRelationship: true,
+                    },
+                },
+                {
+                    name: 'name',
+                    link: true,
+                },
+            ],
+            [
+                {name: 'dateStart'},
+            ],
+        ],
+    }
+
+    listLayoutEntityTypeMap = {
+        Task: {
             rows: [
                 [
                     {
                         name: 'ico',
                         view: 'crm:views/fields/ico',
                         params: {
-                            notRelationship: true,
+                            notRelationship: true
                         },
                     },
                     {
@@ -54,199 +76,177 @@ define('crm:views/dashlets/activities',
                     },
                 ],
                 [
-                    {name: 'dateStart'},
+                    {name: 'dateEnd'},
+                    {
+                        name: 'priority',
+                        view: 'crm:views/task/fields/priority-for-dashlet',
+                    },
                 ],
-            ],
-        },
+            ]
+        }
+    }
 
-        listLayoutEntityTypeMap: {
-            Task: {
-                rows: [
-                    [
-                        {
-                            name: 'ico',
-                            view: 'crm:views/fields/ico',
-                            params: {
-                                notRelationship: true
-                            },
-                        },
-                        {
-                            name: 'name',
-                            link: true,
-                        },
-                    ],
-                    [
-                        {name: 'dateEnd'},
-                        {
-                            name: 'priority',
-                            view: 'crm:views/task/fields/priority-for-dashlet',
-                        },
-                    ],
-                ]
+    setup() {
+        this.seeds = {};
+
+        this.scopeList = this.getOption('enabledScopeList') || [];
+
+        this.listLayout = {};
+
+        this.scopeList.forEach((item) => {
+            if (item in this.listLayoutEntityTypeMap) {
+                this.listLayout[item] = this.listLayoutEntityTypeMap[item];
+
+                return;
             }
-        },
 
-        init: function () {
-            Dep.prototype.init.call(this);
-        },
+            this.listLayout[item] = this.defaultListLayout;
+        });
 
-        setup: function () {
-            this.seeds = {};
+        this.wait(true);
+        let i = 0;
 
-            this.scopeList = this.getOption('enabledScopeList') || [];
+        this.scopeList.forEach(scope => {
+            this.getModelFactory().create(scope, seed => {
+                this.seeds[scope] = seed;
 
-            this.listLayout = {};
+                i++;
 
-            this.scopeList.forEach((item) => {
-                if (item in this.listLayoutEntityTypeMap) {
-                    this.listLayout[item] = this.listLayoutEntityTypeMap[item];
-
-                    return;
-                }
-
-                this.listLayout[item] = this.defaultListLayout;
-            });
-
-            this.wait(true);
-            var i = 0;
-
-            this.scopeList.forEach(scope => {
-                this.getModelFactory().create(scope, seed => {
-                    this.seeds[scope] = seed;
-
-                    i++;
-
-                    if (i === this.scopeList.length) {
-                        this.wait(false);
-                    }
-                });
-            });
-
-            this.scopeList.slice(0).reverse().forEach(scope => {
-                if (this.getAcl().checkScope(scope, 'create')) {
-                    this.actionList.unshift({
-                        name: 'createActivity',
-                        text: this.translate('Create ' + scope, 'labels', scope),
-                        iconHtml: '<span class="fas fa-plus"></span>',
-                        url: '#' + scope + '/create',
-                        data: {
-                            scope: scope,
-                        },
-                    });
+                if (i === this.scopeList.length) {
+                    this.wait(false);
                 }
             });
-        },
+        });
 
-        afterRender: function () {
-            this.collection = new MultiCollection();
-            this.collection.seeds = this.seeds;
-            this.collection.url = 'Activities/upcoming';
-            this.collection.maxSize = this.getOption('displayRecords') ||
-                this.getConfig().get('recordsPerPageSmall') || 5;
-            this.collection.data.entityTypeList = this.scopeList;
-            this.collection.data.futureDays = this.getOption('futureDays');
-
-            this.listenToOnce(this.collection, 'sync', () => {
-                this.createView('list', 'crm:views/record/list-activities-dashlet', {
-                    selector: '> .list-container',
-                    pagination: false,
-                    type: 'list',
-                    rowActionsView: this.rowActionsView,
-                    checkboxes: false,
-                    collection: this.collection,
-                    listLayout: this.listLayout,
-                }, view => {
-                    view.render();
+        this.scopeList.slice(0).reverse().forEach(scope => {
+            if (this.getAcl().checkScope(scope, 'create')) {
+                this.actionList.unshift({
+                    name: 'createActivity',
+                    text: this.translate('Create ' + scope, 'labels', scope),
+                    iconHtml: '<span class="fas fa-plus"></span>',
+                    url: '#' + scope + '/create',
+                    data: {
+                        scope: scope,
+                    },
                 });
-            });
-
-            this.collection.fetch();
-        },
-
-        actionRefresh: function () {
-            this.collection.fetch({
-                previousDataList: this.collection.models.map(model => {
-                    return Espo.Utils.cloneDeep(model.attributes);
-                }),
-            });
-        },
-
-        actionCreateActivity: function (data) {
-            var scope = data.scope;
-            var attributes = {};
-
-            this.populateAttributesAssignedUser(scope, attributes);
-
-            Espo.Ui.notify(' ... ');
-
-            var viewName = this.getMetadata().get('clientDefs.'+scope+'.modalViews.edit') || 'views/modals/edit';
-
-            this.createView('quickCreate', viewName, {
-                scope: scope,
-                attributes: attributes,
-            }, view => {
-                view.render();
-                view.notify(false);
-
-                this.listenToOnce(view, 'after:save', () => {
-                    this.actionRefresh();
-                });
-            });
-        },
-
-        actionCreateMeeting: function () {
-            var attributes = {};
-
-            this.populateAttributesAssignedUser('Meeting', attributes);
-
-            Espo.Ui.notify(' ... ');
-
-            var viewName = this.getMetadata().get('clientDefs.Meeting.modalViews.edit') || 'views/modals/edit';
-
-            this.createView('quickCreate', viewName, {
-                scope: 'Meeting',
-                attributes: attributes,
-            }, view => {
-                view.render();
-                view.notify(false);
-
-                this.listenToOnce(view, 'after:save', () => {
-                    this.actionRefresh();
-                });
-            });
-        },
-
-        actionCreateCall: function () {
-            var attributes = {};
-
-            this.populateAttributesAssignedUser('Call', attributes);
-
-            Espo.Ui.notify(' ... ');
-
-            var viewName = this.getMetadata().get('clientDefs.Call.modalViews.edit') || 'views/modals/edit';
-
-            this.createView('quickCreate', viewName, {
-                scope: 'Call',
-                attributes: attributes,
-            }, view => {
-                view.render();
-                view.notify(false);
-
-                this.listenToOnce(view, 'after:save', () => {
-                    this.actionRefresh();
-                });
-            });
-        },
-
-        populateAttributesAssignedUser: function (scope, attributes) {
-            if (this.getMetadata().get(['entityDefs', scope, 'fields', 'assignedUsers'])) {
-                attributes['assignedUsersIds'] = [this.getUser().id];
-                attributes['assignedUsersNames'] = {};
-                attributes['assignedUsersNames'][this.getUser().id] = this.getUser().get('name');
-            } else {
-                attributes['assignedUserId'] = this.getUser().id;
-                attributes['assignedUserName'] = this.getUser().get('name');
             }
-        },
-    });
-});
+        });
+    }
+
+    afterRender() {
+        this.collection = new MultiCollection();
+        this.collection.seeds = this.seeds;
+        this.collection.url = 'Activities/upcoming';
+        this.collection.maxSize = this.getOption('displayRecords') ||
+            this.getConfig().get('recordsPerPageSmall') || 5;
+        this.collection.data.entityTypeList = this.scopeList;
+        this.collection.data.futureDays = this.getOption('futureDays');
+
+        this.listenToOnce(this.collection, 'sync', () => {
+            this.createView('list', 'crm:views/record/list-activities-dashlet', {
+                selector: '> .list-container',
+                pagination: false,
+                type: 'list',
+                rowActionsView: this.rowActionsView,
+                checkboxes: false,
+                collection: this.collection,
+                listLayout: this.listLayout,
+            }, view => {
+                view.render();
+            });
+        });
+
+        this.collection.fetch();
+    }
+
+    actionRefresh() {
+        this.collection.fetch({
+            previousDataList: this.collection.models.map(model => {
+                return Espo.Utils.cloneDeep(model.attributes);
+            }),
+        });
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    actionCreateActivity(data) {
+        const scope = data.scope;
+        const attributes = {};
+
+        this.populateAttributesAssignedUser(scope, attributes);
+
+        Espo.Ui.notify(' ... ');
+
+        const viewName = this.getMetadata().get('clientDefs.' + scope + '.modalViews.edit') || 'views/modals/edit';
+
+        this.createView('quickCreate', viewName, {
+            scope: scope,
+            attributes: attributes,
+        }, view => {
+            view.render();
+            view.notify(false);
+
+            this.listenToOnce(view, 'after:save', () => {
+                this.actionRefresh();
+            });
+        });
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    actionCreateMeeting() {
+        const attributes = {};
+
+        this.populateAttributesAssignedUser('Meeting', attributes);
+
+        Espo.Ui.notify(' ... ');
+
+        const viewName = this.getMetadata().get('clientDefs.Meeting.modalViews.edit') || 'views/modals/edit';
+
+        this.createView('quickCreate', viewName, {
+            scope: 'Meeting',
+            attributes: attributes,
+        }, view => {
+            view.render();
+            view.notify(false);
+
+            this.listenToOnce(view, 'after:save', () => {
+                this.actionRefresh();
+            });
+        });
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    actionCreateCall() {
+        const attributes = {};
+
+        this.populateAttributesAssignedUser('Call', attributes);
+
+        Espo.Ui.notify(' ... ');
+
+        const viewName = this.getMetadata().get('clientDefs.Call.modalViews.edit') || 'views/modals/edit';
+
+        this.createView('quickCreate', viewName, {
+            scope: 'Call',
+            attributes: attributes,
+        }, view => {
+            view.render();
+            view.notify(false);
+
+            this.listenToOnce(view, 'after:save', () => {
+                this.actionRefresh();
+            });
+        });
+    }
+
+    populateAttributesAssignedUser(scope, attributes) {
+        if (this.getMetadata().get(['entityDefs', scope, 'fields', 'assignedUsers'])) {
+            attributes['assignedUsersIds'] = [this.getUser().id];
+            attributes['assignedUsersNames'] = {};
+            attributes['assignedUsersNames'][this.getUser().id] = this.getUser().get('name');
+        } else {
+            attributes['assignedUserId'] = this.getUser().id;
+            attributes['assignedUserName'] = this.getUser().get('name');
+        }
+    }
+}
+
+export default ActivitiesDashletView;
