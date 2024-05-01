066f815d0ce7fa53ad04c78e75a3cb150115aba4
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2024-05-01 13:37:04 +0300

    dashblet dropdown overflow fix

--- client/src/ui/app-init.js
+++ client/src/ui/app-init.js
@@ -57,7 +57,6 @@ function uiAppInit() {
         const isRight = e.target.classList.contains('pull-right');
 
         const $ul = $(e.target.parentElement).find('.dropdown-menu');
-        const ul = $ul.get(0);
 
         const rect = e.target.getBoundingClientRect();
 
@@ -74,10 +73,51 @@ function uiAppInit() {
         const top = rect.top + scrollTop + e.target.getBoundingClientRect().height;
 
         const left = isRight ?
-            rect.left - $(ul).outerWidth() + rect.width:
+            rect.left - $ul.outerWidth() + rect.width:
             rect.left
 
         $ul.css({
+            top: top,
+            left: left,
+            right: 'auto',
+        });
+    });
+
+    $document.on('show.bs.dropdown', e => {
+        const $body = $(e.target).closest('.dashlet-body');
+
+        if (!$body.length) {
+            return;
+        }
+
+        const $group = $(e.target);
+
+        const rect = e.target.getBoundingClientRect();
+        const $ul = $group.find('.dropdown-menu');
+        const isRight = e.target.classList.contains('pull-right');
+
+        const $toggle = $group.find('.dropdown-toggle');
+
+        $body.on('scroll.dd', () => {
+            if ($group.hasClass('open')) {
+                // noinspection JSUnresolvedReference
+                $toggle.dropdown('toggle');
+                $body.off('scroll.dd');
+            }
+        })
+
+        $group.one('hide.bs.dropdown', () => {
+            $body.off('scroll.dd');
+        });
+
+        const left = isRight ?
+            rect.left - $ul.outerWidth() + rect.width:
+            rect.left
+
+        const top = rect.top + e.target.getBoundingClientRect().height;
+
+        $ul.css({
+            position: 'fixed',
             top: top,
             left: left,
             right: 'auto',
