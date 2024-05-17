f4b9b3fd814ad5b878a933100bdafd9a300546e6
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2024-05-17 12:52:50 +0300

    ref

--- client/modules/crm/src/views/lead/convert.js
+++ client/modules/crm/src/views/lead/convert.js
@@ -26,215 +26,214 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-define('crm:views/lead/convert', ['view'], function (Dep) {
+import View from 'view';
 
-    return Dep.extend({
+class ConvertLeadView extends View {
 
-        template: 'crm:lead/convert',
+    template = 'crm:lead/convert'
 
-        data: function () {
-            return {
-                scopeList: this.scopeList,
-                scope: this.model.entityType,
-            };
-        },
+    data() {
+        return {
+            scopeList: this.scopeList,
+            scope: this.model.entityType,
+        };
+    }
 
-        events: {
-            'change input.check-scope': function (e) {
-                var scope = $(e.currentTarget).data('scope');
-                var $div = this.$el.find('.edit-container-' + Espo.Utils.toDom(scope));
+    setup() {
+        this.addHandler('change', 'input.check-scope', (e, /** HTMLInputElement */target) => {
+            const scope = target.dataset.scope;
+            const $div = this.$el.find(`.edit-container-${Espo.Utils.toDom(scope)}`);
 
-                if (e.currentTarget.checked)    {
-                    $div.removeClass('hide');
-                } else {
-                    $div.addClass('hide');
-                }
-            },
-            'click button[data-action="convert"]': function (e) {
-                this.convert();
-            },
-            'click button[data-action="cancel"]': function (e) {
-                this.getRouter().navigate('#Lead/view/' + this.id, {trigger: true});
-            },
-        },
+            if (target.checked)    {
+                $div.removeClass('hide');
+            } else {
+                $div.addClass('hide');
+            }
+        });
 
-        setup: function () {
-            this.wait(true);
-            this.id = this.options.id;
+        this.addActionHandler('convert', () => this.convert());
+
+        this.addActionHandler('cancel', () => {
+            this.getRouter().navigate(`#Lead/view/${this.id}`, {trigger: true});
+        });
+
+        this.wait(true);
+        this.id = this.options.id;
+
+        Espo.Ui.notify(' ... ');
+
+        this.getModelFactory().create('Lead', model => {
+            this.model = model;
+            model.id = this.id;
+
+            this.listenToOnce(model, 'sync', () => this.build());
+
+            model.fetch();
+        });
+    }
+
+    build() {
+        const scopeList = this.scopeList = [];
+
+        (this.getMetadata().get('entityDefs.Lead.convertEntityList') || []).forEach(scope => {
+            if (scope === 'Account' && this.getConfig().get('b2cMode')) {
+                return;
+            }
+
+            if (this.getMetadata().get(['scopes', scope, 'disabled'])) {
+                return
+            }
+
+            if (this.getAcl().check(scope, 'edit')) {
+                scopeList.push(scope);
+            }
+        });
+
+        let i = 0;
+
+        const ignoreAttributeList = [
+            'createdAt',
+            'modifiedAt',
+            'modifiedById',
+            'modifiedByName',
+            'createdById',
+            'createdByName',
+        ];
+
+        if (scopeList.length === 0) {
+            this.wait(false);
+
+            return;
+        }
+
+
+        Espo.Ajax.postRequest('Lead/action/getConvertAttributes', {id: this.model.id}).then(data => {
+            scopeList.forEach(scope => {
+                this.getModelFactory().create(scope, model => {
+                    model.populateDefaults();
+
+                    model.set(data[scope] || {}, {silent: true});
+
+                    const convertEntityViewName = this.getMetadata()
+                        .get(['clientDefs', scope, 'recordViews', 'edit']) || 'views/record/edit';
+
+                    this.createView(scope, convertEntityViewName, {
+                        model: model,
+                        fullSelector: '#main .edit-container-' + Espo.Utils.toDom(scope),
+                        buttonsPosition: false,
+                        buttonsDisabled: true,
+                        layoutName: 'detailConvert',
+                        exit: () => {},
+                    }, () => {
+                        i++;
+
+                        if (i === scopeList.length) {
+                            this.wait(false);
+
+                            Espo.Ui.notify(false);
+                        }
+                    });
+                });
+            });
+        });
+    }
+
+    convert() {
+        const scopeList = [];
+
+        this.scopeList.forEach(scope => {
+            /** @type {HTMLInputElement} */
+            const el = this.$el.find(`input[data-scope="${scope}"]`).get(0);
+
+            if (el && el.checked) {
+                scopeList.push(scope);
+            }
+        });
+
+        if (scopeList.length === 0) {
+            Espo.Ui.error(this.translate('selectAtLeastOneRecord', 'messages'))
+
+            return;
+        }
+
+        this.getRouter().confirmLeaveOut = false;
+
+        let notValid = false;
+
+        scopeList.forEach(scope => {
+            const editView = /** @type {import('views/record/edit').default} */this.getView(scope);
+
+            editView.model.set(editView.fetch());
+            notValid = editView.validate() || notValid;
+        });
+
+        const data = {
+            id: this.model.id,
+            records: {},
+        };
+
+        scopeList.forEach(scope => {
+            data.records[scope] = this.getView(scope).model.attributes;
+        });
+
+        const process = (data) => {
+            this.$el.find('[data-action="convert"]').addClass('disabled');
 
             Espo.Ui.notify(' ... ');
 
-            this.getModelFactory().create('Lead', (model) => {
-                this.model = model;
-                model.id = this.id;
+            Espo.Ajax
+            .postRequest('Lead/action/convert', data)
+            .then(() => {
+                this.getRouter().confirmLeaveOut = false;
+                this.getRouter().navigate('#Lead/view/' + this.model.id, {trigger: true});
 
-                this.listenToOnce(model, 'sync', () => {
-                    this.build();
-                });
+                this.notify('Converted', 'success');
+            })
+            .catch(xhr => {
+                Espo.Ui.notify(false);
 
-                model.fetch();
-            });
-        },
+                this.$el.find('[data-action="convert"]').removeClass('disabled');
 
-        build: function () {
-            var scopeList = this.scopeList = [];
-
-            (this.getMetadata().get('entityDefs.Lead.convertEntityList') || []).forEach(scope => {
-                if (scope === 'Account' && this.getConfig().get('b2cMode')) {
+                if (xhr.status !== 409) {
                     return;
                 }
 
-                if (this.getMetadata().get(['scopes', scope, 'disabled'])) {
-                    return
+                if (xhr.getResponseHeader('X-Status-Reason') !== 'duplicate') {
+                    return;
                 }
 
-                if (this.getAcl().check(scope, 'edit')) {
-                    scopeList.push(scope);
+                let response = null;
+
+                try {
+                    response = JSON.parse(xhr.responseText);
+                } catch (e) {
+                    console.error('Could not parse response header.');
+
+                    return;
                 }
-            });
 
-            let i = 0;
+                xhr.errorIsHandled = true;
 
-            let ignoreAttributeList = [
-                'createdAt',
-                'modifiedAt',
-                'modifiedById',
-                'modifiedByName',
-                'createdById',
-                'createdByName',
-            ];
+                this.createView('duplicate', 'views/modals/duplicate', {duplicates: response}, view => {
+                    view.render();
 
-            if (scopeList.length) {
-                Espo.Ajax.postRequest('Lead/action/getConvertAttributes', {id: this.model.id})
-                    .then(data => {
-                        scopeList.forEach(scope => {
-                            this.getModelFactory().create(scope, model => {
-                                model.populateDefaults();
+                    this.listenToOnce(view, 'save', () => {
+                        data.skipDuplicateCheck = true;
 
-                                model.set(data[scope] || {}, {silent: true});
-
-                                let convertEntityViewName = this.getMetadata()
-                                    .get(['clientDefs', scope, 'recordViews', 'edit']) || 'views/record/edit';
-
-                                this.createView(scope, convertEntityViewName, {
-                                    model: model,
-                                    fullSelector: '#main .edit-container-' + Espo.Utils.toDom(scope),
-                                    buttonsPosition: false,
-                                    buttonsDisabled: true,
-                                    layoutName: 'detailConvert',
-                                    exit: () => {},
-                                }, view => {
-                                    i++;
-
-                                    if (i === scopeList.length) {
-                                        this.wait(false);
-                                        Espo.Ui.notify(false);
-                                    }
-                                });
-                            });
-                        });
-                    });
-            }
-
-            if (scopeList.length === 0) {
-                this.wait(false);
-            }
-        },
-
-        convert: function () {
-            let scopeList = [];
-
-            this.scopeList.forEach(scope => {
-                var el = this.$el.find('input[data-scope="' + scope + '"]').get(0);
-
-                if (el && el.checked) {
-                    scopeList.push(scope);
-                }
-            });
-
-            if (scopeList.length === 0) {
-                this.notify('Select one or more checkboxes', 'error');
-
-                return;
-            }
-
-            this.getRouter().confirmLeaveOut = false;
-
-            let notValid = false;
-
-            scopeList.forEach(scope => {
-                let editView = this.getView(scope);
-
-                editView.model.set(editView.fetch());
-                notValid = editView.validate() || notValid;
-            });
-
-            let data = {
-                id: this.model.id,
-                records: {},
-            };
-
-            scopeList.forEach(scope => {
-                data.records[scope] = this.getView(scope).model.attributes;
-            });
-
-            var process = (data) => {
-                this.$el.find('[data-action="convert"]').addClass('disabled');
-
-                Espo.Ui.notify(' ... ');
-
-                Espo.Ajax
-                .postRequest('Lead/action/convert', data)
-                .then(() => {
-                    this.getRouter().confirmLeaveOut = false;
-                    this.getRouter().navigate('#Lead/view/' + this.model.id, {trigger: true});
-
-                    this.notify('Converted', 'success');
-                })
-                .catch(xhr => {
-                    Espo.Ui.notify(false);
-
-                    this.$el.find('[data-action="convert"]').removeClass('disabled');
-
-                    if (xhr.status !== 409) {
-                        return;
-                    }
-
-                    if (xhr.getResponseHeader('X-Status-Reason') !== 'duplicate') {
-                        return;
-                    }
-
-                    let response = null;
-
-                    try {
-                        response = JSON.parse(xhr.responseText);
-                    } catch (e) {
-                        console.error('Could not parse response header.');
-
-                        return;
-                    }
-
-                    xhr.errorIsHandled = true;
-
-                    this.createView('duplicate', 'views/modals/duplicate', {duplicates: response}, view => {
-                        view.render();
-
-                        this.listenToOnce(view, 'save', () => {
-                            data.skipDuplicateCheck = true;
-
-                            process(data);
-                        });
+                        process(data);
                     });
                 });
-            };
+            });
+        };
 
-            if (notValid) {
-                this.notify('Not Valid', 'error');
+        if (notValid) {
+            Espo.Ui.error(this.translate('Not valid'))
 
-                return;
-            }
+            return;
+        }
 
-            process(data);
-        },
-    });
-});
+        process(data);
+    }
+}
+
+export default ConvertLeadView;
