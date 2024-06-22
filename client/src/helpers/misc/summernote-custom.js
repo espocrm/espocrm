e2673473c5a53d918183259dcf0831b9f0c3d6c8
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2024-06-22 09:09:42 +0300

    wysiwyg codeview not limited height

--- client/src/helpers/misc/summernote-custom.js
+++ client/src/helpers/misc/summernote-custom.js
@@ -443,7 +443,7 @@ function init(langSets) {
                     aceEditor.setValue(html);
 
                     aceEditor.setOptions({
-                        maxLines: !$editor.hasClass('fullscreen') ? 34: null,
+                        maxLines: !$editor.hasClass('fullscreen') ? 100000: null,
                         enableLiveAutocompletion: true,
                         tabSize: 2,
                         useSoftTabs: true,
@@ -941,7 +941,7 @@ function init(langSets) {
                     this._isFullscreen = false;
 
                     if (aceEditor) {
-                        aceEditor.setOptions({maxLines: maxLines});
+                        aceEditor.setOptions({maxLines: 100000});
                         aceEditor.resize();
                     }
                 }
