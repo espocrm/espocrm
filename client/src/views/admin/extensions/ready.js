60fc6279ff7e15d6c77331920004b978f48ad216
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2024-09-19 11:06:30 +0300

    ref

--- client/src/views/admin/extensions/ready.js
+++ client/src/views/admin/extensions/ready.js
@@ -26,48 +26,44 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-define('views/admin/extensions/ready', ['views/modal'], function (Dep) {
+import ModalView from 'views/modal';
 
-    return Dep.extend({
+export default class extends ModalView {
 
-        cssName: 'ready-modal',
+    template = 'admin/extensions/ready'
+    cssName = 'ready-modal'
+    createButton = true
 
-        header: false,
+    data() {
+        return {
+            version: this.upgradeData.version,
+            text: this.translate('installExtension', 'messages', 'Admin')
+                .replace('{version}', this.upgradeData.version)
+                .replace('{name}', this.upgradeData.name)
+        };
+    }
 
-        template: 'admin/extensions/ready',
+    setup() {
+        this.buttonList = [
+            {
+                name: 'run',
+                text: this.translate('Install', 'labels', 'Admin'),
+                style: 'danger',
+                onClick: () => this.actionRun(),
+            },
+            {
+                name: 'cancel',
+                label: 'Cancel',
+            },
+        ];
 
-        createButton: true,
+        this.upgradeData = this.options.upgradeData;
 
-        data: function () {
-            return {
-                version: this.upgradeData.version,
-                text: this.translate('installExtension', 'messages', 'Admin')
-                    .replace('{version}', this.upgradeData.version)
-                    .replace('{name}', this.upgradeData.name)
-            };
-        },
+        this.headerText = this.getLanguage().translate('Ready for installation', 'labels', 'Admin');
+    }
 
-        setup: function () {
-            this.buttonList = [
-                {
-                    name: 'run',
-                    text: this.translate('Install', 'labels', 'Admin'),
-                    style: 'danger',
-                },
-                {
-                    name: 'cancel',
-                    label: 'Cancel',
-                },
-            ];
-
-            this.upgradeData = this.options.upgradeData;
-
-            this.header = this.getLanguage().translate('Ready for installation', 'labels', 'Admin');
-        },
-
-        actionRun: function () {
-            this.trigger('run');
-            this.remove();
-        },
-    });
-});
+    actionRun() {
+        this.trigger('run');
+        this.remove();
+    }
+}
