3af293392336e832eb77b23adfceccd75ec9ace4
Renata Slupetska <99325916+lazespo@users.noreply.github.com>
2024-11-11 15:43:51 +0200

    Create PrettyPrintType.php

---
+++ application/Espo/Core/Formula/Functions/JsonGroup/PrettyPrintType.php
@@ -0,0 +1,57 @@
+<?php
+/************************************************************************
+ * This file is part of EspoCRM.
+ *
+ * EspoCRM – Open Source CRM application.
+ * Copyright (C) 2014-2024 Yurii Kuznietsov, Taras Machyshyn, Oleksii Avramenko
+ * Website: https://www.espocrm.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
+ ************************************************************************/
+
+namespace Espo\Core\Formula\Functions\JsonGroup;
+
+use Espo\Core\Formula\Functions\BaseFunction;
+use Espo\Core\Formula\ArgumentList;
+
+class PrettyPrintType extends BaseFunction
+{
+    public function process(ArgumentList $args)
+    {
+        if (count($args) < 1) {
+            $this->throwTooFewArguments();
+        }
+
+        $jsonString = $this->evaluate($args[0]);
+
+        return $this->prettyPrintJson($jsonString);
+    }
+    private function prettyPrintJson($jsonString)
+    {
+        $decodedJson = json_decode($jsonString, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            return $jsonString;
+        }
+
+        return json_encode($decodedJson, JSON_PRETTY_PRINT);
+    }
+}
