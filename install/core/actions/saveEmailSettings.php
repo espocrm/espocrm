963fb40b2b04e3a3c14a68fddbaef68243fe1775
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2025-04-14 11:13:15 +0300

    fix save email settings

--- install/core/actions/saveEmailSettings.php
+++ install/core/actions/saveEmailSettings.php
@@ -39,27 +39,26 @@ if (!empty($_SESSION['install'])) {
     ];
 
     $preferences = [];
+
     foreach ($paramList as $paramName) {
-
         switch ($paramName) {
-            case 'smtpAuth':
-                $preferences['smtpAuth'] = (empty($_SESSION['install']['smtpAuth']) || $_SESSION['install']['smtpAuth'] == 'false' || !$_SESSION['install']['smtpAuth']) ? false : true;
-                break;
-
             case 'outboundEmailIsShared':
-                $preferences['outboundEmailIsShared'] = (empty($_SESSION['install']['smtpAuth']) || $_SESSION['install']['outboundEmailIsShared'] == 'false' || !$_SESSION['install']['smtpAuth']) ? false : true;
+                $preferences['outboundEmailIsShared'] = $_SESSION['install']['outboundEmailIsShared'] === 'true';
+
                 break;
 
             default:
                 if (array_key_exists($paramName, $_SESSION['install'])) {
                     $preferences[$paramName] = $_SESSION['install'][$paramName];
                 }
+
                 break;
         }
 
     }
 
     $res = $installer->savePreferences($preferences);
+
     if (!empty($res)) {
         $result['success'] = true;
     } else {
