d479408fea4cefaa5fc718b01bba151d8fd2fb64
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2025-06-29 13:25:38 +0300

    date change on after change only for date

--- client/src/views/fields/date.js
+++ client/src/views/fields/date.js
@@ -144,19 +144,7 @@ class DateFieldView extends BaseFieldView {
 
                 // Timeout prevents the picker popping one when the duration field adjusts the date end.
                 setTimeout(() => {
-                    /** @type {string} */
-                    const from = this.model.attributes[this.params.after];
-                    /** @type {string} */
-                    const currentValue = this.model.attributes[this.name];
-
-                    if (
-                        from &&
-                        currentValue &&
-                        this.getDateTime().toMomentDate(currentValue)
-                            .isBefore(this.getDateTime().toMomentDate(from))
-                    ) {
-                        this.model.set(this.name, from);
-                    }
+                    this.onAfterChange();
 
                     this.datepicker.setStartDate(this.getStartDateForDatePicker());
                 }, 100);
@@ -580,6 +568,28 @@ class DateFieldView extends BaseFieldView {
             this.showValidationMessage(msg);
 
             return true;
+        }
+    }
+
+    /**
+     * @protected
+     * @since 9.2.0
+     */
+    onAfterChange() {
+        /** @type {string} */
+        const from = this.model.attributes[this.params.after];
+        /** @type {string} */
+        const currentValue = this.model.attributes[this.name];
+
+        if (!from || !currentValue || from.length !== currentValue.length) {
+            return;
+        }
+
+        if (
+            this.getDateTime().toMomentDate(currentValue)
+                .isBefore(this.getDateTime().toMomentDate(from))
+        ) {
+            this.model.set(this.name, from);
         }
     }
 }
--- client/src/views/fields/datetime.js
+++ client/src/views/fields/datetime.js
@@ -360,6 +360,8 @@ class DatetimeFieldView extends DateFieldView {
 
         return data;
     }
+
+    onAfterChange() {}
 }
 
 export default DatetimeFieldView;
