9935a6cd11718cb8a768b5483478f1a4c299f5c4
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2025-10-26 10:29:05 +0200

    default template value

--- application/Espo/ORM/Repository/RDBRelation.php
+++ application/Espo/ORM/Repository/RDBRelation.php
@@ -51,7 +51,7 @@ use RuntimeException;
 /**
  * An access point for a specific relation of a record.
  *
- * @template TEntity of Entity
+ * @template TEntity of Entity = Entity
  */
 class RDBRelation
 {
