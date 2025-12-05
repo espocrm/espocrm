5cbeea8119d141bfff034ee99be876ee98f28e42
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2025-12-05 11:18:57 +0200

    upgrade check

---
+++ upgrades/9.3/scripts/BeforeUpgrade.php
@@ -0,0 +1,95 @@
+<?php
+/************************************************************************
+ * This file is part of EspoCRM.
+ *
+ * EspoCRM – Open Source CRM application.
+ * Copyright (C) 2014-2025 EspoCRM, Inc.
+ * Website: https://www.espocrm.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
+ ************************************************************************/
+
+use Espo\Core\Container;
+use Espo\Entities\Extension;
+use Espo\ORM\EntityManager;
+
+/** @noinspection PhpMultipleClassDeclarationsInspection */
+class BeforeUpgrade
+{
+    private ?Container $container = null;
+
+    /**
+     * @throws Exception
+     */
+    public function run(Container $container): void
+    {
+        $this->container = $container;
+
+        $this->processCheckExtensions();
+    }
+
+    /**
+     * @throws Error
+     */
+    private function processCheckExtensions(): void
+    {
+        $errorMessageList = [];
+
+        $this->processCheckExtension('Google Integration', '1.8.3', $errorMessageList);
+        $this->processCheckExtension('Outlook Integration', '1.6.12', $errorMessageList);
+
+        if (!count($errorMessageList)) {
+            return;
+        }
+
+        $message = implode("\n\n", $errorMessageList);
+
+        throw new Error($message);
+    }
+
+    private function processCheckExtension(string $name, string $minVersion, array &$errorMessageList): void
+    {
+        $em = $this->container->getByClass(EntityManager::class);
+
+        $extension = $em->getRDBRepository(Extension::ENTITY_TYPE)
+            ->where([
+                'name' => $name,
+                'isInstalled' => true,
+            ])
+            ->findOne();
+
+        if (!$extension) {
+            return;
+        }
+
+        $version = $extension->get('version');
+
+        if (version_compare($version, $minVersion, '>=')) {
+            return;
+        }
+
+        $message =
+            "EspoCRM 9.3 is not compatible with '$name' extension of versions lower than $minVersion. " .
+            "You need to upgrade the extension.";
+
+        $errorMessageList[] = $message;
+    }
+}
