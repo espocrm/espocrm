611d9936915432aaabfa051b832adbaf34ebb624
Yurii <yurikuzn@users.noreply.github.com>
2025-12-12 17:35:48 +0200

    dev

---
+++ application/Espo/Classes/Acl/CurrencyRecordRate/AccessChecker.php
@@ -0,0 +1,76 @@
+<?php
+/************************************************************************
+ * This file is part of EspoCRM.
+ *
+ * EspoCRM – Open Source CRM application.
+ * Copyright (C) 2014-2025 EspoCRM, Inc.
+ * Website: https://www.espocrm.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
+ ************************************************************************/
+
+namespace Espo\Classes\Acl\CurrencyRecordRate;
+
+use Espo\Core\Acl\AccessEntityCREDChecker;
+use Espo\Core\Acl\DefaultAccessChecker;
+use Espo\Core\Acl\ScopeData;
+use Espo\Core\Acl\Table;
+use Espo\Core\Acl\Traits\DefaultAccessCheckerDependency;
+use Espo\Entities\User;
+use Espo\ORM\Entity;
+
+class AccessChecker implements AccessEntityCREDChecker
+{
+    use DefaultAccessCheckerDependency;
+
+    public function __construct(
+        private DefaultAccessChecker $defaultAccessChecker,
+    ) {}
+
+
+    public function checkCreate(User $user, ScopeData $data): bool
+    {
+        if ($data->getEdit() === Table::LEVEL_YES) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function checkDelete(User $user, ScopeData $data): bool
+    {
+        if ($data->getEdit() === Table::LEVEL_YES) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function checkEntityCreate(User $user, Entity $entity, ScopeData $data): bool
+    {
+        return $this->checkCreate($user, $data);
+    }
+
+    public function checkEntityDelete(User $user, Entity $entity, ScopeData $data): bool
+    {
+        return $this->checkDelete($user, $data);
+    }
+}
