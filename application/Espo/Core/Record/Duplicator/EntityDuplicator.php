9ca71c91bf1642c8fe730be32bc3e80679c7ab24
Yurii <yurikuzn@users.noreply.github.com>
2026-03-10 20:32:33 +0200

    ignore password

--- application/Espo/Core/Record/Duplicator/EntityDuplicator.php
+++ application/Espo/Core/Record/Duplicator/EntityDuplicator.php
@@ -134,7 +134,15 @@ class EntityDuplicator
     {
         $type = $fieldDefs->getType();
 
-        if (in_array($type, [FieldType::AUTOINCREMENT, FieldType::NUMBER, FieldType::LINK_ONE])) {
+        if (
+            in_array($type, [
+                FieldType::AUTOINCREMENT,
+                FieldType::NUMBER,
+                FieldType::LINK_ONE,
+                // Additional measure. It's not fetched.
+                FieldType::PASSWORD,
+            ])
+        ) {
             return true;
         }
 
