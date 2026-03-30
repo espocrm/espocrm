4e07cb25e1e56793fb358cde615fe2ce7438fa80
Yurii <yurikuzn@users.noreply.github.com>
2026-03-30 16:21:18 +0300

    fix type

--- client/src/views/list.js
+++ client/src/views/list.js
@@ -785,7 +785,7 @@ class ListView extends MainView {
      * params.options.categoryName = this.currentCategoryName;
      * ```
      *
-     * @param {{controller?: string, action?: string, options?: Record}} params Parameters to be modified.
+     * @param {{controller?: string, action?: string|null, options?: Record}} params Parameters to be modified.
      * Unset `controller` if bypass return dispatching (to force to come back to the `returnUrl`).
      */
     prepareCreateReturnDispatchParams(params) {}
