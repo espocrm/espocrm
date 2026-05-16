71ad7661215d25f7d0f2756c8e8dbc257cad848f
Yurii <yurikuzn@users.noreply.github.com>
2026-05-16 08:51:18 +0300

    Fix import export csv

--- application/Espo/Tools/Import/Service.php
+++ application/Espo/Tools/Import/Service.php
@@ -447,16 +447,11 @@ class Service
      * @param string $importId An import ID.
      * @return ?string An attachment ID.
      * @throws NotFound
+     * @throws Forbidden
      */
     public function exportErrors(string $importId): ?string
     {
-        $import = $this->entityManager
-            ->getRepositoryByClass(ImportEntity::class)
-            ->getById($importId);
-
-        if (!$import) {
-            throw new NotFound();
-        }
+        $import = $this->getImport($importId);
 
         $count = $this->entityManager
             ->getRDBRepositoryByClass(ImportEntity::class)
@@ -544,5 +539,24 @@ class Service
         fclose($resource);
 
         return $attachment->getId();
+    }
+
+    /**
+     * @throws Forbidden
+     * @throws NotFound
+     */
+    private function getImport(string $importId): ImportEntity
+    {
+        $import = $this->entityManager->getRepositoryByClass(ImportEntity::class)->getById($importId);
+
+        if (!$import) {
+            throw new NotFound();
+        }
+
+        if (!$this->acl->checkEntityRead($import)) {
+            throw new Forbidden("No access to the import record.");
+        }
+
+        return $import;
     }
 }
