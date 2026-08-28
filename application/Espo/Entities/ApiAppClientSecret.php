2b8fc373119cdd87be9d3c9097e0f1a49c6235fb
Yurii <yurikuzn@users.noreply.github.com>
2026-08-28 11:06:01 +0300

    Oauth 2 server dev

---
+++ application/Espo/Entities/ApiAppClientSecret.php
@@ -0,0 +1,78 @@
+<?php
+/************************************************************************
+ * This file is part of EspoCRM.
+ *
+ * EspoCRM – Open Source CRM application.
+ * Copyright (C) 2014-2026 EspoCRM, Inc.
+ * Website: https://www.espocrm.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
+ ************************************************************************/
+
+namespace Espo\Entities;
+
+use Espo\Core\ORM\Entity;
+use UnexpectedValueException;
+
+class ApiAppClientSecret extends Entity
+{
+    public const string ENTITY_TYPE = 'ApiAppClientSecret';
+
+    public const string FIELD_STATUS = 'status';
+    public const string FIELD_VALUE = 'value';
+    public const string FIELD_APP = 'app';
+    public const string FIELD_EXPIRATION_DATE = 'expirationDate';
+
+    public const string STATUS_ACTIVE = 'Active';
+    public const string STATUS_REVOKED = 'Revoked';
+    public const string STATUS_EXPIRED = 'Expired';
+
+    /**
+     * Set an encrypted value.
+     */
+    public function setValue(string $value): self
+    {
+        return $this->set(self::FIELD_VALUE, $value);
+    }
+
+    /**
+     * Get an encrypted value.
+     */
+    public function getValue(): string
+    {
+        return $this->get(self::FIELD_VALUE) ?? throw new UnexpectedValueException("No value.");
+    }
+
+    public function isActive(): bool
+    {
+        return $this->get(self::FIELD_STATUS) === self::STATUS_ACTIVE;
+    }
+
+    public function setRevoked(): self
+    {
+        return $this->set(self::FIELD_STATUS, self::STATUS_REVOKED);
+    }
+
+    public function setExpired(): self
+    {
+        return $this->set(self::FIELD_STATUS, self::STATUS_EXPIRED);
+    }
+}
